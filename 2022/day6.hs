ea161325326806a673f417432e3058d35e98476e
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2023-11-27 21:44:30 +0100

    2022, day 6, part 2

--- 2022/day6.hs
+++ 2022/day6.hs
@@ -3,14 +3,18 @@ import Data.List.Split ( splitOn )
 import System.Directory ( getCurrentDirectory )
 import System.Environment ( getArgs )
 
+import Data.List ( nub )
+
 day = 6
 
-shorten :: String -> String
-shorten (a:b:c:d:xs)
-    | a == b = shorten (b:c:d:xs)
-    | a == c || b == c = shorten (c:d:xs)
-    | a == d || b == d || c == d= shorten (d:xs)
-    | otherwise = xs
+shorten :: Int -> String -> String
+shorten size string@(x:xs)
+    | length full == length nubbed = drop size string
+    | otherwise = shorten size xs
+  where
+        full = take size string
+        nubbed = nub full
+
 main = do
         args <- getArgs
         directory <- getCurrentDirectory
@@ -18,5 +22,6 @@ main = do
         input <- retrieveInput year day args 
         let sequence = init input
         putStrLn "part 1"
-        print (length sequence - (length . shorten $ sequence))
+        print (length sequence - (length . shorten 4 $ sequence))
         putStrLn "part 2"
+        print (length sequence - (length . shorten 14 $ sequence))
