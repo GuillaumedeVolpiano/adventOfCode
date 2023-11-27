b4cd5761104a536e459e0fbfc3b49111e2f8a155
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2023-11-27 16:14:42 +0100

    2022, day 1

--- 2022/day1.hs
+++ 2022/day1.hs
@@ -1,5 +1,15 @@
 import InputRetrieval ( retrieveInput )
+import Data.List ( sortBy )
+import Data.List.Split ( splitOn)
+import System.Environment ( getArgs )
+
 
 main = do
-        input <- retrieveInput 2022 1
-        print input
+        args <- getArgs
+        input <- retrieveInput 2022 1 args 
+        let calories = map (sum . map read . filter (not . null) . splitOn "\n") . splitOn "\n\n" $ input 
+        putStrLn "part 1"
+        print . maximum $ calories
+        putStrLn "part 2"
+        print . sum . take 3 . sortBy (flip compare) $ calories
+
--- InputRetrieval.hs
+++ InputRetrieval.hs
@@ -14,12 +14,17 @@ module InputRetrieval ( retrieveInput ) where
             let (name:value:_) = map (\(_:y:_) -> init y) . filter (\(x:_) -> x == "name" || x == "value") . map (splitOn ":") . splitOn "\n" . filter (\x -> x /= ' ' && x /= '\"') . drop 4 . take (cookieLength - 2) $ rawCookie 
             return $ name ++ "=" ++ value
 
-    retrieveInput :: Int -> Int -> IO String
-    retrieveInput year day = do
+    retrieveInput :: Int -> Int -> [String] -> IO String
+    retrieveInput year day args = do
+            case args of 
+                     [] -> remoteInput year day
+                     a -> readFile $ "day" ++ show day ++ "-test.txt"
+
+    remoteInput :: Int -> Int -> IO String 
+    remoteInput year day = do
             let url = adventURL ++ show year ++ "/day/" ++ show day ++ "/input"
             home <- getHomeDirectory
             cookie <- parseCookie $  home ++ cookiePath
             proxy <- readFile (home ++ proxyPath)
             (code, rsp) <- curlGetString url [CurlProxy (init proxy), CurlCookie cookie] 
-            print code
             return rsp
