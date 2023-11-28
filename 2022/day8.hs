cc5765c37267e6a230a0bcc97f95b48fa74b5a39
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2023-11-28 19:50:45 +0100

    2022, day 8, part 2

--- 2022/day8.hs
+++ 2022/day8.hs
@@ -10,28 +10,30 @@ import           Linear.V2          (V2 (..))
 
 day = 8
 
-findVisibles :: Array (V2 Int) Int -> [V2 Int] -> [V2 Int]
-findVisibles treeArray toSee
-  | null toSee = []
-  | testVisible treeArray p = p : findVisibles treeArray ps
-  | otherwise = findVisibles treeArray ps
-  where
-    (p:ps) = toSee
-
 testVisible :: Array (V2 Int) Int -> V2 Int -> Bool
 testVisible treeArray pos@(V2 x y)
   | x == 0 || y == 0 || x == maxX || y == maxY = True
-  | otherwise = testLines [V2 0 1, V2 0 (-1), V2 1 0, V2 (-1) 0]
+  | otherwise = foldl testLines False [V2 0 1, V2 0 (-1), V2 1 0, V2 (-1) 0]
   where
     b@(_, V2 maxX maxY) = bounds treeArray
     height = treeArray ! pos
-    testLines []     = False
-    testLines (p:ps) = test p (p + pos) || testLines ps
+    testLines state p = state || test p (p + pos)
     test p cur
       | not (inRange b cur) = True
       | otherwise = treeArray ! cur < height && test p (p + cur)
 
--- scoreTrees :: Array (V2 Int) Int -> V2 Int -> [Int]
+scoreTrees :: Array (V2 Int) Int -> V2 Int -> Int
+scoreTrees treeArray pos =
+  product . map scoreLine $ [V2 0 1, V2 0 (-1), V2 1 0, V2 (-1) 0]
+  where
+    height = treeArray ! pos
+    b = bounds treeArray
+    scoreLine p = score p (p + pos)
+    score p cur
+      | not (inRange b cur) = 0
+      | height <= treeArray ! cur = 1
+      | otherwise = 1 + score p (p + cur)
+
 main = do
   args <- getArgs
   directory <- getCurrentDirectory
@@ -48,5 +50,6 @@ main = do
           , y <- [0 .. height]
           ] :: Array (V2 Int) Int
   putStrLn "part 1"
-  print . length . findVisibles treeArray $ indices treeArray
+  print . length . filter (testVisible treeArray) $ indices treeArray
   putStrLn "part 2"
+  print . maximum . map (scoreTrees treeArray) $ indices treeArray
