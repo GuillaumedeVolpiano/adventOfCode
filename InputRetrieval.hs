8f6008c107e73f24f480dcc1609f0bd913ce1978
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2023-11-28 19:26:24 +0100

    prettyfying

--- InputRetrieval.hs
+++ InputRetrieval.hs
@@ -1,30 +1,42 @@
-module InputRetrieval ( retrieveInput ) where
-    import Data.List.Split ( splitOn )
-    import Network.Curl ( CurlOption (CurlProxy, CurlCookie), curlGetString )
-    import System.Directory ( getHomeDirectory )
+module InputRetrieval
+  ( retrieveInput
+  ) where
 
-    cookiePath = "/adventOfCode/cookies.json"
-    adventURL = "https://adventofcode.com/"
-    proxyPath = "/adventOfCode/proxy"
+import           Data.List.Split  (splitOn)
+import           Network.Curl     (CurlOption (CurlCookie, CurlProxy),
+                                   curlGetString)
+import           System.Directory (getHomeDirectory)
 
-    parseCookie :: String -> IO String
-    parseCookie path = do
-            rawCookie <- readFile path
-            let cookieLength = length rawCookie
-            let (name:value:_) = map (\(_:y:_) -> init y) . filter (\(x:_) -> x == "name" || x == "value") . map (splitOn ":") . splitOn "\n" . filter (\x -> x /= ' ' && x /= '\"') . drop 4 . take (cookieLength - 2) $ rawCookie 
-            return $ name ++ "=" ++ value
+cookiePath = "/adventOfCode/cookies.json"
 
-    retrieveInput :: Int -> Int -> [String] -> IO String
-    retrieveInput year day args = do
-            case args of 
-                     [] -> remoteInput year day
-                     a -> readFile $ "day" ++ show day ++ "-test.txt"
+adventURL = "https://adventofcode.com/"
 
-    remoteInput :: Int -> Int -> IO String 
-    remoteInput year day = do
-            let url = adventURL ++ show year ++ "/day/" ++ show day ++ "/input"
-            home <- getHomeDirectory
-            cookie <- parseCookie $  home ++ cookiePath
-            proxy <- readFile (home ++ proxyPath)
-            (code, rsp) <- curlGetString url [CurlProxy (init proxy), CurlCookie cookie] 
-            return rsp
+proxyPath = "/adventOfCode/proxy"
+
+parseCookie :: String -> IO String
+parseCookie path = do
+  rawCookie <- readFile path
+  let cookieLength = length rawCookie
+  let (name:value:_) =
+        map (\(_:y:_) -> init y) .
+        filter (\(x:_) -> x == "name" || x == "value") .
+        map (splitOn ":") .
+        splitOn "\n" .
+        filter (\x -> x /= ' ' && x /= '\"') . drop 4 . take (cookieLength - 2) $
+        rawCookie
+  return $ name ++ "=" ++ value
+
+retrieveInput :: Int -> Int -> [String] -> IO String
+retrieveInput year day args = do
+  case args of
+    [] -> remoteInput year day
+    a  -> readFile $ "day" ++ show day ++ "-test.txt"
+
+remoteInput :: Int -> Int -> IO String
+remoteInput year day = do
+  let url = adventURL ++ show year ++ "/day/" ++ show day ++ "/input"
+  home <- getHomeDirectory
+  cookie <- parseCookie $ home ++ cookiePath
+  proxy <- readFile (home ++ proxyPath)
+  (code, rsp) <- curlGetString url [CurlProxy (init proxy), CurlCookie cookie]
+  return rsp
