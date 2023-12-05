cf42ea816e0b00ce8e03c6a46a27e0c496d22019
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2023-12-05 12:50:03 +0100

    2023, day 6

--- template.hs
+++ template.hs
@@ -1,6 +1,7 @@
 import           InputRetrieval     (retrieveInput)
 import           System.Directory   (getCurrentDirectory)
 import           System.Environment (getArgs, getProgName)
+import           System.TimeIt      (timeIt)
 import           Text.Regex.TDFA    (getAllTextMatches, (=~))
 
 main = do
@@ -11,4 +12,6 @@ main = do
       day = read $ prog =~ "[0-9]+"
   input <- retrieveInput year day args
   putStrLn "part 1"
+  timeIt . print $ "solution 1"
   putStrLn "part 2"
+  timeIt . print $ "solution 2"
