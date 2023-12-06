d7d120a31bebee010abba24f0aeaa2a62ff4b730
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2023-12-06 10:37:33 +0100

    2022, day 6, improved

--- 2023/day6.hs
+++ 2023/day6.hs
@@ -27,6 +27,16 @@ bestOutcomes race@(time, _) = time + 1 - loseFromLeft - loseFromRight
     loseFromLeft = loseFrom race [0 .. time]
     loseFromRight = loseFrom race [time - x | x <- [0 .. time]]
 
+-- The problem boils down to x(t - x) > r, that is x² - tx + r < 0,
+-- that is any integer between the roots of x^2 - tx + r
+quadraticSolution :: (Int, Int) -> Int
+quadraticSolution (time, record) = maxX - minX + 1
+  where
+    t = fromIntegral time :: Double
+    r = fromIntegral record :: Double
+    minX = ceiling $ (t - sqrt (t ^ 2 - 4 * r)) / 2
+    maxX = floor $ (t + sqrt (t ^ 2 - 4 * r)) / 2
+
 main = do
   args <- getArgs
   directory <- getCurrentDirectory
@@ -38,5 +48,8 @@ main = do
   -- let's compare timing between the two versions of bestOutcomes in both
   -- situtations, because why not?
   preciseTimeIt 4 . print . product . map bestOutcomes . parseLines $ input
+  preciseTimeIt 5 . print . product . map quadraticSolution . parseLines $ input
   putStrLn "part 2"
   preciseTimeIt 4 . print . bestOutcomes . kern . map parseLine . lines $ input
+  preciseTimeIt 5 . print . quadraticSolution . kern . map parseLine . lines $
+    input
