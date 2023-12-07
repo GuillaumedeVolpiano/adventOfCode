815c653e7d5ae6299473cb1f23f6a5b734e013f2
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2023-12-08 00:00:14 +0100

    ignoring template

--- template.hs
+++ template.hs
@@ -1,14 +1,8 @@
-import           General            (customPreciseTimeIt, retrieveInput)
-import           System.Directory   (getCurrentDirectory)
-import           System.Environment (getArgs, getProgName)
+module DayX (part1, part2) where
 import           Text.Regex.TDFA    (getAllTextMatches, (=~))
 
-main = do
-  args <- getArgs
-  directory <- getCurrentDirectory
-  prog <- getProgName
-  let year = read $ directory =~ "[0-9]+"
-      day = read $ prog =~ "[0-9]+"
-  input <- retrieveInput year day args
-  customPreciseTimeIt "part 1 CPU time" 3 . print $ "solution 1"
-  customPreciseTimeIt "part 2 CPU time" 3 . print $ "solution 2"
+part1 :: Bool -> String -> String
+part1 _ = "Part 1"
+
+part2 :: Bool -> String -> String
+part2 _ = "Part 2"
