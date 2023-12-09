b6943942cdc941a58573f647cd55dc5137a53780
Xavier Dectot <xavier.dectot@gmail.com>
2023-12-09 10:34:45 +0100

    adding a proxy switch

--- 2023/days/Day9.hs
+++ 2023/days/Day9.hs
@@ -3,30 +3,27 @@ module Day9
   , part2
   ) where
 
-import           Data.Sequence   as Sq (Seq ((:<|), (:|>)), empty, fromList,
-                                        null, singleton)
 import           Text.Regex.TDFA (getAllTextMatches, (=~))
 
-parseLine :: String -> [Seq Int]
+parseLine :: String -> [[Int]]
 parseLine =
-  map (fromList . map read . (\t -> getAllTextMatches (t =~ "-?[-0-9]+"))) .
-  lines
+  map (map read . (\t -> getAllTextMatches (t =~ "-?[-0-9]+"))) . lines
 
-toZero :: Seq Int -> Seq (Seq Int)
+toZero :: [Int] -> [[Int]]
 toZero s
-  | foldl (\a b -> a && b == 0) True s = empty
-  | otherwise = s :<| (toZero . diffs $ s)
+  | all (== 0) s = []
+  | otherwise = s : (toZero . diffs $ s)
 
-diffs :: Seq Int -> Seq Int
-diffs (a :<| xs@(b :<| r))
-  | Sq.null r = singleton (b - a)
-  | otherwise = (b - a) :<| diffs xs
+diffs :: [Int] -> [Int]
+diffs (a:xs@(b:r))
+  | null r = [b - a]
+  | otherwise = (b - a) : diffs xs
 
-extrapolate :: Seq (Seq Int) -> Int
-extrapolate = foldl (\a (_ :|> b) -> a + b) 0
+extrapolate :: [[Int]] -> Int
+extrapolate = sum . map head
 
-extrapolateBackwards :: Seq (Seq Int) -> Int
-extrapolateBackwards = foldr (\(b :<| _) a -> b - a) 0
+extrapolateBackwards :: [[Int]] -> Int
+extrapolateBackwards = foldr ((-) . head) 0
 
 part1 :: Bool -> String -> String
 part1 _ = show . sum . map (extrapolate . toZero) . parseLine
