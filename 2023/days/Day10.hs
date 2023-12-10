099003b5c727062a394bce4009196074b965b52f
Xavier Dectot <xavier.dectot@gmail.com>
2023-12-10 17:47:59 +0100

    2023, day10. A difficult part two.

--- 2023/days/Day10.hs
+++ 2023/days/Day10.hs
@@ -3,14 +3,15 @@ module Day10
   , part2
   ) where
 
-import           Data.Array.Unboxed as U (UArray, assocs, bounds, inRange,
-                                          indices, (!))
-import           Data.Graph         as G (Graph, graphFromEdges)
-import           Data.Map           as M (keys, (!))
+import           Data.Array.Unboxed as A (UArray, array, assocs, bounds,
+                                          inRange, indices, (!))
+import           Data.List          as L (filter, null)
 import           Data.Maybe         (fromJust)
+import           Data.Sequence      as Sq (Seq ((:<|), (:|>)), fromList, null,
+                                           (><))
+import           Data.Set           as St (Set, empty, filter, fromList, insert,
+                                           notMember, singleton, size, union)
 import           Helpers.Parsers    (arrayFromString)
-import           Helpers.Search     (NodeFromVertex, VertexFromKey, bfsDist,
-                                     floydWarshall)
 import           Linear.V2          (V2 (..))
 
 type Pos = V2 Int
@@ -19,8 +20,6 @@ type Diagram = UArray Pos Pipe
 
 type Pipe = Char
 
-type FullGraph = (Graph, NodeFromVertex Pos Pos, VertexFromKey Pos)
-
 north = V2 0 (-1)
 
 south = V2 0 1
@@ -28,6 +27,9 @@ south = V2 0 1
 east = V2 1 0
 
 west = V2 (-1) 0
+
+testSamplePart2 =
+  "..........\n.S------7.\n.|F----7|.\n.||....||.\n.||....||.\n.|L-7F-J|.\n.|..||..|.\n.L--JL--J.\n.........."
 
 pipes =
   [ ('|', [north, south])
@@ -41,35 +43,96 @@ pipes =
   ]
 
 findStart :: Diagram -> Pos
-findStart = fst . head . filter (\(a, b) -> b == 'S') . assocs
-
-graphFromArray :: Diagram -> FullGraph
-graphFromArray diagram =
-  graphFromEdges [(pos, pos, neighbours diagram pos) | pos <- indices diagram]
+findStart = fst . head . L.filter (\(a, b) -> b == 'S') . assocs
 
 neighbours :: Diagram -> Pos -> [Pos]
 neighbours diagram pos =
-  filter (inRange $ bounds diagram) .
-  map (pos +) . fromJust . lookup (diagram U.! pos) $
+  L.filter (inRange $ bounds diagram) .
+  map (pos +) . fromJust . lookup (diagram A.! pos) $
   pipes
 
-neighboursNoStart :: Diagram -> Pos -> [Pos]
-neighboursNoStart diagram =
-  filter (\t -> diagram U.! t /= 'S') . neighbours diagram
+explore :: Diagram -> Set Pos -> Pos -> Set Pos
+explore diagram seen pos
+  | L.null next = insert pos seen
+  | otherwise = explore diagram (insert pos seen) . head $ next
+  where
+    next = L.filter (`notMember` seen) . neighbours diagram $ pos
+
+startNeighbours :: Diagram -> Pos -> [Pos]
+startNeighbours diagram pos =
+  L.filter (elem pos . neighbours diagram) $ neighbours diagram pos
+
+fill :: Diagram -> Seq Pos -> Set Pos -> Set Pos -> Set Pos
+fill diagram toSee loop seen
+  | Sq.null toSee = seen
+  | otherwise = fill diagram newToSee loop newSeen
+  where
+    (pos :<| rest) = toSee
+    toConsider =
+      L.filter
+        (\t ->
+           inRange (bounds diagram) t && notMember t seen && notMember t loop) $
+      map (pos +) [north, south, east, west]
+    newToSee = rest >< Sq.fromList toConsider
+    newSeen = union seen $ St.fromList toConsider
+
+expandDiagram :: Diagram -> Diagram
+expandDiagram diagram =
+  array
+    (V2 0 0, V2 (2 * width) (2 * height))
+    [(V2 x y, expandVal x y) | x <- [0 .. 2 * width], y <- [0 .. 2 * height]]
+  where
+    b@(_, V2 width height) = bounds diagram
+    expandVal x y
+      | even x && even y = diagram ! V2 (div x 2) (div y 2)
+      | even y &&
+          inRange b (V2 (div (x - 1) 2) (div y 2)) &&
+          inRange b (V2 (div (x + 1) 2) (div y 2)) &&
+          elem (diagram ! V2 (div (x - 1) 2) (div y 2)) "F-LS" &&
+          elem (diagram ! V2 (div (x + 1) 2) (div y 2)) "J-7S" = '-'
+      | even x &&
+          inRange b (V2 (div x 2) (div (y - 1) 2)) &&
+          inRange b (V2 (div x 2) (div (y + 1) 2)) &&
+          elem (diagram ! V2 (div x 2) (div (y - 1) 2)) "7F|S" &&
+          elem (diagram ! V2 (div x 2) (div (y + 1) 2)) "JL|S" = '|'
+      | otherwise = '.'
 
 part1 :: Bool -> String -> String
-part1 _ input = show $ div fromToTo 2 + 1
+part1 _ input =
+  show .
+  div
+    (size . explore diagram (singleton start) . head . startNeighbours diagram $
+     start) $
+  2
   where
     diagram = arrayFromString input
     start = findStart diagram
-    [from, to] =
-      filter (\t -> start `elem` neighbours diagram t) . neighbours diagram $
-      start
-    fromToTo = bfsDist from (neighboursNoStart diagram) (== to)
 
---  show . maximum . map (fw M.!) . filter (\(a, _) -> a == start) . keys $ fw
---    graph@(_, nfv, vfk) = graphFromArray diagram
---    fw = floydWarshall graph
---    start = fromJust . vfk . findStart $ diagram
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 test input = show $ totalSize - sizeLoop - sizeFilled
+  where
+    diagram
+      | test = arrayFromString testSamplePart2
+      | otherwise = arrayFromString input
+    expanded = expandDiagram diagram
+    (_, V2 mx my) = bounds expanded
+    loop =
+      explore expanded (singleton start) . head . startNeighbours expanded $
+      start
+    start = findStart expanded
+    edgesSeq = Sq.fromList edges
+    edgesSet = St.fromList edges
+    edges =
+      L.filter
+        (`notMember` loop)
+        [ V2 x y
+        | x <- [0 .. mx]
+        , y <- [0 .. my]
+        , x == 0 || x == mx || y == 0 || y == my
+        ]
+    totalSize = length . indices $ diagram
+    sizeLoop = St.size . St.filter (\(V2 x y) -> even x && even y) $ loop
+    sizeFilled =
+      St.size .
+      St.filter (\(V2 x y) -> even x && even y) . fill expanded edgesSeq loop $
+      edgesSet
