f3497cb0c6df923893275472c81de869d2bfad54
Xavier Dectot <xavier.dectot@gmail.com>
2023-12-12 00:48:14 +0100

    2021, day 6

--- 2021/days/Day6.hs
+++ 2021/days/Day6.hs
@@ -1,8 +1,25 @@
-module Day6 (part1, part2) where
-import Helpers.Parsers
+module Day6
+  ( part1
+  , part2
+  ) where
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+import           Data.List       as L (length)
+import           Data.Sequence   as Sq (Seq ((:<|), (:|>)), fromList, length)
+import           Helpers.Parsers (integers)
+
+grow :: Seq Int -> Seq Int
+grow f@(h :<| r@(o :|> s :|> e))
+  | Sq.length f == 7 = r :|> h :|> 0 :|> h
+  | otherwise = o :|> h + s :|> e :|> h
+
+part1 :: Bool -> String -> String
+part1 _ input = show . sum . last . take 81 . iterate grow $ fish
+  where
+    ages = concat . integers $ input
+    fish = fromList . map (\x -> L.length . filter (== x) $ ages) $ [0 .. 6]
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ input = show . sum . last . take 257 . iterate grow $ fish
+  where
+    ages = concat . integers $ input
+    fish = fromList . map (\x -> L.length . filter (== x) $ ages) $ [0 .. 6]
