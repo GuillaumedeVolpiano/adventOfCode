29e75c95f23b19deda3c680aef554b115b03c5a2
Xavier Dectot <xavier.dectot@gmail.com>
2023-12-11 09:23:06 +0100

    2023, day11, optimised

--- 2023/days/Day11.hs
+++ 2023/days/Day11.hs
@@ -30,30 +30,42 @@ expandImage (s:xs)
   | all (== '.') s = s : s : expandImage xs
   | otherwise = s : expandImage xs
 
-shortestPath :: Image -> Int -> (Pos, Pos) -> Int
-shortestPath diagram factor (V2 x1 y1, V2 x2 y2) =
+shortestPath :: Image -> Int -> [Int] -> [Int] -> (Pos, Pos) -> Int
+shortestPath diagram factor expX expY (V2 x1 y1, V2 x2 y2) =
   abs (x1 - x2) + abs (y1 - y2) + (factor - 1) * expansion
   where
     expansion = countExpandableX + countExpandableY
-    (_, V2 mx my) = bounds diagram
     countExpandableX =
-      length . filter (all (== '.')) $
-      [[diagram ! V2 x y | y <- [0 .. my]] | x <- [(min x1 x2) .. (max x1 x2)]]
+      length . takeWhile (< max x1 x2) . takeWhile (> min x1 x2) $ expX
     countExpandableY =
-      length . filter (all (== '.')) $
-      [[diagram ! V2 x y | x <- [0 .. mx]] | y <- [(min y1 y2) .. (max y1 y2)]]
+      length . takeWhile (< max y1 y2) . takeWhile (> min y1 y2) $ expX
+
+expandablesX :: Image -> [Int]
+expandablesX image =
+  filter (\x -> all (== '.') [image ! V2 x y | y <- [0 .. my]]) [0 .. mx]
+  where
+    (_, V2 mx my) = bounds image
+
+expandablesY :: Image -> [Int]
+expandablesY image =
+  filter (\y -> all (== '.') [image ! V2 x y | x <- [0 .. mx]]) [0 .. my]
+  where
+    (_, V2 mx my) = bounds image
 
 part1 :: Bool -> String -> String
-part1 _ input = show . sum . map (shortestPath image 2) $ pairs
+part1 _ input = show . sum . map (shortestPath image 2 expX expY) $ pairs
   where
     image = arrayFromString input
     galaxies = filter (\p -> (image ! p) == '#') . indices $ image
     pairs =
       concat . zipWith (\a b -> map (a, ) b) (init galaxies) . tail $
       tails galaxies
+    expX = expandablesX image
+    expY = expandablesY image
 
 part2 :: Bool -> String -> String
-part2 test input = show . sum . map (shortestPath image factor) $ pairs
+part2 test input =
+  show . sum . map (shortestPath image factor expX expY) $ pairs
   where
     image = arrayFromString input
     galaxies = filter (\p -> (image ! p) == '#') . indices $ image
@@ -63,3 +75,5 @@ part2 test input = show . sum . map (shortestPath image factor) $ pairs
     factor
       | test = 10
       | otherwise = 10 ^ 6
+    expX = expandablesX image
+    expY = expandablesY image
