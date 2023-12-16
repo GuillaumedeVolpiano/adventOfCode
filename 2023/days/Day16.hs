f0d7a6c9d93f3b1fc8304bc20931bf08c911848d
Xavier Dectot <xavier.dectot@gmail.com>
2023-12-16 11:25:10 +0100

    2023, day16

--- 2023/days/Day16.hs
+++ 2023/days/Day16.hs
@@ -3,19 +3,25 @@ module Day16
   , part2
   ) where
 
-import           Data.Array.Unboxed as A (UArray, bounds, inRange, (!))
-import           Data.List          as L (map)
-import           Data.Map           as M (fromList, (!))
-import           Data.Set           as St (Set, empty, map, singleton, size)
-import           Helpers.Parsers    (arrayFromString)
-import           Helpers.Search     (dfs)
-import           Linear.V2          (V2 (..))
+import           Control.Parallel.Strategies (parMap, rpar)
+import           Data.Array.IArray           as I (Array, array, (!))
+import           Data.Array.Unboxed          as U (Ix, UArray, bounds, inRange,
+                                                   range, (!))
+import           Data.List                   as L (map)
+import           Data.Map                    as M (fromList, (!))
+import           Data.Set                    as St (Set, empty, map, singleton,
+                                                    size)
+import           Helpers.Parsers             (arrayFromString)
+import           Helpers.Search              (dfs)
+import           Linear.V2                   (V2 (..))
 
 type Pos = (V2 Int)
 
 type Cave = UArray Pos Char
 
-type Beam = (Pos, Pos)
+data Beam =
+  Beam Pos Pos
+  deriving (Show, Eq, Ord, Ix)
 
 type Energized = Set Pos
 
@@ -27,7 +33,7 @@ east = V2 1 0
 
 west = V2 (-1) 0
 
-startPos = (V2 0 0, east)
+startPos = Beam (V2 0 0) east
 
 reflections =
   fromList
@@ -43,31 +49,37 @@ reflections =
 
 edges :: Cave -> [Beam]
 edges cave =
-  [(V2 x 0, south) | x <- [0 .. mx]] ++
-  [(V2 x my, north) | x <- [0 .. mx]] ++
-  [(V2 0 y, east) | y <- [0 .. my]] ++ [(V2 mx y, west) | y <- [0 .. my]]
+  [Beam (V2 x 0) south | x <- [0 .. mx]] ++
+  [Beam (V2 x my) north | x <- [0 .. mx]] ++
+  [Beam (V2 0 y) east | y <- [0 .. my]] ++
+  [Beam (V2 mx y) west | y <- [0 .. my]]
   where
     (_, V2 mx my) = bounds cave
 
 neighbours :: Cave -> Beam -> [Beam]
-neighbours cave beam@(pos, dir) =
-  filter (\(t, _) -> inRange (bounds cave) t) newPos
+neighbours cave beam@(Beam pos dir) =
+  filter (\(Beam t _) -> inRange (bounds cave) t) newPos
   where
     newPos
-      | cave A.! pos == '.' ||
-          cave A.! pos == '−' && elem dir [east, west] ||
-          cave A.! pos == '|' && elem dir [north, south] = [(pos + dir, dir)]
-      | cave A.! pos == '-' = split [east, west]
-      | cave A.! pos == '|' = split [north, south]
+      | cave U.! pos == '.' ||
+          cave U.! pos == '−' && elem dir [east, west] ||
+          cave U.! pos == '|' && elem dir [north, south] =
+        [Beam (pos + dir) dir]
+      | cave U.! pos == '-' = split [east, west]
+      | cave U.! pos == '|' = split [north, south]
       | otherwise =
-        [ ( pos + reflections M.! (dir, cave A.! pos)
-          , reflections M.! (dir, cave A.! pos))
+        [ Beam
+            (pos + reflections M.! (dir, cave U.! pos))
+            (reflections M.! (dir, cave U.! pos))
         ]
-    split = L.map (\a -> (pos + a, a))
+    split = L.map (\a -> Beam (pos + a) a)
+
+toPos :: Beam -> Pos
+toPos (Beam p _) = p
 
 part1 :: Bool -> String -> String
 part1 _ input =
-  show . size . St.map fst . dfs [startPos] (neighbours cave) $ empty
+  show . size . St.map toPos . dfs [startPos] (neighbours cave) $ empty
   where
     cave = arrayFromString input
 
@@ -75,7 +87,17 @@ part2 :: Bool -> String -> String
 part2 _ input =
   show .
   maximum .
-  L.map (\x -> size . St.map fst . dfs [x] (neighbours cave) $ empty) . edges $
+  parMap rpar (\x -> size . St.map toPos . dfs [x] (allBeams I.!) $ empty) .
+  edges $
   cave
   where
     cave = arrayFromString input
+    (start, end@(V2 mx my)) = bounds cave
+    allBeams =
+      array
+        (Beam start (V2 (-1) (-1)), Beam end (V2 1 1))
+        [ (Beam (V2 x y) dir, neighbours cave (Beam (V2 x y) dir))
+        | x <- [0 .. mx]
+        , y <- [0 .. my]
+        , dir <- [north, south, east, west]
+        ] :: Array Beam [Beam]
