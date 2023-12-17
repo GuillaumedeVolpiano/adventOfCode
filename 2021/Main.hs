d21725e9be29d036c0b8073b5edaf8656dc02904
Xavier Dectot <xavier.dectot@gmail.com>
2023-12-17 18:25:52 +0100

    2021, day 7

--- 2021/Main.hs
+++ 2021/Main.hs
@@ -34,15 +34,16 @@ import           Day7
 import           Day8
 import           Day9
 import           Helpers.General                 (customPreciseTimeIt,
-                                                  retrieveInput)
+                                                  retrieveInput, wallTimeIt)
 import           System.Console.CmdArgs.Implicit (Data, Typeable, cmdArgs, def,
                                                   help, opt, (&=))
 
 data Arguments =
   Arguments
-    { day   :: Int
-    , test  :: Bool
-    , proxy :: Bool
+    { day      :: Int
+    , test     :: Bool
+    , proxy    :: Bool
+    , wallTime :: Bool
     }
   deriving (Show, Data, Typeable)
 
@@ -51,28 +52,28 @@ solver =
     [ (1, (Day1.part1, Day1.part2))
     , (2, (Day2.part1, Day2.part2))
     , (3, (Day3.part1, Day3.part2))
-   , (4, (Day4.part1, Day4.part2))
-   , (5, (Day5.part1, Day5.part2))
-   , (6, (Day6.part1, Day6.part2))
-   , (7, (Day7.part1, Day7.part2))
-   , (8, (Day8.part1, Day8.part2))
-   , (9, (Day9.part1, Day9.part2))
-   , (10, (Day10.part1, Day10.part2))
-   , (11, (Day11.part1, Day11.part2))
-   , (12, (Day12.part1, Day12.part2))
-   , (13, (Day13.part1, Day13.part2))
-   , (14, (Day14.part1, Day14.part2))
-   , (15, (Day15.part1, Day15.part2))
-   , (16, (Day16.part1, Day16.part2))
-   , (17, (Day17.part1, Day17.part2))
-   , (18, (Day18.part1, Day18.part2))
-   , (19, (Day19.part1, Day19.part2))
-   , (20, (Day20.part1, Day20.part2))
-   , (21, (Day21.part1, Day21.part2))
-   , (22, (Day22.part1, Day22.part2))
-   , (23, (Day23.part1, Day23.part2))
-   , (24, (Day24.part1, Day24.part2))
-   , (25, (Day25.part1, Day25.part2))
+    , (4, (Day4.part1, Day4.part2))
+    , (5, (Day5.part1, Day5.part2))
+    , (6, (Day6.part1, Day6.part2))
+    , (7, (Day7.part1, Day7.part2))
+    , (8, (Day8.part1, Day8.part2))
+    , (9, (Day9.part1, Day9.part2))
+    , (10, (Day10.part1, Day10.part2))
+    , (11, (Day11.part1, Day11.part2))
+    , (12, (Day12.part1, Day12.part2))
+    , (13, (Day13.part1, Day13.part2))
+    , (14, (Day14.part1, Day14.part2))
+    , (15, (Day15.part1, Day15.part2))
+    , (16, (Day16.part1, Day16.part2))
+    , (17, (Day17.part1, Day17.part2))
+    , (18, (Day18.part1, Day18.part2))
+    , (19, (Day19.part1, Day19.part2))
+    , (20, (Day20.part1, Day20.part2))
+    , (21, (Day21.part1, Day21.part2))
+    , (22, (Day22.part1, Day22.part2))
+    , (23, (Day23.part1, Day23.part2))
+    , (24, (Day24.part1, Day24.part2))
+    , (25, (Day25.part1, Day25.part2))
     ]
 
 main :: IO ()
@@ -86,6 +87,7 @@ main = do
               opt curDay
           , test = def &= help "Run the test suite? Defaults to True if used"
           , proxy = def &= help "Use a proxy? Defaults to True if used"
+          , wallTime = def &= help "Report wall Time rather than CPU time"
           }
   args <- cmdArgs arguments
   let year = 2021
@@ -95,5 +97,9 @@ main = do
           _ -> day args
   input <- retrieveInput year theDay (test args) (proxy args)
   let (solve1, solve2) = solver ! theDay
-  customPreciseTimeIt "Part 1. CPU Time" 4 . putStrLn $ solve1 (test args) input
-  customPreciseTimeIt "Part 2. CPU Time" 4 . putStrLn $ solve2 (test args) input
+  let timer =
+        if wallTime args
+          then wallTimeIt
+          else customPreciseTimeIt
+  timer "Part 1." 4 . putStrLn $ solve1 (test args) input
+  timer "Part 2." 4 . putStrLn $ solve2 (test args) input
--- 2021/days/Day7.hs
+++ 2021/days/Day7.hs
@@ -1,8 +1,37 @@
-module Day7 (part1, part2) where
-import Helpers.Parsers
+module Day7
+  ( part1
+  , part2
+  ) where
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+import           Data.List       (sort)
+import           Helpers.Parsers (integers)
+
+fuelCostSimple :: Int -> [Int] -> Int
+fuelCostSimple point = sum . map (\x -> abs (point - x))
+
+fuelCostCumulative :: Int -> [Int] -> Int
+fuelCostCumulative point = sum . map (\x -> arSum . abs $ (point - x))
+  where
+    arSum n = div (n * (n + 1)) 2
+
+dichotomy :: [Int] -> (Int -> [Int] -> Int) -> Int -> Int -> Int
+dichotomy crabs fuelFormula mini maxi
+  | maxi - mini == 1 = min (fuelFormula maxi crabs) (fuelFormula mini crabs)
+  | fuelFormula maxi crabs < fuelFormula mini crabs =
+    dichotomy crabs fuelFormula midPoint maxi
+  | fuelFormula mini crabs < fuelFormula maxi crabs =
+    dichotomy crabs fuelFormula mini midPoint
+  where
+    midPoint = div (maxi + mini) 2
+
+part1 :: Bool -> String -> String
+part1 _ input =
+  show . dichotomy crabs fuelCostSimple (minimum crabs) $ maximum crabs
+  where
+    crabs = concat . integers $ input
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ input =
+  show . dichotomy crabs fuelCostCumulative (minimum crabs) $ maximum crabs
+  where
+    crabs = concat . integers $ input
