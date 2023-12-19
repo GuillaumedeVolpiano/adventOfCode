33d8d802cb0d5a282c9b770e84ec1c4ef353aa90
Xavier Dectot <xavier.dectot@gmail.com>
2023-12-19 16:04:52 +0100

    optimised classes etc.

--- 2023/days/Day19.hs
+++ 2023/days/Day19.hs
@@ -7,200 +7,149 @@ module Day19
 
 import           Data.List.Split (splitWhen)
 import           Data.Map        as M (Map, fromList, (!))
-import           Data.Maybe      (Maybe (Just, Nothing), catMaybes, isNothing)
+import           Data.Maybe      (Maybe (Just, Nothing), catMaybes, fromJust,
+                                  isNothing)
 import           Text.Regex.TDFA (getAllTextMatches, (=~))
 
-import           Debug.Trace
-
-data Part =
+-- data types
+data Part a =
   Part
-    { x :: Int
-    , m :: Int
-    , a :: Int
-    , s :: Int
+    { x :: Maybe a
+    , m :: Maybe a
+    , a :: Maybe a
+    , s :: Maybe a
     }
-  deriving (Show)
 
-data PartRange =
-  PartRange
-    { xRange :: Range
-    , mRange :: Range
-    , aRange :: Range
-    , sRange :: Range
-    }
-  deriving (Show)
+data Range =
+  Range Int Int
 
-type Range = Maybe (Int, Int)
-
+-- types
 type Workflow = String
 
-type Condition = (Part -> Bool)
+type Condition a = (Part a -> (Part a, Part a))
 
-type RangeCondition = (PartRange -> (PartRange, PartRange))
+type System a = Map Workflow [(Condition a, Workflow)]
 
-type System = Map Workflow [(Condition, Workflow)]
+type Accepted a = [Part a]
 
-type SystemRange = Map Workflow [(RangeCondition, Workflow)]
+-- classes
+class Parseable a where
+  threeParse :: String -> Condition a
+  size :: Part a -> Int
 
-type Accepted = [Part]
+-- instances
+instance Parseable Int where
+  threeParse s = condition
+    where
+      (a, b, c) = s =~ "[<>]"
+      condition part
+        | (toComp ! b) (fromJust . (toCat ! a) $ part) (read c) =
+          (part, emptyPart)
+        | otherwise = (emptyPart, part)
+  size p
+    | empty p = 0
+  size (Part (Just x) (Just m) (Just a) (Just s)) = x + m + a + s
 
-type AcceptedRange = [PartRange]
+instance Parseable Range where
+  threeParse s = lSplit a b (read c)
+    where
+      (a, b, c) = s =~ "[<>]"
+  size p
+    | empty p = 0
+  size (Part (Just x) (Just m) (Just a) (Just s)) =
+    rangeSize x * rangeSize m * rangeSize a * rangeSize s
 
+-- global variables
 toCat = M.fromList [("x", x), ("m", m), ("a", a), ("s", s)]
 
 toComp = M.fromList [("<", (<)), (">", (>))]
 
-range = Just (1, 4000)
+range = Just (Range 1 4000)
 
-initialRange = PartRange range range range range
+initialRange = Part range range range range
 
-emptyRange = PartRange Nothing Nothing Nothing Nothing
+emptyPart = Part Nothing Nothing Nothing Nothing
 
-empty :: PartRange -> Bool
-empty (PartRange a b c d) =
-  isNothing a || isNothing b || isNothing c || isNothing d
+-- helper functions for Parseable
+lSplit :: String -> String -> Int -> Part Range -> (Part Range, Part Range)
+lSplit cat comp val part
+  | cat == "x" = (part {x = yr}, part {x = nr})
+  | cat == "m" = (part {m = yr}, part {m = nr})
+  | cat == "a" = (part {a = yr}, part {a = nr})
+  | cat == "s" = (part {s = yr}, part {s = nr})
+  where
+    (yr, nr) = split comp val $ (toCat ! cat) part
 
-toDicTuple :: [String] -> (Workflow, [(Condition, Workflow)])
+split :: String -> Int -> Maybe Range -> (Maybe Range, Maybe Range)
+split _ _ Nothing = (Nothing, Nothing)
+split comp val (Just (Range minv maxv))
+  | order minv val && order maxv val = (Just (Range minv maxv), Nothing)
+  | order minv val = (Just (Range minv (val - 1)), Just (Range val maxv))
+  | order maxv val = (Just (Range (val + 1) maxv), Just (Range minv val))
+  | otherwise = (Nothing, Just (Range minv maxv))
+  where
+    order = toComp ! comp
+
+rangeSize :: Range -> Int
+rangeSize (Range a b) = b - a + 1
+
+-- Main body
+empty :: Part a -> Bool
+empty (Part a b c d) = isNothing a || isNothing b || isNothing c || isNothing d
+
+toDicTuple :: (Parseable a) => [String] -> (Workflow, [(Condition a, Workflow)])
 toDicTuple (s:ss) = (s, conditionParse ss)
 
-toRangeDicTuple :: [String] -> (Workflow, [(RangeCondition, Workflow)])
-toRangeDicTuple (s:ss) = (s, rangeConditionParse ss)
-
-conditionParse :: [String] -> [(Condition, Workflow)]
+conditionParse :: (Parseable a) => [String] -> [(Condition a, Workflow)]
 conditionParse (a:as)
-  | null as = [(const True, a)]
+  | null as = [((, emptyPart), a)]
 conditionParse (a:b:as) = (threeParse a, b) : conditionParse as
 
-rangeConditionParse :: [String] -> [(RangeCondition, Workflow)]
-rangeConditionParse (a:as)
-  | null as = [((, emptyRange), a)]
-rangeConditionParse (a:b:as) = (rangeThreeParse a, b) : rangeConditionParse as
-
-threeParse :: String -> (Part -> Bool)
-threeParse s = \part -> (toComp ! b) ((toCat ! a) part) (read c)
-  where
-    (a, b, c) = s =~ "[<>]"
-
-rangeThreeParse :: String -> RangeCondition
-rangeThreeParse s = newPartRange
-  where
-    (a, b, c) = s =~ "[<>]"
-    val = read c
-    newPartRange
-      | a == "x" = xSplit b val
-      | a == "m" = mSplit b val
-      | a == "a" = aSplit b val
-      | a == "s" = sSplit b val
-
-xSplit :: String -> Int -> PartRange -> (PartRange, PartRange)
-xSplit comp val (PartRange xr mr ar sr) =
-  (PartRange yr mr ar sr, PartRange nr mr ar sr)
-  where
-    (yr, nr) = split comp val xr
-
-mSplit :: String -> Int -> PartRange -> (PartRange, PartRange)
-mSplit comp val (PartRange xr mr ar sr) =
-  (PartRange xr yr ar sr, PartRange xr nr ar sr)
-  where
-    (yr, nr) = split comp val mr
-
-aSplit :: String -> Int -> PartRange -> (PartRange, PartRange)
-aSplit comp val (PartRange xr mr ar sr) =
-  (PartRange xr mr yr sr, PartRange xr mr nr sr)
-  where
-    (yr, nr) = split comp val ar
-
-sSplit :: String -> Int -> PartRange -> (PartRange, PartRange)
-sSplit comp val (PartRange xr mr ar sr) =
-  (PartRange xr mr ar yr, PartRange xr mr ar nr)
-  where
-    (yr, nr) = split comp val sr
-
-split ::
-     String -> Int -> Maybe (Int, Int) -> (Maybe (Int, Int), Maybe (Int, Int))
-split comp val (Just (minv, maxv))
-  | comp == "<" && minv < val && maxv < val = (Just (minv, maxv), Nothing)
-  | comp == "<" && minv < val = (Just (minv, val - 1), Just (val, maxv))
-  | comp == "<" = (Nothing, Just (minv, maxv))
-  | minv > val && maxv > val = (Just (minv, maxv), Nothing)
-  | maxv > val = (Just (val + 1, maxv), Just (minv, val))
-  | otherwise = (Nothing, Just (minv, maxv))
-
-makePart :: [String] -> Part
-makePart s = Part a b c d
+makePart :: [String] -> Part Int
+makePart s = Part (Just a) (Just b) (Just c) (Just d)
   where
     [a, b, c, d] = map (\x -> read (x =~ "[0-9]+")) s
 
 toTuple :: [[[String]]] -> ([[String]], [[String]])
 toTuple [a, b] = (a, b)
 
-inputParser :: ([[String]], [[String]]) -> (System, [Part])
+inputParser :: ([[String]], [[String]]) -> (System Int, [Part Int])
 inputParser (a, b) = (fromList . map toDicTuple $ a, map makePart b)
 
-rangeParser :: [[String]] -> SystemRange
-rangeParser = fromList . map toRangeDicTuple
+rangeParser :: [[String]] -> (System Range, [Part Range])
+rangeParser a = (fromList . map toDicTuple $ a, [initialRange])
 
-processAll :: Accepted -> (System, [Part]) -> Accepted
+processAll :: Accepted a -> (System a, [Part a]) -> Accepted a
 processAll proc (system, []) = proc
 processAll proc (system, p:ps) =
   processAll (process proc system p "in") (system, ps)
 
-process :: Accepted -> System -> Part -> Workflow -> Accepted
-process proc system part wf
-  | processed == "A" = part : proc
-  | processed == "R" = proc
-  | otherwise = process proc system part processed
-  where
-    processed = processOne part $ system ! wf
-
-processOne :: Part -> [(Condition, Workflow)] -> Workflow
-processOne p ((condition, workflow):xs)
-  | condition p = workflow
-  | otherwise = processOne p xs
-
-score :: Part -> Int
-score (Part x m a s) = x + m + a + s
-
-processAllRange :: AcceptedRange -> SystemRange -> AcceptedRange
-processAllRange ar system = processRange ar system initialRange "in"
-
-processRange ::
-     AcceptedRange -> SystemRange -> PartRange -> Workflow -> AcceptedRange
-processRange accepted system partRange workflow = finalAccepted
+process :: Accepted a -> System a -> Part a -> Workflow -> Accepted a
+process accepted system part workflow = finalAccepted
   where
     processes = system ! workflow
     processProcesses (ar, pr, tp) p =
-      let (na, processed, unprocessed) = processOneRange ar tp p
+      let (na, processed, unprocessed) = processOne ar tp p
        in (na, processed : pr, unprocessed)
     (intAccepted, rawToProcess, _) =
-      foldl processProcesses (accepted, [], Just partRange) processes
+      foldl processProcesses (accepted, [], Just part) processes
     stillToProcess = catMaybes rawToProcess
     finalAccepted =
-      foldl
-        (\a b -> uncurry (processRange a system) b)
-        intAccepted
-        stillToProcess
+      foldl (\a b -> uncurry (process a system) b) intAccepted stillToProcess
 
-partRangeSize :: PartRange -> Int
-partRangeSize (PartRange xr mr ar sr) = size xr * size mr * size ar * size sr
-
-size :: Maybe (Int, Int) -> Int
-size Nothing       = 0
-size (Just (a, b)) = b - a + 1
-
-processOneRange ::
-     AcceptedRange
-  -> Maybe PartRange
-  -> (RangeCondition, Workflow)
-  -> (AcceptedRange, Maybe (PartRange, Workflow), Maybe PartRange)
-processOneRange accepted Nothing _ = (accepted, Nothing, Nothing)
-processOneRange accepted (Just partRange) (condition, workflow)
+processOne ::
+     Accepted a
+  -> Maybe (Part a)
+  -> (Condition a, Workflow)
+  -> (Accepted a, Maybe (Part a, Workflow), Maybe (Part a))
+processOne accepted Nothing _ = (accepted, Nothing, Nothing)
+processOne accepted (Just part) (condition, workflow)
   | empty processed || workflow == "R" =
     (accepted, Nothing, testEmpty unprocessed)
   | workflow == "A" = (processed : accepted, Nothing, testEmpty unprocessed)
   | otherwise = (accepted, Just (processed, workflow), testEmpty unprocessed)
   where
-    (processed, unprocessed) = condition partRange
+    (processed, unprocessed) = condition part
     testEmpty pr
       | empty pr = Nothing
       | otherwise = Just pr
@@ -209,7 +158,7 @@ part1 :: Bool -> String -> String
 part1 _ =
   show .
   sum .
-  map score .
+  map size .
   processAll [] .
   inputParser .
   toTuple .
@@ -220,8 +169,8 @@ part2 :: Bool -> String -> String
 part2 _ =
   show .
   sum .
-  map partRangeSize .
-  processAllRange [] .
+  map size .
+  processAll [] .
   rangeParser .
   head .
   splitWhen null .
