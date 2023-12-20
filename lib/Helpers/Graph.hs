7f653976959b7de7b56ab4de2b557ad35ff46ba4
Xavier Dectot <xavier.dectot@gmail.com>
2023-12-20 19:51:51 +0100

    finished graphviz helper

--- lib/Helpers/Graph.hs
+++ lib/Helpers/Graph.hs
@@ -1,23 +1,27 @@
 {-# LANGUAGE TupleSections #-}
 
-module Helpers.Graph (dicToGraph, graphToViz) where
+module Helpers.Graph
+  ( dicToGraph
+  , graphToViz
+  ) where
 
 import           Data.Graph.Inductive.Graph        (Graph, Node, mkGraph)
 import           Data.Graph.Inductive.PatriciaTree (Gr)
-import           Data.GraphViz                     (DotGraph, graphToDot,
-                                                    nonClusteredParams)
+import           Data.GraphViz                     (DotGraph, Labellable,
+                                                    graphToDot, quickParams, printDotGraph)
 import           Data.Map                          (Map, assocs, fromList, keys,
                                                     (!))
+import Data.Text.Lazy (Text)
 
-dicToGraph :: (Ord a) => Map a [a] -> Gr a (a, a)
+dicToGraph :: Map String [String] -> Gr String String
 dicToGraph dic = mkGraph nodes edges
   where
     nodes = zip [0 ..] . keys $ dic
     edges =
-      map (\(a, b) -> (labelToNode ! a, labelToNode ! b, (a, b))) .
+      map (\(a, b) -> (labelToNode ! a, labelToNode ! b, a ++ " " ++ b)) .
       concatMap (\(a, b) -> map (a, ) b) . assocs $
       dic
     labelToNode = fromList . map (\(a, b) -> (b, a)) $ nodes
 
-graphToViz :: Gr a (a, a) -> DotGraph Node
-graphToViz = graphToDot nonClusteredParams
+graphToViz :: Gr String String -> Text
+graphToViz = printDotGraph . graphToDot quickParams
