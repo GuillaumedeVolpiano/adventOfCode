5e527a694765cf47813d9148c68d6d7bd8385439
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-03 22:16:10 +0100

    2020, day 2

--- 2020/days/Day2.hs
+++ 2020/days/Day2.hs
@@ -1,9 +1,31 @@
-module Day2 (part1, part2) where
+module Day2
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Helpers.Parsers (alpha, complexParser, nums)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+validate :: [String] -> Bool
+validate [a, b, [c], d] = length pruned <= read b && length pruned >= read a
+  where
+    pruned = filter (== c) d
+
+otherValidate :: [String] -> Bool
+otherValidate [a, b, [c], d] = (da || db) && not (da && db)
+  where
+    da = d !! (read a - 1) == c
+    db = d !! (read b - 1) == c
+
+part1 :: Bool -> String -> String
+part1 _ =
+  show .
+  length .
+  filter validate .
+  map concat . complexParser ["-", " ", ": "] [nums, nums, alpha, alpha]
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ =
+  show .
+  length .
+  filter otherValidate .
+  map concat . complexParser ["-", " ", ": "] [nums, nums, alpha, alpha]
