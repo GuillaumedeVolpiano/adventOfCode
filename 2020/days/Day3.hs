ec3dfd7ca50177a827f5e06a4fb7c5d78961d173
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-04 13:03:20 +0100

    2023, day1 ported to Megaparsec

--- 2020/days/Day3.hs
+++ 2020/days/Day3.hs
@@ -1,8 +1,15 @@
-module Day3 (part1, part2) where
+module Day3
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.Array.Unboxed (UArray, bounds, (!))
+import           Helpers.Parsers    (arrayFromString)
+import           Linear.V2          (V2 (..))
 
-part1 :: Bool -> String -> String
+type Pos = V2 Int
+
+part1 :: Bool -> String -> String
 part1 _ _ = "Part 1"
 
 part2 :: Bool -> String -> String
--- 2023/days/Day1.hs
+++ 2023/days/Day1.hs
@@ -1,8 +1,24 @@
 {-# LANGUAGE PackageImports #-}
-module Day1 (part1, part2) where
+
+module Day1
+  ( part1
+  , part2
+  ) where
+
 import           "regex-pcre" Text.Regex.PCRE (getAllTextMatches, (=~))
 
+import           Data.Char                    (digitToInt, isAlpha, isDigit)
+import           Data.Either                  (fromRight)
 import           Data.Map                     (fromList, (!))
+import           Data.Maybe                   (catMaybes)
+import           Data.Void                    (Void)
+import           Text.Megaparsec              (Parsec, eof, lookAhead, manyTill,
+                                               optional, parse, takeWhileP, try,
+                                               (<|>))
+import           Text.Megaparsec.Char         (char, digitChar, eol, lowerChar,
+                                               string)
+
+type Parser = Parsec Void String
 
 specialRead =
   fromList
@@ -26,20 +42,112 @@ specialRead =
     , ("9", 9)
     ]
 
-parseLine :: String -> String -> [String]
-parseLine p s = getAllTextMatches (s =~ p)
+parseInteger :: Parser (Maybe Int)
+parseInteger = do
+  _ <- takeWhileP Nothing (not . tester)
+  Just . digitToInt <$> digitChar
 
-firstLast :: [String] -> Int
-firstLast a = 10 * (specialRead ! head a) + specialRead ! last a
+parseOne :: Parser (Maybe Int)
+parseOne = do
+  _ <- takeWhileP Nothing (not . tester)
+  _ <- string "on"
+  lookAhead (char 'e')
+  return (Just 1)
+
+parseTwo :: Parser (Maybe Int)
+parseTwo = do
+  _ <- takeWhileP Nothing (not . tester)
+  _ <- string "tw"
+  lookAhead (char 'o')
+  return (Just 2)
+
+parseThree :: Parser (Maybe Int)
+parseThree = do
+  _ <- takeWhileP Nothing (not . tester)
+  _ <- string "thre"
+  lookAhead (char 'e')
+  return (Just 3)
+
+parseFour :: Parser (Maybe Int)
+parseFour = do
+  _ <- takeWhileP Nothing (not . tester)
+  _ <- string "four"
+  return (Just 4)
+
+parseFive :: Parser (Maybe Int)
+parseFive = do
+  _ <- takeWhileP Nothing (not . tester)
+  _ <- string "fiv"
+  lookAhead (char 'e')
+  return (Just 5)
+
+parseSix :: Parser (Maybe Int)
+parseSix = do
+  _ <- takeWhileP Nothing (not . tester)
+  _ <- string "six"
+  return (Just 6)
+
+parseSeven :: Parser (Maybe Int)
+parseSeven = do
+  _ <- takeWhileP Nothing (not . tester)
+  _ <- string "eve"
+  lookAhead (char 'n')
+  return (Just 7)
+
+parseEight :: Parser (Maybe Int)
+parseEight = do
+  _ <- takeWhileP Nothing (not . tester)
+  _ <- string "eigh"
+  lookAhead (char 't')
+  return (Just 8)
+
+parseNine :: Parser (Maybe Int)
+parseNine = do
+  _ <- takeWhileP Nothing (not . tester)
+  _ <- string "nin"
+  lookAhead (char 'e')
+  return (Just 9)
+
+tester :: Char -> Bool
+tester c =
+  isDigit c ||
+  c == 'o' ||
+  c == 't' || c == 'f' || c == 's' || c == 'e' || c == 'n' || c == '\n'
+
+consume :: Parser (Maybe Int)
+consume = do
+  _ <- lowerChar
+  return Nothing
+
+simpleParser :: Parser (Maybe Int)
+simpleParser = try parseInteger <|> consume
+
+complexParser :: Parser (Maybe Int)
+complexParser =
+  try parseOne <|> try parseTwo <|> try parseThree <|> try parseFour <|>
+  try parseFive <|>
+  try parseSix <|>
+  try parseSeven <|>
+  try parseEight <|>
+  try parseNine <|>
+  try parseInteger <|>
+  consume
+
+parseLine :: Parser (Maybe Int) -> Parser [Int]
+parseLine parser = catMaybes <$> manyTill parser eol
+
+parseInput :: Parser (Maybe Int) -> Parser [[Int]]
+parseInput parser = manyTill (parseLine parser) eof
+
+firstLast :: [Int] -> Int
+firstLast [] = 0
+firstLast a  = 10 * head a + last a
 
 part1 :: Bool -> String -> String
-part1 _ = show . sum . map (firstLast . parseLine "[1-9]") . lines
+part1 _ =
+  show . sum . map firstLast . fromRight [] . parse (parseInput simpleParser) ""
 
-part2 :: Bool -> String -> String
-part2 _ = show . 
-    sum .
-    map
-      (firstLast .
-       parseLine
-         "([0-9]|on(?=e)|tw(?=o)|thre(?=e)|four|fiv(?=e)|six|seve(?=n)|eigh(?=t)|nin(?=e))") .
-    lines
+part2 :: Bool -> String -> String
+part2 _ =
+  show .
+  sum . map firstLast . fromRight [] . parse (parseInput complexParser) ""
