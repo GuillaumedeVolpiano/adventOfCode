b1d29369d7ed0efd83d6f7a95e34419d838b07f4
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-04 20:43:01 +0100

    2020, day 3

--- 2020/days/Day3.hs
+++ 2020/days/Day3.hs
@@ -4,13 +4,34 @@ module Day3
   ) where
 
 import           Data.Array.Unboxed (UArray, bounds, (!))
+import           Data.List          (unfoldr)
 import           Helpers.Parsers    (arrayFromString)
 import           Linear.V2          (V2 (..))
 
 type Pos = V2 Int
 
+type Toboggan = UArray Pos Char
+
+findPos :: Int -> Pos -> Pos -> Maybe (Pos, Pos)
+findPos int dp p@(V2 _ y)
+  | y > int = Nothing
+  | otherwise = Just (p, p + dp)
+
+allTrees :: Pos -> Toboggan -> Int
+allTrees dp toboggan =
+  length .
+  filter (== '#') . map (\x -> toboggan ! vMod x) . unfoldr (findPos my dp) $
+  V2 0 0
+  where
+    vMod (V2 x y) = V2 (mod x (mx + 1)) y
+    (_, V2 mx my) = bounds toboggan
+
 part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+part1 _ = show . allTrees (V2 3 1) . arrayFromString
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ input =
+  show . product . map (`allTrees` toboggan) $
+  [V2 1 1, V2 3 1, V2 5 1, V2 7 1, V2 1 2]
+  where
+    toboggan = arrayFromString input
