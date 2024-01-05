292fe55b02024cfe0adbc4f6b422acf5db08afda
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-05 22:34:57 +0100

    2020, day 10

--- 2020/days/Day10.hs
+++ 2020/days/Day10.hs
@@ -1,9 +1,51 @@
-module Day10 (part1, part2) where
+module Day10
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.List  (group, sort)
+import           Data.Maybe (Maybe (Just, Nothing), catMaybes, fromJust,
+                             isNothing)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+countDifs :: [Int] -> Int
+countDifs adapters =
+  (\[a, b] -> (a + 1) * (b + 1)) .
+  map length . group . sort . zipWith (flip (-)) (init sorted) $
+  tail sorted
+  where
+    sorted = sort adapters
+
+combinations :: [Int] -> Int -> Maybe Int
+combinations _ (-2) = Just 0
+combinations _ (-1) = Just 0
+combinations _ 0 = Just 1
+combinations list n
+  | n `elem` list && all isNothing combined = Nothing
+  | n `elem` list = Just . sum . catMaybes $ combined
+  | otherwise = Nothing
+  where
+    combined = map (combinations list . (n -)) [1 .. 3]
+
+memCombinations :: [Int] -> Int -> Maybe Int
+memCombinations list = (map comb [0 ..] !!)
+  where
+    comb 0 = Just 0
+    comb 1 = combinations list 1
+    comb 2 = combinations list 2
+    comb n
+      | n `elem` list && all isNothing (combined n) = Nothing
+      | n `elem` list = Just . sum . catMaybes $ combined n
+      | otherwise = Nothing
+    combined n = map (memCombinations list . (n -)) [1 .. 3]
+
+findCombinations :: [Int] -> Int
+findCombinations list = fromJust $ memCombinations sorted device
+  where
+    sorted = sort list
+    device = last sorted
+
+part1 :: Bool -> String -> String
+part1 _ = show . countDifs . map read . lines
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . findCombinations . map read . lines
--- 2020/days/Day9.hs
+++ 2020/days/Day9.hs
@@ -1,9 +1,75 @@
-module Day9 (part1, part2) where
+module Day9
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Control.Monad.State.Lazy (State, evalState, get, put)
+import           Data.List                as L (drop, inits, tails)
+import           Data.Maybe               (Maybe (Nothing), fromJust, isJust,
+                                           isNothing)
+import           Data.Sequence            as S (Seq ((:<|), (:|>)), drop,
+                                                elemIndexL, fromList, (><))
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+type Preamble = Seq Int
+
+type Accum = Seq Int
+
+type XMAS = State (Preamble, Accum, [Int], Int) [Maybe Int]
+
+buildState :: Int -> [Int] -> (Preamble, Accum, [Int], Int)
+buildState pl s = (preamble, accum, L.drop pl s, pl)
+  where
+    rawPreamble = take pl s
+    preamble = fromList rawPreamble
+    accum =
+      fromList . concatMap (\x -> map (x +) . filter (/= x) $ rawPreamble) $
+      rawPreamble
+
+findFirstWeakness :: XMAS
+findFirstWeakness = do
+  (preamble, accum, list, pl) <- get
+  let (l:ist) = list
+      p :<| reamble = preamble
+      newAccum = S.drop (pl - 1) accum >< fmap (l +) reamble
+      rest = evalState findFirstWeakness (reamble :|> l, newAccum, ist, pl)
+      result
+        | null list = []
+        | isNothing (elemIndexL l accum) = Just l : rest
+        | otherwise = Nothing : rest
+  return result
+
+decryptWeakness :: Bool -> [Int] -> Int
+decryptWeakness test xmas = maximum results + minimum results
+  where
+    target =
+      fromJust .
+      head . filter isJust . evalState findFirstWeakness . buildState pl $
+      xmas
+    toTest = tails xmas
+    results =
+      head .
+      concatMap
+        (filter (\x -> length x > 1 && sum x == target) .
+         takeWhile (\x -> sum x <= target) . inits) $
+      toTest
+    pl
+      | test = 5
+      | otherwise = 25
+
+part1 :: Bool -> String -> String
+part1 test =
+  show .
+  fromJust .
+  head .
+  filter isJust . evalState findFirstWeakness . buildState pl . map read . lines
+  where
+    pl
+      | test = 5
+      | otherwise = 25
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 test = show . decryptWeakness test . map read . lines
+  where
+    pl
+      | test = 5
+      | otherwise = 25
