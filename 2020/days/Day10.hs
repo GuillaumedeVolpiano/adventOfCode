48042f162b0f953358165b461d0b5593d4d46b78
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-05 22:54:34 +0100

    2020, day 10, performance tweak

--- 2020/days/Day10.hs
+++ 2020/days/Day10.hs
@@ -4,6 +4,7 @@ module Day10
   ) where
 
 import           Data.List (group, sort)
+import           Data.Set  (Set, fromList, member)
 
 countDifs :: [Int] -> Int
 countDifs adapters =
@@ -13,21 +14,21 @@ countDifs adapters =
   where
     sorted = sort adapters
 
-combinations :: [Int] -> Int -> Int
+combinations :: Set Int -> Int -> Int
 combinations list = (map comb [0 ..] !!)
   where
     comb 0 = 1
     comb 1
-      | 1 `elem` list = comb 0
+      | 1 `member` list = comb 0
     comb 2
-      | 2 `elem` list = comb 1 + comb 0
+      | 2 `member` list = comb 1 + comb 0
     comb n
-      | n `elem` list = sum . combined $ n
+      | n `member` list = sum . combined $ n
       | otherwise = 0
     combined n = map (combinations list . (n -)) [1 .. 3]
 
 findCombinations :: [Int] -> Int
-findCombinations list = combinations sorted device
+findCombinations list = combinations (fromList sorted) device
   where
     sorted = sort list
     device = last sorted
