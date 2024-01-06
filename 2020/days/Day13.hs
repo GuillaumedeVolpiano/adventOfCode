1ef3768fbc721bd2ffe7d21ec0e2be053081fefd
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-06 18:46:42 +0100

    2021, day 13

--- 2020/days/Day13.hs
+++ 2020/days/Day13.hs
@@ -1,9 +1,31 @@
-module Day13 (part1, part2) where
+module Day13
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.Bifunctor                   (second)
+import           Data.List                        (minimumBy)
+import           Data.List.Split                  (splitOn)
+import           Data.Maybe                       (fromJust)
+import           Math.NumberTheory.Moduli.Chinese (chinese)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+findBus :: [[Int]] -> Int
+findBus [[timestamp], timetable] =
+  (\x -> x * (x - mod timestamp x)) .
+  minimumBy (\a b -> compare (a - mod timestamp a) (b - mod timestamp b)) $
+  timetable
+
+findTimeStamp :: [(Integer, Integer)] -> Integer
+findTimeStamp (h:ead) =
+  (\(a, b) -> mod (b - a) b) . foldl (\a -> fromJust . chinese a) h $ ead
+
+part1 :: Bool -> String -> String
+part1 _ =
+  show . findBus . map (map read . filter (/= "x") . splitOn ",") . lines
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ =
+  show .
+  findTimeStamp .
+  map (second read) .
+  filter ((/= "x") . snd) . zip [0 ..] . splitOn "," . last . lines
