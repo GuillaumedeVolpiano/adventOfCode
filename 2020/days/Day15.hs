a99a17036b80564b7c1c487070001125674e7f0b
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-07 15:57:19 +0100

    2020, day 18

--- 2020/days/Day15.hs
+++ 2020/days/Day15.hs
@@ -15,8 +15,10 @@ type Val = Int
 
 type State = (Turn, Val, Game)
 
-doRound :: State -> State 
-doRound (turn, val, game) = (turn + 1, newVal, insert val turn game)
+doNRounds :: Int -> State -> Int 
+doNRounds nturns (turn, val, game) 
+    | nturns == turn = val
+    | otherwise = doNRounds nturns (turn + 1, newVal, insert val turn game)
   where
     newVal
       | isNothing lastSaid = 0
@@ -29,19 +31,16 @@ createGame prelude = (turn, val, game)
     game = fromList . init $ prelude
     (val, turn) = last prelude
 
-afterN :: Int -> [(Int, Int)] -> Int
-afterN nturns list = (\(_, r, _) -> r) . last . take (nturns - length list + 1) . iterate doRound . createGame $ list
-
 part1 :: Bool -> String -> String
 part1 _ =
   show .
-  afterN 2020 .
+  doNRounds 2020 . createGame .
   zipWith (\a b -> (b, a)) [1 ..] .
   map Prelude.read . splitOn "," . filter (/= '\n')
 
 part2 :: Bool -> String -> String
 part2 _ =
   show .
-  afterN 30000000 .
+  doNRounds 30000000 . createGame .
   zipWith (\a b -> (b, a)) [1 ..] .
   map Prelude.read . splitOn "," . filter (/= '\n')
