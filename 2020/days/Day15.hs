60df2e4a460bde62dd2dacdeef347b89cad0bba1
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-07 10:08:24 +0100

    2020, day 15

--- 2020/days/Day15.hs
+++ 2020/days/Day15.hs
@@ -1,9 +1,48 @@
-module Day15 (part1, part2) where
+module Day15
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.IntMap     as M (IntMap, fromList, insert, lookup)
+import           Data.List.Split (splitOn)
+import           Data.Maybe      (Maybe (Just, Nothing), fromJust, isNothing)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+type Game = IntMap Int
+
+type Turn = Int
+
+type Val = Int
+
+type State = (Turn, Val, Game)
+
+doNRounds :: Int -> State -> Int
+doNRounds nturns (turn, val, game)
+  | turn == nturns = val
+  | otherwise = doNRounds nturns (turn + 1, newVal, insert val turn game)
+  where
+    newVal
+      | isNothing lastSaid = 0
+      | otherwise = turn - fromJust lastSaid
+    lastSaid = M.lookup val game
+
+createGame :: [(Int, Int)] -> State
+createGame prelude = (turn, val, game)
+  where
+    game = fromList . init $ prelude
+    (val, turn) = last prelude
+
+part1 :: Bool -> String -> String
+part1 _ =
+  show .
+  doNRounds 2020 .
+  createGame .
+  zipWith (\a b -> (b, a)) [1 ..] .
+  map Prelude.read . splitOn "," . filter (/= '\n')
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ =
+  show .
+  doNRounds 30000000 .
+  createGame .
+  zipWith (\a b -> (b, a)) [1 ..] .
+  map Prelude.read . splitOn "," . filter (/= '\n')
