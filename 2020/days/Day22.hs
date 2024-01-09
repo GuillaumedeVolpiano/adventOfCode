4d8e393f28e78cdff342f74e452a129f578cf737
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-09 12:11:15 +0100

    2020, day 23, using STVector

--- 2020/days/Day22.hs
+++ 2020/days/Day22.hs
@@ -1,9 +1,60 @@
-module Day22 (part1, part2) where
+module Day22
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.List       as L (null)
+import           Data.List.Split (splitWhen)
+import           Data.Sequence   as S (Seq ((:<|), (:|>)), empty, fromList,
+                                       length, null, take)
+import           Data.Set        as St (Set, empty, insert, member)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+type Deck = Seq Int
+
+type Game = (Deck, Deck)
+
+type Rounds = Set Game
+
+playGame :: Game -> Game
+playGame (a, b)
+  | S.null a || S.null b = (a, b)
+  | ca > cb = playGame (ra :|> ca :|> cb, rb)
+  | cb > ca = playGame (ra, rb :|> cb :|> ca)
+  where
+    (ca :<| ra) = a
+    (cb :<| rb) = b
+
+playRecursiveGame :: Rounds -> Game -> Game
+playRecursiveGame rounds (a, b)
+  | S.null a || S.null b = (a, b)
+  | (a, b) `member` rounds = (a, S.empty)
+  | (ca > S.length ra || cb > S.length rb) && ca > cb = wonA
+  | ca > S.length ra || cb > S.length rb = wonB
+  | S.null . fst $ subGame = wonB
+  | S.null . snd $ subGame = wonA
+  where
+    wonA = playRecursiveGame (insert (a, b) rounds) winA
+    wonB = playRecursiveGame (insert (a, b) rounds) winB
+    winA = (ra :|> ca :|> cb, rb)
+    winB = (ra, rb :|> cb :|> ca)
+    (ca :<| ra) = a
+    (cb :<| rb) = b
+    subGame = playRecursiveGame St.empty (S.take ca ra, S.take cb rb)
+
+deal :: String -> Game
+deal =
+  (\[a, b] -> (fromList . map read . tail $ a, fromList . map read . tail $ b)) .
+  splitWhen L.null . lines
+
+score :: Game -> Int
+score (a, b)
+  | S.null a = result b
+  | S.null b = result a
+  where
+    result = snd . foldr (\a (c, acc) -> (c + 1, a * c + acc)) (1, 0)
+
+part1 :: Bool -> String -> String
+part1 _ = show . score . playGame . deal
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . score . playRecursiveGame St.empty . deal
