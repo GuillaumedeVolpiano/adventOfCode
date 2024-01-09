029e0dade18b3c4caba3f86fbf0e1e800d0111ef
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-09 17:23:14 +0100

    2020, day 25

--- 2020/days/Day25.hs
+++ 2020/days/Day25.hs
@@ -1,9 +1,34 @@
-module Day25 (part1, part2) where
+{-# LANGUAGE DataKinds #-}
 
-import Helpers.Parsers
+module Day25
+  ( part1
+  , part2
+  ) where
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+import           Data.Maybe                              (fromJust)
+import           GHC.TypeLits                            (Natural)
+import           Math.NumberTheory.Moduli.Class          (Mod, getVal)
+import           Math.NumberTheory.Moduli.Multiplicative (discreteLogarithm,
+                                                          isMultElement,
+                                                          isPrimitiveRoot)
+import           Math.NumberTheory.Moduli.Singleton      (CyclicGroup,
+                                                          cyclicGroup)
+
+findDiscreteLogarithm :: Int -> Int -> Natural
+findDiscreteLogarithm root remainder = discreteLogarithm cg rt rem
+  where
+    cg = fromJust cyclicGroup :: CyclicGroup Integer 20201227
+    rt = fromJust . isPrimitiveRoot cg . fromIntegral $ root
+    rem = fromJust . isMultElement . fromIntegral $ remainder
+
+findKey :: [Int] -> Integer
+findKey [cpk, dpk] = ek
+  where
+    dl = findDiscreteLogarithm 7 cpk
+    ek = getVal (fromIntegral dpk ^ dl :: Mod 20201227)
+
+part1 :: Bool -> String -> String
+part1 _ = show . findKey . map read . lines
 
 part2 :: Bool -> String -> String
 part2 _ _ = "Part 2"
