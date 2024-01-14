e31245df300d035b25d947258a8bd09bad689164
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-14 18:00:14 +0100

    2019, day 18, using old bfs as flg was giving a wrong result

--- 2019/days/Day15.hs
+++ 2019/days/Day15.hs
@@ -3,44 +3,32 @@ module Day15
   , part2
   ) where
 
-import           Data.Graph.Inductive.Graph        (labNodes)
-import           Data.Graph.Inductive.PatriciaTree (Gr)
-import           Data.Graph.Inductive.Query.BFS    (esp, level)
-import           Data.HashSet                      as St (HashSet, empty,
-                                                          insert, member,
-                                                          singleton, toList)
-import           Data.List                         as L (unfoldr)
-import           Data.Sequence                     as Sq (Seq ((:|>)),
-                                                          ViewL (EmptyL, (:<)),
-                                                          singleton, viewl)
-import           Helpers.Graph                     (Pos, assocsToDigraph, dirs,
-                                                    origin)
-import           Intcode                           (Intcode, initialise,
-                                                    runIntcode, sendInput)
-
-graphBuilder ::
-     (Seq ((Pos, Int), Intcode), HashSet (Pos, Int))
-  -> Maybe ( ((Pos, Int), [((Pos, Int), Int)])
-           , (Seq ((Pos, Int), Intcode), HashSet (Pos, Int)))
-graphBuilder (toSee, seen)
-  | decons == EmptyL = Nothing
-  | otherwise =
-    Just ((curVal, map (\x -> (fst x, 1)) toConsider), (newToSee, newSeen))
-  where
-    decons = viewl toSee
-    ((curVal@(curPos, _), curCode) :< rest) = decons
-    toConsider =
-      filter (\(x, _) -> snd x /= 0 && not (x `member` seen)) .
-      map (\(a, (b, c)) -> ((a, head b), c)) .
-      zipWith (\a b -> (curPos + a, runIntcode . sendInput b $ curCode)) dirs $
-      [1, 2, 4, 3]
-    newToSee = foldr (flip (:|>)) rest toConsider
-    newSeen = foldr (insert . fst) seen toConsider
+import           Data.Graph.Inductive.Graph     (labNodes)
+import           Data.Graph.Inductive.Query.BFS (esp, level)
+import           Data.HashSet                   as St (HashSet, empty, insert,
+                                                       member, singleton,
+                                                       toList)
+import           Data.List                      as L (unfoldr)
+import           Data.Sequence                  as Sq (Seq ((:|>)),
+                                                       ViewL (EmptyL, (:<)),
+                                                       singleton, viewl)
+import           Helpers.Graph                  (Gr, Pos, assocsToDigraph, dirs,
+                                                 origin, unfoldAssocs)
+import           Intcode                        (Intcode, initialise,
+                                                 runIntcode, sendInput)
 
 buildGraph :: Intcode -> Gr (Pos, Int) Int
 buildGraph intcode =
-  assocsToDigraph . unfoldr graphBuilder $
+  assocsToDigraph . unfoldr (unfoldAssocs buildCons fst) $
   (Sq.singleton ((origin, 1), intcode), St.singleton (origin, 1))
+  where
+    buildCons set pos =
+      filter (\((x, _), _) -> snd x /= 0 && not (x `member` set)) .
+      map (\(a, (b, c)) -> (((a, head b), c), 1)) .
+      zipWith
+        (\a b -> (fst (fst pos) + a, runIntcode . sendInput b . snd $ pos))
+        dirs $
+      [1, 2, 4, 3]
 
 oxygenTank :: Gr (Pos, Int) Int -> Int
 oxygenTank = fst . head . filter ((== 2) . snd . snd) . labNodes
