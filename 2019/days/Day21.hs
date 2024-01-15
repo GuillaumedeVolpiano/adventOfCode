3eddfabac1df3717797ee1baad7acbc74479277d
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-15 18:26:12 +0100

    2019, day 21

--- 2019/days/Day21.hs
+++ 2019/days/Day21.hs
@@ -1,9 +1,17 @@
-module Day21 (part1, part2) where
+module Day21
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Intcode (initialise, outputASCIIcode, outputIntcode,
+                          sendASCIIInput)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+walkscript = "NOT B J\nNOT C T\nOR T J\nAND D J\nNOT A T\nOR T J\nWALK\n"
+
+runscript = "NOT B J\nNOT C T\nOR T J\nAND D J\nAND H J\nNOT A T\nOR T J\nRUN\n"
+
+part1 :: Bool -> String -> String
+part1 _ = show . head . outputIntcode . sendASCIIInput walkscript . initialise
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . head . outputIntcode . sendASCIIInput runscript . initialise
