ebe2f18d26e5786b1d84ad3481d86138b0ae4b6e
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-17 05:05:07 +0100

    2019, day 24, optimised using Data.Bits

--- 2019/days/Day24.hs
+++ 2019/days/Day24.hs
@@ -3,88 +3,102 @@ module Day24
   , part2
   ) where
 
-import           Control.Lens.Getter ((^.))
-import           Data.Array.Unboxed  (UArray, bounds, inRange, indices, (!))
-import           Data.List           as L (filter, map)
-import           Data.Set            as St (Set, empty, filter, fromList,
-                                            insert, intersection, map, member,
-                                            singleton, size, union, unions)
-import           Helpers.Graph       (Pos, dirs)
-import           Helpers.Parsers     (boolArrayFromString)
-import           Linear.V2           (V2 (..))
-import           Linear.V3           (V3 (..), _xy, _z)
+import           Data.Array.Unboxed (UArray, indices, (!))
+import           Data.Bits          (bit, clearBit, popCount, setBit, testBit,
+                                     (.&.), (.|.))
+import           Data.IntMap        as M (IntMap, alter, findWithDefault,
+                                          insert, keys, map, singleton)
+import           Data.IntSet        as St (IntSet, empty, fromList, insert,
+                                           member, toList, unions)
+import           Data.List          as L (map)
+import           Data.Maybe         (Maybe (Just, Nothing), mapMaybe)
+import           Helpers.Graph      (Pos)
+import           Helpers.Parsers    (boolArrayFromString)
 
-type RecurPos = V3 Int
+type Bugs = Int
 
-type RecurBugs = Set RecurPos
+type RecurBugs = IntMap Bugs
 
-type Bugs = Set Pos
-
-range = (V2 0 0, V2 4 4)
-
-recurBirth :: UArray Pos Bool -> RecurBugs
-recurBirth = St.map (\(V2 x y) -> V3 x y 0) . birth
-
-birth :: UArray Pos Bool -> Bugs
-birth bugMap = St.filter (bugMap !) . fromList . indices $ bugMap
+birth :: String -> Bugs
+birth = foldr bitify 0
+  where
+    bitify a b
+      | a == '#' = 1 + 2 * b
+      | a == '.' = 2 * b
+      | a == '\n' = b
 
 recurGol :: RecurBugs -> RecurBugs
-recurGol bugs = survive `union` rebirth
+recurGol rBugs = foldr (\d -> alter (newVal d) d) rBugs activeDepths
   where
-    survive = St.filter survivor bugs
-    survivor = (1 ==) . countNeighbours . neighbours
-    candidates =
-      foldr union empty . St.map (St.filter (not . flip elem bugs) . neighbours) $
-      bugs
-    rebirth = St.filter reborn candidates
-    reborn = flip elem [1, 2] . countNeighbours . neighbours
-    left (V3 x y z)
-      | x == 0 = singleton $ V3 1 2 (z + 1)
-      | x == 3 && y == 2 = fromList [V3 4 a (z - 1) | a <- [0 .. 4]]
-      | otherwise = singleton $ V3 (x - 1) y z
-    right (V3 x y z)
-      | x == 4 = singleton $ V3 3 2 (z + 1)
-      | x == 1 && y == 2 = fromList [V3 0 a (z - 1) | a <- [0 .. 4]]
-      | otherwise = singleton $ V3 (x + 1) y z
-    up (V3 x y z)
-      | y == 0 = singleton $ V3 2 1 (z + 1)
-      | y == 3 && x == 2 = fromList [V3 a 4 (z - 1) | a <- [0 .. 4]]
-      | otherwise = singleton $ V3 x (y - 1) z
-    down (V3 x y z)
-      | y == 4 = singleton $ V3 2 3 (z + 1)
-      | y == 1 && x == 2 = fromList [V3 a 0 (z - 1) | a <- [0 .. 4]]
-      | otherwise = singleton $ V3 x (y + 1) z
-    neighbours = left <> right <> up <> down
-    countNeighbours = size . intersection bugs
+    status depth n v
+      | (bugs depth `testBit` n && bugMask depth n == 1) ||
+          (not (bugs depth `testBit` n) && bugMask depth n `elem` [1, 2]) =
+        v `setBit` n
+      | otherwise = v `clearBit` n
+    bugMask depth n = left depth n + right depth n + up depth n + down depth n
+    left depth n
+      | n `mod` 5 == 0 = bitify (external depth) 11
+      | n == 13 = sum . L.map (bitify (internal depth)) $ [4, 9, 14, 19, 24]
+      | otherwise = popCount $ bit (n - 1) .&. bugs depth
+    right depth n
+      | n `mod` 5 == 4 = bitify (external depth) 13
+      | n == 11 = sum . L.map (bitify (internal depth)) $ [0, 5, 10, 15, 20]
+      | otherwise = popCount $ bit (n + 1) .&. bugs depth
+    up depth n
+      | n < 5 = bitify (external depth) 7
+      | n == 17 = sum . L.map (bitify (internal depth)) $ [20, 21, 22, 23, 24]
+      | otherwise = popCount $ bit (n - 5) .&. bugs depth
+    down depth n
+      | n > 19 = bitify (external depth) 17
+      | n == 7 = sum . L.map (bitify (internal depth)) $ [0, 1, 2, 3, 4]
+      | otherwise = popCount $ bit (n + 5) .&. bugs depth
+    external depth = findWithDefault 0 (depth + 1) rBugs
+    internal depth = findWithDefault 0 (depth - 1) rBugs
+    bugs depth = findWithDefault 0 depth rBugs
+    bitify bugLayer val = popCount $ bugLayer .&. bit val
+    golLayer depth = foldr (status depth) (bugs depth) $ [0 .. 11] ++ [13 .. 24]
+    newVal depth _
+      | golLayer depth == 0 = Nothing
+      | otherwise = Just $ golLayer depth
+    activeDepths =
+      toList . unions . L.map (\d -> fromList . L.map (d +) $ [-1, 0, 1]) . keys $
+      rBugs
 
 gol :: Bugs -> Bugs
-gol bugs = survive `union` rebirth
+gol bugs = foldr status bugs [0 .. 24]
   where
-    survive = St.filter survivor bugs
-    survivor p = (1 ==) . length . L.filter (`member` bugs) . L.map (p +) $ dirs
-    candidates =
-      St.filter (not . flip elem bugs) .
-      unions . L.map (\p -> St.map (p +) bugs) $
-      dirs
-    rebirth = St.filter reborn candidates
-    reborn p =
-      inRange range p &&
-      (flip elem [1, 2] . length . L.filter (`member` bugs) . L.map (p +) $ dirs)
+    status n v
+      | (bugs `testBit` n && (popCount . bugMask $ n) == 1) ||
+          (not (bugs `testBit` n) && (popCount . bugMask $ n) `elem` [1, 2]) =
+        v `setBit` n
+      | otherwise = v `clearBit` n
+    bugMask = (.&.) bugs . foldr ((.|.) . bit) 0 . neighbours
+    neighbours = left <> right <> up <> down
+    left n
+      | n `mod` 5 == 0 = []
+      | otherwise = [n - 1]
+    right n
+      | n `mod` 5 == 4 = []
+      | otherwise = [n + 1]
+    up n
+      | n < 5 = []
+      | otherwise = [n - 5]
+    down n
+      | n > 19 = []
+      | otherwise = [n + 5]
 
-biodiversity :: Set Bugs -> Bugs -> Int
-biodiversity layout bugs
-  | bugs `member` layout = sum . St.map biodiv $ bugs
-  | otherwise = biodiversity (insert bugs layout) $ gol bugs
-  where
-    biodiv (V2 x y) = 2 ^ (5 * y + x)
+biodiversity :: IntSet -> Bugs -> Bugs
+biodiversity seen bugs
+  | bugs `member` seen = bugs
+  | otherwise = biodiversity (St.insert bugs seen) $ gol bugs
 
 part1 :: Bool -> String -> String
-part1 _ = show . biodiversity empty . birth . boolArrayFromString '#'
+part1 _ = show . biodiversity empty . birth
 
 part2 :: Bool -> String -> String
 part2 test =
   show .
-  size . (!! time) . iterate recurGol . recurBirth . boolArrayFromString '#'
+  sum . M.map popCount . (!! time) . iterate recurGol . singleton 0 . birth
   where
     time
       | test = 10
