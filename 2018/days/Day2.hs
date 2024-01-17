4a1604877ea3eea3e6fe679cc956bb2aa8623479
Xavier Dectot <xavier.dectot@gmail.com>
2024-01-17 16:59:18 +0100

    2018, day 2

--- 2018/days/Day2.hs
+++ 2018/days/Day2.hs
@@ -1,9 +1,35 @@
-module Day2 (part1, part2) where
+module Day2
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.List  (group, sort, tails)
+import           Data.Maybe (Maybe (Just, Nothing), catMaybes, isNothing)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+findDifference :: [String] -> String
+findDifference list =
+  catMaybes .
+  head .
+  filter ((== 1) . length . filter isNothing) .
+  concat .
+  zipWith
+    (map .
+     zipWith
+       (\a b ->
+          if a == b
+            then Just a
+            else Nothing))
+    (init list) $
+  tails list
+
+checksum :: [[Int]] -> Int
+checksum iDs = twos * threes
+  where
+    threes = length . filter (3 `elem`) $ iDs
+    twos = length . filter (2 `elem`) $ iDs
+
+part1 :: Bool -> String -> String
+part1 _ = show . checksum . map (map length . group . sort) . lines
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = findDifference . lines
