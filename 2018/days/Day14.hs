c45ffb151c8d5487163c6575f193c31a566f094d
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-05-19 19:29:06 +0200

    2018, day 14, lazy bummer

--- 2018/days/Day14.hs
+++ 2018/days/Day14.hs
@@ -4,52 +4,40 @@ module Day14
   ) where
 
 import           Data.Char     (digitToInt, intToDigit)
-import           Data.Sequence as S (Seq ((:|>)), drop, fromList, index, length,
-                                     take)
+import           Data.List     as L (isPrefixOf, length, tails)
+import           Data.Sequence as S (fromList, index, length, (><))
 import           Debug.Trace
 
-initialState = fromList [3, 7]
+initialList = [3, 7]
+
+initialState = fromList initialList
 
 initialPos1 = 0
 
 initialPos2 = 1
 
-cookTo :: Seq Int -> Int -> Int -> Int -> String
-cookTo recipes fe se num
-  | S.length newRecipes >= num + 10 =
-    foldr (\a b -> intToDigit a : b) "" . S.take 10 . S.drop num $ newRecipes
-  | otherwise = cookTo newRecipes nfe nse num
+recipes :: [Int]
+recipes = 3 : 7 : cookLazy initialPos1 initialPos2 initialState
   where
-    (newRecipes, nfe, nse) = cook recipes fe se
+    cookLazy fe se re = newRecipes ++ cookLazy nfe nse nre
+      where
+        fr = index re fe
+        sr = index re se
+        newRecipes = splitTens (fr + sr)
+        nre = re >< fromList newRecipes
+        nfe = mod (1 + fe + fr) (S.length nre)
+        nse = mod (1 + se + sr) (S.length nre)
 
-cook :: Seq Int -> Int -> Int -> (Seq Int, Int, Int)
-cook recipes fe se = (newRecipes, nfe, nse)
-  where
-    fr = index recipes fe --The first elf's recipe
-    sr = index recipes se --The second elf's recipe
-    intRecipes
-      | fr + sr >= 10 = recipes :|> 1
-      | otherwise = recipes
-    newRecipes = intRecipes :|> mod (fr + sr) 10
-    nfe = mod (1 + fe + fr) . S.length $ newRecipes
-    nse = mod (1 + se + sr) . S.length $ newRecipes
-
-cookLeft :: Seq Int -> Int -> Int -> Seq Int -> Int
-cookLeft recipes fe se num
-  | lr >= ln && (S.take ln . S.drop (lr - ln) $ recipes) == num = lr - ln
-  | lr >= ln + 1 && (S.take ln . S.drop (lr - ln - 1) $ recipes) == num =
-    lr - ln - 1
-  | otherwise = cookLeft newRecipes nfe nse num
-  where
-    (newRecipes, nfe, nse) = cook recipes fe se
-    lr = S.length recipes
-    ln = S.length num
+splitTens :: Int -> [Int]
+splitTens rec
+  | rec < 10 = [rec]
+  | otherwise = [div rec 10, mod rec 10]
 
 part1 :: Bool -> String -> String
-part1 _ = cookTo initialState initialPos1 initialPos2 . read . init
+part1 _ input = map intToDigit . take 10 . drop (read . init $ input) $ recipes
 
 part2 :: Bool -> String -> String
-part2 _ =
-  show .
-  cookLeft initialState initialPos1 initialPos2 .
-  fromList . map digitToInt . init
+part2 _ input =
+  show . L.length . takeWhile (not . (number `isPrefixOf`)) . tails $ recipes
+  where
+    number = map digitToInt . init $ input
