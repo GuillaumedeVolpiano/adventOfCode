ccb4d86503b1991bc70d8dc7b7643ad02a0f7a82
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-11-22 16:32:45 +0100

    2017, day 7

--- 2017/days/Day7.hs
+++ 2017/days/Day7.hs
@@ -1,9 +1,53 @@
-module Day7 (part1, part2) where
+module Day7
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.List       ((\\))
+import           Helpers.Parsers (alphaNum)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+data Tower = Program
+  { name        :: String
+  , weight      :: Int
+  , totalWeight :: Int
+  , subTowers   :: [Tower]
+  } deriving (Show, Eq)
+
+findRoot :: [[String]] -> String
+findRoot l = head $ names \\ contents
+  where
+    names = map head l
+    contents = concatMap (drop 2) l
+
+buildTower :: [[String]] -> Tower
+buildTower l = tower l . findRoot $ l
+
+tower :: [[String]] -> String -> Tower
+tower l n = Program n w tw st
+  where
+    st = map (tower l) subs
+    tw = (+) w . sum . map totalWeight $ st
+    (_:rawW:subs) = head . filter ((== n) . head) $ l
+    w = read rawW
+
+balance :: Int -> Tower -> Int
+balance curDiff tower
+  | null sub || minV == maxV = curDiff + weight tower
+  | otherwise = balance newDiff nextTower
+  where
+    sub = subTowers tower
+    minV = minimum . map totalWeight $ sub
+    maxV = maximum . map totalWeight $ sub
+    minima = filter ((== minV) . totalWeight) sub
+    newDiff
+      | length minima == 1 = maxV - minV
+      | otherwise = minV - maxV
+    nextTower
+      | length minima == 1 = head minima
+      | otherwise = head . filter ((== maxV) . totalWeight) $ sub
+
+part1 :: Bool -> String -> String
+part1 _ = show . findRoot . alphaNum
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . balance 0 . buildTower . alphaNum
