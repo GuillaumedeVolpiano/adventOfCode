d39ffa91456c04e77982f7ec878aa4c860b963f3
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-11-23 10:18:39 +0100

    2017, day 9

--- 2017/days/Day9.hs
+++ 2017/days/Day9.hs
@@ -1,9 +1,53 @@
-module Day9 (part1, part2) where
+module Day9
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.Either          (fromRight)
+import           Helpers.Parsers      (Parser)
+import           Text.Megaparsec      (manyTill, optional, parse, takeWhile1P,
+                                       try, (<|>))
+import           Text.Megaparsec.Char (char, printChar)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+data Group
+  = Group Int [Group]
+  | Garbage Int
+  deriving (Show)
+
+parseGroup :: Int -> Parser Group
+parseGroup depth = do
+  char '{'
+  contents <- manyTill (parseGroup (depth + 1) <|> parseGarbage) (char '}')
+  optional . char $ ','
+  return . Group depth $ contents
+
+parseGarbage :: Parser Group
+parseGarbage = do
+  char '<'
+  count <- manyTill (cancel <|> garb) (char '>')
+  optional . char $ ','
+  return . Garbage . sum $ count
+
+cancel :: Parser Int
+cancel = do
+  char '!'
+  printChar
+  return 0
+
+garb :: Parser Int
+garb = do
+  length <$> takeWhile1P Nothing (`notElem` "!>")
+
+score1 :: Group -> Int
+score1 (Garbage _) = 0
+score1 (Group s g) = (+ s) . sum . map score1 $ g
+
+score2 :: Group -> Int
+score2 (Garbage g) = g
+score2 (Group _ g) = sum . map score2 $ g
+
+part1 :: Bool -> String -> String
+part1 _ = show . score1 . fromRight (Garbage 0) . parse (parseGroup 1) ""
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . score2 . fromRight (Garbage 0) . parse (parseGroup 1) ""
