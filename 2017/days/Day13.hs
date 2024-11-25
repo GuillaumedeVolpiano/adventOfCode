b144a38a8371f507dd12dcc555891a7ae0b1c3c9
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-11-25 20:15:16 +0100

    2017, day 13

--- 2017/days/Day13.hs
+++ 2017/days/Day13.hs
@@ -1,9 +1,44 @@
-module Day13 (part1, part2) where
+module Day13
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.Maybe      (mapMaybe)
+import           Helpers.Parsers (numbers)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+data Scanner =
+  Scanner Depth Range
+  deriving (Show)
+
+type Depth = Int
+
+type Range = Int
+
+inRange :: Int -> Scanner -> Bool
+inRange start (Scanner depth range) =
+  (start + depth) `mod` (2 * (range - 1)) == 0
+
+severity :: Scanner -> Int
+severity (Scanner depth range) = depth * range
+
+mkScanners :: String -> [Scanner]
+mkScanners = map (\[depth, range] -> Scanner depth range) . numbers
+
+check :: [Scanner] -> Int -> Maybe Int
+check scanners start
+  | any (inRange start) scanners = Nothing
+  | otherwise = Just start
+
+part1 :: Bool -> String -> String
+part1 _ =
+  show
+    . sum
+    . map severity
+    . filter (inRange 0)
+    . map (\[depth, range] -> Scanner depth range)
+    . numbers
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ input = show . head . mapMaybe (check scanners) $ [0 ..]
+  where
+    scanners = mkScanners input
