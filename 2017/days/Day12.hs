3607c6e69d763b72d58c6818baffec0691a50bde
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-11-25 19:47:34 +0100

    2017, day 12

--- 2017/days/Day12.hs
+++ 2017/days/Day12.hs
@@ -1,9 +1,28 @@
-module Day12 (part1, part2) where
+{-# LANGUAGE TupleSections #-}
 
-import Helpers.Parsers
+module Day12
+  ( part1
+  , part2
+  ) where
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+import           Data.Graph.Inductive.Graph     (mkUGraph)
+import           Data.Graph.Inductive.Query.DFS (components)
+import           Data.Set                       (fromList, toList)
+import           Helpers.Graph                  (Gr)
+import           Helpers.Parsers                (numbers)
+
+type System = Gr () ()
+
+buildGraph :: [[Int]] -> System
+buildGraph pipes = mkUGraph nodes (outEdges ++ inEdges)
+  where
+    outEdges = concatMap (\(x:xs) -> map (x, ) xs) pipes
+    inEdges = concatMap (\(x:xs) -> map (, x) xs) pipes
+    nodes = toList . fromList . concat $ pipes
+
+part1 :: Bool -> String -> String
+part1 _ =
+  show . length . head . filter (elem 0) . components . buildGraph . numbers
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . length . components . buildGraph . numbers
