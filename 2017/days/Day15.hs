1bb7684c33edfbfc834c7e20cee54869741633a1
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-11-26 08:47:17 +0100

    2017, day 15

--- 2017/days/Day15.hs
+++ 2017/days/Day15.hs
@@ -1,9 +1,38 @@
-module Day15 (part1, part2) where
+module Day15
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.Bits       ((.&.))
+import           Helpers.Parsers (numbers)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+genA :: Int -> [Int]
+genA = gen 16807
+
+genB :: Int -> [Int]
+genB = gen 48271
+
+gen :: Int -> Int -> [Int]
+gen factor = tail . iterate op
+  where
+    op = flip mod 2147483647 . (factor *)
+
+fstGen :: [Int] -> [(Int, Int)]
+fstGen [a, b] = zip (genA a) (genB b)
+
+sndGen :: [Int] -> [(Int, Int)]
+sndGen [a, b] = zip (filter (mult 4) . genA $ a) (filter (mult 8) . genB $ b)
+  where
+    mult v x = x `mod` v == 0
+
+score :: Int -> [(Int, Int)] -> Int
+score numPairs = length . filter agree . take numPairs
+
+agree :: (Int, Int) -> Bool
+agree (a, b) = a .&. 0xffff == b .&. 0xffff
+
+part1 :: Bool -> String -> String
+part1 _ = show . score (4 * 10 ^ 7) . fstGen . concat . numbers
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . score (5 * 10 ^ 6) . sndGen . concat . numbers
