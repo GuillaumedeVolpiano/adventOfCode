6f6eb40b360b6476ac3c1576e0afdbf8051a86a3
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-11-29 21:59:48 +0100

    2016, day 4

--- 2016/days/Day4.hs
+++ 2016/days/Day4.hs
@@ -1,9 +1,62 @@
-module Day4 (part1, part2) where
+module Day4
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.Char            (chr, isAlpha, isDigit, ord)
+import           Data.List            (group, intersperse, isInfixOf, sort,
+                                       sortBy)
+import           Data.Maybe           (catMaybes)
+import           Data.Ord             (Down, comparing)
+import           Helpers.Parsers      (Parser, parseByLine)
+import           Text.Megaparsec      (many, optional, takeWhileP)
+import           Text.Megaparsec.Char (char, eol)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+type Cipher = ([String], Int)
+
+parseInput :: String -> [Cipher]
+parseInput = catMaybes . parseByLine parseCipher
+
+parseCipher :: Parser (Maybe Cipher)
+parseCipher = do
+  name <- many word
+  iD <- read <$> takeWhileP Nothing isDigit
+  char '['
+  checksum <- takeWhileP Nothing isAlpha
+  char ']'
+  optional eol
+  let checked =
+        take 5
+          . map head
+          . sortBy (flip (comparing length))
+          . group
+          . sort
+          . concat
+          $ name
+      result
+        | checked == checksum = Just (name, iD)
+        | otherwise = Nothing
+  return result
+
+word :: Parser String
+word = do
+  result <- takeWhileP Nothing isAlpha
+  char '-'
+  return result
+
+decipher :: Cipher -> (String, Int)
+decipher (wordList, iD) = (unwords . map (map decode) $ wordList, iD)
+  where
+    decode c = chr . (97 +) . mod (ord c - 97 + iD) $ 26
+
+part1 :: Bool -> String -> String
+part1 _ = show . sum . map snd . parseInput
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ =
+  show
+    . snd
+    . head
+    . filter (\x -> "northpole" `isInfixOf` fst x)
+    . map decipher
+    . parseInput
