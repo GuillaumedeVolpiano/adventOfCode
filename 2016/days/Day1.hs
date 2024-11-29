c93707076aba62fe335a2c41a67084754bb065af
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-11-29 16:01:26 +0100

    2016, day 1

--- 2016/days/Day1.hs
+++ 2016/days/Day1.hs
@@ -1,9 +1,53 @@
-module Day1 (part1, part2) where
+module Day1
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.Char            (isDigit)
+import           Data.Either          (fromRight)
+import           Data.Set             (Set, empty, insert, member)
+import           Helpers.Graph        (Pos, left, manhattanDistance, north,
+                                       origin, right)
+import           Helpers.Parsers      (Parser)
+import           Linear.Vector        ((*^))
+import           Text.Megaparsec      (optional, parse, takeWhileP, (<|>))
+import           Text.Megaparsec.Char (eol, string, upperChar)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+type Dir = Pos
+
+readDir :: Char -> (Dir -> Dir)
+readDir 'R' = right
+readDir 'L' = left
+
+parseInput :: Pos -> Dir -> Parser Int
+parseInput pos dir = do
+  turn <- readDir <$> upperChar
+  dist <- read <$> takeWhileP Nothing isDigit
+  optional . string $ ", "
+  let dir' = turn dir
+      pos' = pos + dist *^ dir'
+  end pos' <|> parseInput pos' dir'
+
+parseTwice :: Pos -> Dir -> Set Pos -> Parser Int
+parseTwice pos dir seen = do
+  turn <- readDir <$> upperChar
+  dist <- read <$> takeWhileP Nothing isDigit
+  optional . string $ ", "
+  let dir' = turn dir
+      pos' = pos + dist *^ dir'
+      seen' = insert pos' seen
+      result
+        | pos' `member` seen = return . manhattanDistance origin $ pos'
+        | otherwise = parseTwice pos' dir' seen'
+  result
+
+end :: Pos -> Parser Int
+end pos = do
+  eol
+  return $ manhattanDistance origin pos
+
+part1 :: Bool -> String -> String
+part1 _ = show . fromRight 0 . parse (parseInput origin north) ""
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . fromRight 0 . parse (parseTwice origin north empty) ""
