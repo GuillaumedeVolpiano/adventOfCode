fb15f5d6713ae6278f9e188c60582a03855086a6
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-11-29 20:51:04 +0100

    2016, day 3

--- 2016/days/Day3.hs
+++ 2016/days/Day3.hs
@@ -1,9 +1,18 @@
-module Day3 (part1, part2) where
+module Day3
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.List       (transpose)
+import           Data.List.Split (chunksOf)
+import           Helpers.Parsers (numbers)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+isTriangle :: [Int] -> Bool
+isTriangle [a, b, c] = a + b > c && a + c > b && b + c > a
+
+part1 :: Bool -> String -> String
+part1 _ = show . length . filter isTriangle . numbers
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ =
+  show . length . filter isTriangle . chunksOf 3 . concat . transpose . numbers
