dd2b3eb0142226f3592eccf5809a2e5e2bee2985
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-11-30 16:49:46 +0100

    2016, day 7

--- 2016/days/Day7.hs
+++ 2016/days/Day7.hs
@@ -1,9 +1,49 @@
-module Day7 (part1, part2) where
+module Day7
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.List       (intersect)
+import           Data.List.Split (splitOneOf)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+hasTLS :: String -> Bool
+hasTLS ip = any isABBA external && not (any isABBA internal)
+  where
+    ips = splitOneOf "[]" ip
+    (external, internal) = foldr alternate ([], []) ips
+
+alternate :: a -> ([a], [a]) -> ([a], [a])
+alternate x (others, ones) = (x : ones, others)
+
+isABBA :: String -> Bool
+isABBA xs
+  | length xs < 4 = False
+isABBA (a:b:c:d:xs) = (a == d && b == c && a /= b) || isABBA (b : c : d : xs)
+
+hasSSL :: String -> Bool
+hasSSL ip =
+  not . null . intersect (concatMap listABA external)
+    $ concatMap listBAB internal
+  where
+    ips = splitOneOf "[]" ip
+    (external, internal) = foldr alternate ([], []) ips
+
+listABA :: String -> [(Char, Char)]
+listABA xs
+  | length xs < 3 = []
+listABA (a:b:c:xs)
+  | a == c && a /= b = (a, b) : listABA (b : c : xs)
+  | otherwise = listABA (b : c : xs)
+
+listBAB :: String -> [(Char, Char)]
+listBAB xs
+  | length xs < 3 = []
+listBAB (a:b:c:xs)
+  | a == c && a /= b = (b, a) : listBAB (b : c : xs)
+  | otherwise = listBAB (b : c : xs)
+
+part1 :: Bool -> String -> String
+part1 _ = show . length . filter hasTLS . lines
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . length . filter hasSSL . lines
