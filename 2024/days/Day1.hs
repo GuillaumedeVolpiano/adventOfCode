590c3fd654b3a645f020a0385a46dc911ecef713
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-01 08:24:27 +0100

    2024, day 1

--- 2024/days/Day1.hs
+++ 2024/days/Day1.hs
@@ -1,9 +1,21 @@
-module Day1 (part1, part2) where
+module Day1
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+import           Data.List       (sort, transpose)
+import           Helpers.Parsers (numbers)
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+diff :: [[Int]] -> [Int]
+diff [a, b] = map abs . zipWith (-) a $ b
+
+similarity :: [[Int]] -> [Int]
+similarity [a, b] = map appears a
+  where
+    appears x = (* x) . length . filter (== x) $ b
+
+part1 :: Bool -> String -> String
+part1 _ = show . sum . diff . map sort . transpose . numbers
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . sum . similarity . transpose . numbers
