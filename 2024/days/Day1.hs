fb0df890973147a447362fee33a626786ae4ea7a
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-01 12:34:48 +0100

    2024, day 1, overengineering - p2

--- 2024/days/Day1.hs
+++ 2024/days/Day1.hs
@@ -4,22 +4,22 @@ module Day1
   ) where
 
 import           Data.List       (sort, transpose)
-import           Data.MultiSet   (fromList, occur)
+import           Data.MultiSet   as MS (fromList, minView, null, occur)
 import           Helpers.Parsers (numbers)
 
-diff :: [[Int]] -> [Int]
-diff [a, b] = map abs . zipWith (-) a $ b
+diff :: [[Int]] -> Int
+diff [a, b] = sum . map abs . zipWith (-) a $ b
 
-similarity :: [[Int]] -> [Int]
-similarity [a, b] = map appears a
+similarity :: [[Int]] -> Int
+similarity [a, b] = sum . map appears $ a
   where
+    -- pre-overengineering
+    -- appears x = (* x) . length . filter (== x) $ b
     b' = fromList b
     appears x = (* x) . occur x $ b'
 
--- pre-overengineering
--- appears x = (* x) . length . filter (== x) $ b
 part1 :: Bool -> String -> String
-part1 _ = show . sum . diff . map sort . transpose . numbers
+part1 _ = show . diff . map sort . transpose . numbers
 
 part2 :: Bool -> String -> String
-part2 _ = show . sum . similarity . transpose . numbers
+part2 _ = show . similarity . transpose . numbers
