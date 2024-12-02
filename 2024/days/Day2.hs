662c2302e4ff9c520350469759dc33e037cef3cf
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-02 10:53:03 +0100

    2024, day 2, overengineered

--- 2024/days/Day2.hs
+++ 2024/days/Day2.hs
@@ -3,24 +3,37 @@ module Day2
   , part2
   ) where
 
-isSafe :: [Int] -> Bool
+import           Data.Sequence  as Sq (Seq ((:<|), (:|>)), empty, fromList,
+                                       null, (><))
+import           Data.Sequences (IsSequence, tailEx)
+import           Data.Zip       as Z (Zip, zipWith)
+
+isSafe :: Foldable t => t Int -> Bool
 isSafe diffs =
   (all (> 0) diffs && not (any (> 3) diffs))
     || (all (< 0) diffs && not (any (< (-3)) diffs))
 
-dampenedSafe :: [Int] -> Bool
-dampenedSafe levels = dampen [] (head levels) (tail levels)
+dampenedSafe :: Seq Int -> Bool
+dampenedSafe (l :<| evels) = dampen empty l evels
 
-dampen :: [Int] -> Int -> [Int] -> Bool
-dampen [] x xs@(y:ys)  = checkSafe xs || dampen [x] y ys
-dampen xs x []         = checkSafe xs
-dampen xs x xs'@(y:ys) = checkSafe (xs ++ xs') || dampen (xs ++ [x]) y ys
+dampen :: Seq Int -> Int -> Seq Int -> Bool
+dampen xs x ys
+  | Sq.null xs = checkSafe ys || dampen (x :<| empty) y ys'
+  | Sq.null ys = checkSafe xs
+  | otherwise = checkSafe (xs >< ys) || dampen (xs :|> x) y ys'
+  where
+    (y :<| ys') = ys
 
-checkSafe :: [Int] -> Bool
-checkSafe levels = isSafe . zipWith (-) (init levels) $ tail levels
+checkSafe :: (Foldable t, Zip t, IsSequence (t Int)) => t Int -> Bool
+checkSafe levels = isSafe . Z.zipWith (-) levels $ tailEx levels
 
 part1 :: Bool -> String -> String
 part1 _ = show . length . filter checkSafe . map (map read . words) . lines
 
 part2 :: Bool -> String -> String
-part2 _ = show . length . filter dampenedSafe . map (map read . words) . lines
+part2 _ =
+  show
+    . length
+    . filter dampenedSafe
+    . map (fromList . map read . words)
+    . lines
