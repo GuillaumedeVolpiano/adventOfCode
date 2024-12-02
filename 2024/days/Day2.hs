96791b910acf171513c8fe0c3830dfd5218a48b0
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-02 14:14:20 +0100

    2024, day 2, cleaned up

--- 2024/days/Day2.hs
+++ 2024/days/Day2.hs
@@ -3,37 +3,34 @@ module Day2
   , part2
   ) where
 
+import           Data.Ix        (inRange)
+import           Data.List      as L (length)
 import           Data.Sequence  as Sq (Seq ((:<|), (:|>)), empty, fromList,
-                                       null, (><))
+                                       length, null, splitAt, (><))
 import           Data.Sequences (IsSequence, tailEx)
 import           Data.Zip       as Z (Zip, zipWith)
 
 isSafe :: Foldable t => t Int -> Bool
-isSafe diffs =
-  (all (> 0) diffs && not (any (> 3) diffs))
-    || (all (< 0) diffs && not (any (< (-3)) diffs))
+isSafe diffs = all (inRange (1, 3)) diffs || all (inRange (-3, -1)) diffs
 
 dampenedSafe :: Seq Int -> Bool
-dampenedSafe (l :<| evels) = dampen empty l evels
+dampenedSafe levels = any (dampen levels) [0 .. Sq.length levels - 1]
 
-dampen :: Seq Int -> Int -> Seq Int -> Bool
-dampen xs x ys
-  | Sq.null xs = checkSafe ys || dampen (x :<| empty) y ys'
-  | Sq.null ys = checkSafe xs
-  | otherwise = checkSafe (xs >< ys) || dampen (xs :|> x) y ys'
+dampen :: Seq Int -> Int -> Bool
+dampen levels index = checkSafe (xs >< ys)
   where
-    (y :<| ys') = ys
+    (xs, _ :<| ys) = Sq.splitAt index levels
 
 checkSafe :: (Foldable t, Zip t, IsSequence (t Int)) => t Int -> Bool
-checkSafe levels = isSafe . Z.zipWith (-) levels $ tailEx levels
+checkSafe = isSafe . (Z.zipWith (-) <*> tailEx)
 
 part1 :: Bool -> String -> String
-part1 _ = show . length . filter checkSafe . map (map read . words) . lines
+part1 _ = show . L.length . filter checkSafe . map (map read . words) . lines
 
 part2 :: Bool -> String -> String
 part2 _ =
   show
-    . length
+    . L.length
     . filter dampenedSafe
     . map (fromList . map read . words)
     . lines
