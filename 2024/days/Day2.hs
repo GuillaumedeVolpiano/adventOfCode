f573f0161c977a227d8d87ea784c21e32b4318f8
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-02 08:16:32 +0100

    2024, day 2

--- 2024/days/Day2.hs
+++ 2024/days/Day2.hs
@@ -1,9 +1,26 @@
-module Day2 (part1, part2) where
+module Day2
+  ( part1
+  , part2
+  ) where
 
-import Helpers.Parsers
+isSafe :: [Int] -> Bool
+isSafe diffs =
+  (all (> 0) diffs && not (any (> 3) diffs))
+    || (all (< 0) diffs && not (any (< (-3)) diffs))
 
-part1 :: Bool -> String -> String
-part1 _ _ = "Part 1"
+dampenedSafe :: [Int] -> Bool
+dampenedSafe levels = dampen [] (head levels) (tail levels)
+
+dampen :: [Int] -> Int -> [Int] -> Bool
+dampen [] x xs@(y:ys)  = checkSafe xs || dampen [x] y ys
+dampen xs x []         = checkSafe xs
+dampen xs x xs'@(y:ys) = checkSafe (xs ++ xs') || dampen (xs ++ [x]) y ys
+
+checkSafe :: [Int] -> Bool
+checkSafe levels = isSafe . zipWith (-) (init levels) $ tail levels
+
+part1 :: Bool -> String -> String
+part1 _ = show . length . filter checkSafe . map (map read . words) . lines
 
 part2 :: Bool -> String -> String
-part2 _ _ = "Part 2"
+part2 _ = show . length . filter dampenedSafe . map (map read . words) . lines
