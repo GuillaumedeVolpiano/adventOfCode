834ac78b775552e77dffa38e93f32062070ea07c
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-10 14:10:01 +0100

    2024, day 3, statifying the parser

--- 2024/days/Day3.hs
+++ 2024/days/Day3.hs
@@ -3,47 +3,56 @@ module Day3
   , part2
   ) where
 
+import           Control.Monad.State        (State, evalState, get, modify)
+import           Data.Bifunctor             (second)
 import           Data.Either                (fromRight)
-import           Data.Text                  (Text)
-import           Helpers.Parsers.Text       (Parser, string)
-import           Text.Megaparsec            (eof, parse, try, (<|>))
-import           Text.Megaparsec.Char       (char, eol, printChar)
+import           Data.Text                  (Text, pack)
+import           Data.Void                  (Void)
+import           Text.Megaparsec            (ParsecT, eof, parse, runParserT,
+                                             try, (<|>))
+import           Text.Megaparsec.Char       (char, eol, printChar, string)
 import           Text.Megaparsec.Char.Lexer (decimal)
 
-parseInput :: Bool -> Bool -> Parser Int
-parseInput isPart1 isDo =
-  try (parseMul isPart1 isDo)
+type Parser = ParsecT Void Text (State (Bool, Bool))
+
+parseInput :: Parser Int
+parseInput =
+  try parseMul
     <|> try
           (do
-             string "do()"
-             parseInput isPart1 True)
+             string . pack $ "do()"
+             modify (second . const $ True)
+             parseInput)
     <|> try
           (do
-             string "don't()"
-             parseInput isPart1 False)
+             string . pack $ "don't()"
+             modify (second . const $ False)
+             parseInput)
     <|> (do
            printChar
-           parseInput isPart1 isDo)
+           parseInput)
     <|> (do
            eol
-           parseInput isPart1 isDo)
+           parseInput)
     <|> (do
            eof
            return 0)
 
-parseMul :: Bool -> Bool -> Parser Int
-parseMul isPart1 isDo = do
-  string "mul("
+parseMul :: Parser Int
+parseMul = do
+  (isPart1, isDo) <- get
+  string . pack $ "mul("
   a <- decimal
   char ','
   b <- decimal
   char ')'
   if isPart1 || isDo
-    then ((a * b) +) <$> parseInput isPart1 isDo
-    else parseInput isPart1 isDo
+    then ((a * b) +) <$> parseInput
+    else parseInput
 
 part1 :: Bool -> Text -> String
-part1 _ = show . fromRight 0 . parse (parseInput True True) ""
+part1 _ input = show . evalState (runParserT parseInput "" input) $ (True, True)
 
 part2 :: Bool -> Text -> String
-part2 _ = show . fromRight 0 . parse (parseInput False True) ""
+part2 _ input =
+  show . evalState (runParserT parseInput "" input) $ (False, True)
