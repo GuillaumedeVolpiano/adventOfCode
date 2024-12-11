df0eeb7057ac9c93dc65736f36e4afd77491efb6
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-11 12:24:25 +0100

    2024, day 11

--- 2024/days/Day11.hs
+++ 2024/days/Day11.hs
@@ -3,11 +3,50 @@ module Day11
   , part2
   ) where
 
-import           Data.Text       (Text)
-import           Helpers.Parsers.Text
+import           Data.Either          (fromRight)
+import           Data.MultiSet        as MS (MultiSet, delete, distinctElems,
+                                             distinctSize, empty, fromList,
+                                             insert, insertMany, map, occur,
+                                             size)
+import           Data.Text            (Text)
+import           Helpers.Parsers.Text (Parser, decimal)
+import           Text.Megaparsec      (eof, manyTill, parse)
+import           Text.Megaparsec.Char (eol)
+
+import           Debug.Trace
+
+type Stones = MultiSet Int
+
+parseInput :: Parser Stones
+parseInput = fromList <$> manyTill decimal eol
+
+blink :: Stones -> Stones
+blink stones = foldr (collatz stones) empty . distinctElems $ stones
+
+collatz :: Stones -> Int -> Stones -> Stones
+collatz stones stone stones'
+  | stone == 0 = insertMany 1 count stones'
+  | even splitter = foldr (`insertMany` count) stones' split
+  | otherwise = insertMany (2024 * stone) count stones'
+  where
+    count = occur stone stones
+    splitter = ceiling . logBase 10 . fromIntegral $ (stone + 1)
+    split = [stone `mod` 10 ^ div splitter 2, stone `div` 10 ^ div splitter 2]
 
 part1 :: Bool -> Text -> String
-part1 _ _ = "Part 1"
+part1 _ =
+  show
+    . size
+    . (!! 25)
+    . iterate blink
+    . fromRight (error "input could not be parsed")
+    . parse parseInput "day11"
 
 part2 :: Bool -> Text -> String
-part2 _ _ = "Part 2"
+part2 _ =
+  show
+    . size
+    . (!! 75)
+    . iterate blink
+    . fromRight (error "input could not be parsed")
+    . parse parseInput "day11"
