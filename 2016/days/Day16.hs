349e2cbe4bf64dd2b2c5c5755dc9d63934660c11
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-11 22:39:52 +0100

    2016, day 16, ongoing

--- 2016/days/Day16.hs
+++ 2016/days/Day16.hs
@@ -11,7 +11,10 @@ import           Helpers.Parsers.Text
 -- to be continued. The final bits are going to be of the form a · b · a · b · a
 -- ·b…  In the end, there will be k ab pairs, and (2k - 1) separators.The total
 -- length of the filler will be 2k (length a + 1) - 1, that is 2k length a - 1,
--- which will then need to be trimmed to the actual length of the filler.
+-- which will then need to be trimmed to the actual length of the filler. The
+-- size of the chunks will be the highest power of 2 that divide the disk size.
+-- The parity of the chunk will be determined by the parity of the separators +
+-- that of the potential trailing or starting parts of an ab pair.
 
 bitSize :: Int -> Int
 bitSize = (1 +) . floor . logBase 2 . fromIntegral . (1 +)
