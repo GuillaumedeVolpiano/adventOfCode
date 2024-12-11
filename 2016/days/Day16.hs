1f1675c4af584d6913aa4714bace4e807b667ce2
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-11 22:37:59 +0100

    2016, day 16, ongoing

--- 2016/days/Day16.hs
+++ 2016/days/Day16.hs
@@ -3,11 +3,26 @@ module Day16
   , part2
   ) where
 
-import           Data.Text            (Text)
+import           Data.Bits            (bit, complement, shiftL, shiftR)
+import           Data.Char            (intToDigit)
+import           Data.Text            (Text, unpack)
 import           Helpers.Parsers.Text
 
+-- to be continued. The final bits are going to be of the form a · b · a · b · a
+-- ·b…  In the end, there will be k ab pairs, and (2k - 1) separators.The total
+-- length of the filler will be 2k (length a + 1) - 1, that is 2k length a - 1,
+-- which will then need to be trimmed to the actual length of the filler.
+
+bitSize :: Int -> Int
+bitSize = (1 +) . floor . logBase 2 . fromIntegral . (1 +)
+
+bitReverse :: Int -> Int -> Int
+bitReverse a b
+  | a == 0 = b
+  | otherwise = bitReverse (shiftR a 1) . (+ shiftL b 1) $ a `mod` 2
+
 part1 :: Bool -> Text -> String
-part1 _ _ = "Part 1"
+part1 _ = show . bitSize . read . init . unpack
 
 part2 :: Bool -> Text -> String
 part2 _ _ = "Part 2"
