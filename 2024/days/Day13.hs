080042aa804d5990d48fef59a9e1fc4863485509
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-13 12:33:38 +0100

    2024, day 13, using built in functions

--- 2024/days/Day13.hs
+++ 2024/days/Day13.hs
@@ -7,11 +7,13 @@ import           Control.Applicative        (empty)
 import           Data.Char                  (isAlpha)
 import           Data.Either                (fromRight)
 import           Data.Maybe                 (mapMaybe)
-import           Data.Ratio                 (denominator, numerator, (%))
+import           Data.Ratio                 (Ratio, denominator, numerator, (%))
 import           Data.Text                  (Text)
 import           Helpers.Graph              (Pos, origin)
 import           Helpers.Parsers.Text       (Parser)
 import           Helpers.Search             (dijkstraGoalValSafe)
+import           Linear.Matrix              (det22, inv22, transpose, (!*))
+import           Linear.V1                  (V1 (..))
 import           Linear.V2                  (V2 (..))
 import           Text.Megaparsec            (eof, manyTill, optional, parse,
                                              takeWhileP)
@@ -22,54 +24,48 @@ data ClawMachine =
   ClawMachine A B Prize
   deriving (Show, Eq)
 
-type A = V2 Int
+type A = V2 (Ratio Int)
 
-type B = V2 Int
+type B = V2 (Ratio Int)
 
-type Prize = V2 Int
+type Prize = V2 (Ratio Int)
 
 space :: Parser ()
 space = do
   _ <- takeWhileP Nothing (\c -> isAlpha c || c `elem` " ' :+,=")
   return ()
 
+parseTwo :: Parser (V2 (Ratio Int))
+parseTwo = do
+  space
+  x <- L.decimal
+  space
+  y <- L.decimal
+  eol
+  return (V2 x y)
+
 parseClawMachine :: Parser ClawMachine
 parseClawMachine = do
-  space
-  xa <- L.decimal
-  space
-  ya <- L.decimal
-  eol
-  space
-  xb <- L.decimal
-  space
-  yb <- L.decimal
-  eol
-  space
-  xp <- L.decimal
-  space
-  yp <- L.decimal
-  eol
+  a <- parseTwo
+  b <- parseTwo
+  prize <- parseTwo
   optional eol
-  return $ ClawMachine (V2 xa ya) (V2 xb yb) (V2 xp yp)
+  return $ ClawMachine a b prize
 
 parseInput :: Parser [ClawMachine]
 parseInput = manyTill parseClawMachine eof
 
 getPrize :: ClawMachine -> Maybe Int
-getPrize (ClawMachine (V2 xa ya) (V2 xb yb) (V2 xp yp))
-  | det == 0 = Nothing
-  | denominator na == 1 && denominator nb == 1 =
-    Just $ 3 * numerator na + numerator nb
-  | otherwise = Nothing
+getPrize (ClawMachine a b prize)
+  | det22 matrix == 0 || denominator na /= 1 || denominator nb /= 1 = Nothing
+  | otherwise = Just $ 3 * numerator na + numerator nb
   where
-    det = xa * yb - xb * ya
-    na = (yb % det) * (xp % 1) - (xb % det) * (yp % 1)
-    nb = (xa % det) * (yp % 1) - (ya % det) * (xp % 1)
+    matrix = V2 a b
+    (V2 na nb) = (inv22 . transpose $ matrix) !* prize
 
 farAwayPrize :: ClawMachine -> ClawMachine
 farAwayPrize (ClawMachine a b (V2 xp yp)) =
-  ClawMachine a b $ V2 (xp + 10000000000000) (yp + 10000000000000)
+  ClawMachine a b $ V2 (xp + 10 ^ 13) (yp + 10 ^ 13)
 
 part1 :: Bool -> Text -> String
 part1 _ =
