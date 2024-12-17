7c520255b392070e387f7c15f06903cdbdbb87b2
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-17 15:05:02 +0100

    2016, starting on day 17

--- 2016/days/Day17.hs
+++ 2016/days/Day17.hs
@@ -3,8 +3,15 @@ module Day17
   , part2
   ) where
 
-import           Data.Text as T (Text, init, unpack)
-import           MD5       (md5Concat)
+import           Data.Text     as T (Text, init, unpack)
+import           Helpers.Graph (Pos, east, north, south, west)
+import           MD5           (md5Concat)
+
+data Search =
+  Search Pos Path
+  deriving (Show, Eq, Ord)
+
+type Path = String
 
 part1 :: Bool -> Text -> String
 part1 _ = unpack . T.init
