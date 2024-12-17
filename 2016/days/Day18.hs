406a5c3b530e4548a78d0b337f6b7dadc1b10f23
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-17 19:19:13 +0100

    2016, day 18

--- 2016/days/Day18.hs
+++ 2016/days/Day18.hs
@@ -3,11 +3,52 @@ module Day18
   , part2
   ) where
 
-import           Data.Text            (Text)
-import           Helpers.Parsers.Text
+import           Data.Text      as T (Text, init, unpack)
+
+expand :: [Bool] -> [Bool]
+expand line = gol (False : line)
+
+gol :: [Bool] -> [Bool]
+gol [x, y] = [x]
+gol (x:xs@(y:z:_)) =
+  ((x && not y && not z)
+     || (x && y && not z)
+     || (not x && y && z)
+     || (not x && not y && z))
+    : gol xs
+
+render :: [[Bool]] -> String
+render =
+  unlines
+    . map
+        (map
+           (\x ->
+              if x
+                then '^'
+                else '.'))
 
 part1 :: Bool -> Text -> String
-part1 _ _ = "Part 1"
+part1 test =
+  show
+    . sum
+    . map (length . filter not)
+    . take n
+    . iterate expand
+    . map (== '^')
+    . unpack
+    . T.init
+  where
+    n
+      | test = 10
+      | otherwise = 40
 
 part2 :: Bool -> Text -> String
-part2 _ _ = "Part 2"
+part2 _ =
+  show
+    . sum
+    . map (length . filter not)
+    . take 400000
+    . iterate expand
+    . map (== '^')
+    . unpack
+    . T.init
