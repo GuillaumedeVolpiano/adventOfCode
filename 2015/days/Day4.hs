5ba40ca0d6fa0ae6f4c37dc70325571aada96d3e
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-17 19:24:16 +0100

    creating 2015

---
+++ 2015/days/Day4.hs
@@ -0,0 +1,13 @@
+module Day4
+  ( part1
+  , part2
+  ) where
+
+import           Data.Text            (Text)
+import           Helpers.Parsers.Text
+
+part1 :: Bool -> Text -> String
+part1 _ _ = "Part 1"
+
+part2 :: Bool -> Text -> String
+part2 _ _ = "Part 2"
