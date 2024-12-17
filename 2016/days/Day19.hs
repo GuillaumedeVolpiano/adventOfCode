b139f4c1f665a13b37e9cf479132921bb041f9f2
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-17 21:39:51 +0100

    2016, day 19

--- 2016/days/Day19.hs
+++ 2016/days/Day19.hs
@@ -3,11 +3,46 @@ module Day19
   , part2
   ) where
 
+import           Data.Sequence        as Sq (Seq ((:<|), (:|>)), fromList,
+                                             length, splitAt, (><))
 import           Data.Text            (Text)
-import           Helpers.Parsers.Text
+import           Helpers.Parsers.Text (signedInts)
+
+type Elf = Int
+
+type Index = Int
+
+type Presents = Int
+
+makeElves :: Int -> Seq Elf
+makeElves total = fromList [1 .. total]
+
+distribute :: Seq Elf -> Int
+distribute elves
+  | Sq.length elves == 1 = e
+  | otherwise = distribute elves''
+  where
+    (e :<| elves') = elves
+    (l :<| rest) = elves'
+    elves'' = rest :|> e
+
+distributeOpposite :: Seq Elf -> Int
+distributeOpposite elves
+  | Sq.length elves == 1 = e
+  | otherwise = distributeOpposite elves''
+  where
+    (e :<| elves') = elves
+    mid = div (Sq.length elves') 2
+    (before, loser, after)
+      | even (Sq.length elves') = (b, l, a)
+      | odd (Sq.length elves') = (b', l', a')
+      where
+        (b :|> l, a) = Sq.splitAt mid elves'
+        (b', l' :<| a') = Sq.splitAt mid elves'
+    elves'' = before >< (after :|> e)
 
 part1 :: Bool -> Text -> String
-part1 _ _ = "Part 1"
+part1 _ = show . distribute . makeElves . head . head . signedInts
 
 part2 :: Bool -> Text -> String
-part2 _ _ = "Part 2"
+part2 _ = show . distributeOpposite . makeElves . head . head . signedInts
