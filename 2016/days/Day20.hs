b0585698c691cc272d4b979d481f9cb7ee31a992
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2024-12-18 21:38:34 +0100

    2016, day 20,update

--- 2016/days/Day20.hs
+++ 2016/days/Day20.hs
@@ -33,23 +33,23 @@ parseRange = do
   eol
   return $ Range a b
 
-merge :: [Range] -> [Range]
-merge [x] = [x]
-merge (r1@(Range a b):rest@(r2@(Range c d):rest'))
-  | c > b + 1 = r1 : merge rest
-  | b >= d = merge $ r1 : rest'
-  | otherwise = merge $ Range a d : rest'
+merge :: Range -> [Range] -> [Range]
+merge x [] = [x]
+merge r1@(Range a b) rest@(r2@(Range c d):rest')
+  | c > b + 1 = r1 : rest
+  | b >= d = merge r1 rest'
+  | otherwise = merge (Range a d) rest'
 
-findFree :: [Range] -> [Int]
-findFree [x]                            = []
-findFree (Range _ a:rest@(Range b _:_)) = [a + 1 .. b - 1] ++ findFree rest
+findFree :: [Range] -> Int
+findFree [x]                            = 0
+findFree (Range _ a:rest@(Range b _:_)) = b - a - 1 + findFree rest
 
 part1 :: Bool -> Text -> String
 part1 _ =
   show
     . (\(Range _ b) -> b + 1)
     . head
-    . merge
+    . foldr merge []
     . sort
     . fromRight (error "parse failed")
     . parse parseInput "day20"
@@ -57,9 +57,8 @@ part1 _ =
 part2 :: Bool -> Text -> String
 part2 _ =
   show
-    . length
     . findFree
-    . merge
+    . foldr merge []
     . sort
     . fromRight (error "parsefailed")
     . parse parseInput "20"
