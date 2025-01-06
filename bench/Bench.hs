42977f74592580034c165295860f63eed7a63c28
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-06 19:04:49 +0100

    2024, updating cabal file

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -2,18 +2,18 @@ module Main where
 
 import           Data.Text         as T (Text)
 import qualified Data.Text.IO.Utf8 as TIO (readFile)
-import qualified Day6
+import qualified Day20
 import           System.Directory  (getHomeDirectory)
 import           Test.Tasty.Bench  (Benchmark, bcompare, bench, bgroup,
                                     defaultMain, nf)
 
-inputPath = "/adventOfCode/input/2024/day6.txt"
+inputPath = "/adventOfCode/input/2024/day20.txt"
 
 
 tests :: Text -> [Benchmark]
 tests input =
-  [ bench "part 1" $ nf (Day6.part1 False) input
-  , bench "part 2" $ nf (Day6.part2 False) input
+  [ bench "part 1" $ nf (Day20.part1 False) input
+  , bench "part 2" $ nf (Day20.part2 False) input
   ]
 
 main :: IO ()
