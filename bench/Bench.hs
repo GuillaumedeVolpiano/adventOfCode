0b1037b645c21b436bb623b330626a2434a37c13
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-07 20:16:29 +0100

    updating bench

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -2,18 +2,17 @@ module Main where
 
 import           Data.Text         as T (Text)
 import qualified Data.Text.IO.Utf8 as TIO (readFile)
-import qualified Day20
+import qualified Day22
 import           System.Directory  (getHomeDirectory)
 import           Test.Tasty.Bench  (Benchmark, bcompare, bench, bgroup,
                                     defaultMain, nf)
 
-inputPath = "/adventOfCode/input/2024/day20.txt"
-
+inputPath = "/adventOfCode/input/2024/day22.txt"
 
 tests :: Text -> [Benchmark]
 tests input =
-  [ bench "part 1" $ nf (Day20.part1 False) input
-  , bench "part 2" $ nf (Day20.part2 False) input
+  [ bench "part 1" $ nf (Day22.part1 False) input
+  , bench "part 2" $ nf (Day22.part2 False) input
   ]
 
 main :: IO ()
