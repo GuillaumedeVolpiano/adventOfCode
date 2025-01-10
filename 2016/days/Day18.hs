0e778b034e3b48ed5791edfb915f0cbcd8f823a1
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-10 17:31:43 +0100

    Optimising Day18

--- 2016/days/Day18.hs
+++ 2016/days/Day18.hs
@@ -3,52 +3,44 @@ module Day18
   , part2
   ) where
 
-import           Data.Text      as T (Text, init, unpack)
+import           Data.Bifunctor        (bimap)
+import           Data.Bits             (popCount, shiftL, shiftR, xor, (.&.),
+                                        (.|.))
+import           Data.Either           (fromRight)
+import           Data.List             (unfoldr)
+import           Data.Text             (Text)
+import           Data.WideWord.Word128 (Word128)
+import           Helpers.Parsers.Text  (Parser)
+import           Text.Megaparsec       (parse, (<|>))
+import           Text.Megaparsec.Char  (char, eol)
 
-expand :: [Bool] -> [Bool]
-expand line = gol (False : line)
+parseInput :: Parser (Word128, Word128)
+parseInput =
+  (char '^'
+     >> bimap ((.|. 1) . flip shiftL 1) ((.|. 1) . flip shiftL 1) <$> parseInput)
+    <|> (char '.' >> bimap (`shiftL` 1) ((.|. 1) . flip shiftL 1) <$> parseInput)
+    <|> (eol >> return (0, 0))
 
-gol :: [Bool] -> [Bool]
-gol [x, y] = [x]
-gol (x:xs@(y:z:_)) =
-  ((x && not y && not z)
-     || (x && y && not z)
-     || (not x && y && z)
-     || (not x && not y && z))
-    : gol xs
+expand :: (Word128, Word128) -> Maybe (Int, (Word128, Word128))
+expand (traps, mask) =
+  Just (popCount (traps `xor` mask), (traps' .&. mask, mask))
+  where
+    traps' = shiftR traps 1 `xor` shiftL traps 1
 
-render :: [[Bool]] -> String
-render =
-  unlines
-    . map
-        (map
-           (\x ->
-              if x
-                then '^'
-                else '.'))
+countSafe :: Int -> Text -> Int
+countSafe height =
+  sum
+    . take height
+    . unfoldr expand
+    . fromRight (error "parser failed")
+    . parse parseInput "day18"
 
 part1 :: Bool -> Text -> String
-part1 test =
-  show
-    . sum
-    . map (length . filter not)
-    . take n
-    . iterate expand
-    . map (== '^')
-    . unpack
-    . T.init
+part1 test = show . countSafe n
   where
     n
       | test = 10
       | otherwise = 40
 
 part2 :: Bool -> Text -> String
-part2 _ =
-  show
-    . sum
-    . map (length . filter not)
-    . take 400000
-    . iterate expand
-    . map (== '^')
-    . unpack
-    . T.init
+part2 _ = show . countSafe 400000
