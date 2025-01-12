127fae8a0918a2ee022b5d2e57e2e4d876835259
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-12 18:28:04 +0100

    2015, day 8, using FlatParse

--- 2015/days/Day8.hs
+++ 2015/days/Day8.hs
@@ -1,13 +1,47 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Day8
   ( part1
   , part2
   ) where
 
-import           Data.ByteString            (ByteString)
-import           Helpers.Parsers.ByteString
+import           Data.ByteString (ByteString)
+import           Data.Void
+import           FlatParse.Basic (Result (OK), anyChar, char, eof, runParser,
+                                  switch, (<|>))
+import qualified FlatParse.Basic as F (Parser)
+
+type Parser = F.Parser Void Int
+
+parseInput :: Parser -> Parser
+parseInput parser = parseLine parser <|> (eof >> return 0)
+
+parseLine :: Parser -> Parser
+parseLine parser = parser <|> ($(char '\n') >> parseInput parser)
+
+parseChar :: Parser
+parseChar =
+  $(switch
+      [|case _ of
+          "\""  -> (+ 1) <$> parseLine parseChar
+          "\\x" -> anyChar >> anyChar >> (+ 3) <$> parseLine parseChar
+          "\\"  -> anyChar >> (+ 1) <$> parseLine parseChar
+          _     -> anyChar >> parseLine parseChar|])
+
+parseChar' :: Parser
+parseChar' =
+  $(switch
+      [|case _ of
+          "\""  -> (+ 2) <$> parseLine parseChar'
+          "\\x" -> anyChar >> anyChar >> (+ 1) <$> parseLine parseChar'
+          "\\"  -> anyChar >> (+ 2) <$> parseLine parseChar'
+          _     -> anyChar >> parseLine parseChar'|])
+
+extract :: Result Void Int -> Int
+extract (OK result _) = result
 
 part1 :: Bool -> ByteString -> String
-part1 _ _ = "Part 1"
+part1 _ = show . extract . runParser (parseInput parseChar)
 
 part2 :: Bool -> ByteString -> String
-part2 _ _ = "Part 2"
+part2 _ = show . extract . runParser (parseInput parseChar')
--- bench/fullyear/FullYear.hs
+++ bench/fullyear/FullYear.hs
@@ -1,9 +1,9 @@
 module Main where
 
 import           Control.Monad.State (evalState)
+import           Data.ByteString     as B (ByteString, pack)
+import qualified Data.ByteString     as B (readFile)
 import           Data.Either         (fromRight)
-import           Data.Text           as T (Text, pack)
-import qualified Data.Text.IO.Utf8   as TIO (readFile)
 import qualified Day1                as D1
 import qualified Day10               as D10
 import qualified Day11               as D11
@@ -84,151 +84,151 @@ input24 = "/adventOfCode/input/2024/day24.txt"
 
 input25 = "/adventOfCode/input/2024/day25.txt"
 
-tests1 :: Text -> [Benchmark]
+tests1 :: ByteString -> [Benchmark]
 tests1 input =
   [ bench " Day 1, part 1" $ nf (D1.part1 False) input
   , bench " Day 1, part 2" $ nf (D1.part2 False) input
   ]
 
-tests2 :: Text -> [Benchmark]
+tests2 :: ByteString -> [Benchmark]
 tests2 input =
   [ bench " Day 2, part 1" $ nf (D2.part1 False) input
   , bench " Day 2, part 2" $ nf (D2.part2 False) input
   ]
 
-tests3 :: Text -> [Benchmark]
+tests3 :: ByteString -> [Benchmark]
 tests3 input =
   [ bench " Day 3, part 1" $ nf (D3.part1 False) input
   , bench " Day 3, part 2" $ nf (D3.part2 False) input
   ]
 
-tests4 :: Text -> [Benchmark]
+tests4 :: ByteString -> [Benchmark]
 tests4 input =
   [ bench " Day 4, part 1" $ nf (D4.part1 False) input
   , bench " Day 4, part 2" $ nf (D4.part2 False) input
   ]
 
-tests5 :: Text -> [Benchmark]
+tests5 :: ByteString -> [Benchmark]
 tests5 input =
   [ bench " Day 5, part 1" $ nf (D5.part1 False) input
   , bench " Day 5, part 2" $ nf (D5.part2 False) input
   ]
 
-tests6 :: Text -> [Benchmark]
+tests6 :: ByteString -> [Benchmark]
 tests6 input =
   [ bench " Day 6, part 1" $ nf (D6.part1 False) input
   , bench " Day 6, part 2" $ nf (D6.part2 False) input
   ]
 
-tests7 :: Text -> [Benchmark]
+tests7 :: ByteString -> [Benchmark]
 tests7 input =
   [ bench " Day 7, part 1" $ nf (D7.part1 False) input
   , bench " Day 7, part 2" $ nf (D7.part2 False) input
   ]
 
-tests8 :: Text -> [Benchmark]
+tests8 :: ByteString -> [Benchmark]
 tests8 input =
   [ bench " Day 8, part 1" $ nf (D8.part1 False) input
   , bench " Day 8, part 2" $ nf (D8.part2 False) input
   ]
 
-tests9 :: Text -> [Benchmark]
+tests9 :: ByteString -> [Benchmark]
 tests9 input =
   [ bench " Day 9, part 1" $ nf (D9.part1 False) input
   , bench " Day 9, part 2" $ nf (D9.part2 False) input
   ]
 
-tests10 :: Text -> [Benchmark]
+tests10 :: ByteString -> [Benchmark]
 tests10 input =
   [ bench " Day 10, part 1" $ nf (D10.part1 False) input
   , bench " Day 10, part 2" $ nf (D10.part2 False) input
   ]
 
-tests11 :: Text -> [Benchmark]
+tests11 :: ByteString -> [Benchmark]
 tests11 input =
   [ bench " Day 11, part 1" $ nf (D11.part1 False) input
   , bench " Day 11, part 2" $ nf (D11.part2 False) input
   ]
 
-tests12 :: Text -> [Benchmark]
+tests12 :: ByteString -> [Benchmark]
 tests12 input =
   [ bench " Day 12, part 1" $ nf (D12.part1 False) input
   , bench " Day 12, part 2" $ nf (D12.part2 False) input
   ]
 
-tests13 :: Text -> [Benchmark]
+tests13 :: ByteString -> [Benchmark]
 tests13 input =
   [ bench " Day 13, part 1" $ nf (D13.part1 False) input
   , bench " Day 13, part 2" $ nf (D13.part2 False) input
   ]
 
-tests14 :: Text -> [Benchmark]
+tests14 :: ByteString -> [Benchmark]
 tests14 input =
   [ bench " Day 14, part 1" $ nf (D14.part1 False) input
   , bench " Day 14, part 2" $ nf (D14.part2 False) input
   ]
 
-tests15 :: Text -> [Benchmark]
+tests15 :: ByteString -> [Benchmark]
 tests15 input =
   [ bench " Day 15, part 1" $ nf (D15.part1 False) input
   , bench " Day 15, part 2" $ nf (D15.part2 False) input
   ]
 
-tests16 :: Text -> [Benchmark]
+tests16 :: ByteString -> [Benchmark]
 tests16 input =
   [ bench " Day 16, part 1" $ nf (D16.part1 False) input
   , bench " Day 16, part 2" $ nf (D16.part2 False) input
   ]
 
-tests17 :: Text -> [Benchmark]
+tests17 :: ByteString -> [Benchmark]
 tests17 input =
   [ bench " Day 17, part 1" $ nf (D17.part1 False) input
   , bench " Day 17, part 2" $ nf (D17.part2 False) input
   ]
 
-tests18 :: Text -> [Benchmark]
+tests18 :: ByteString -> [Benchmark]
 tests18 input =
   [ bench " Day 18, part 1" $ nf (D18.part1 False) input
   , bench " Day 18, part 2" $ nf (D18.part2 False) input
   ]
 
-tests19 :: Text -> [Benchmark]
+tests19 :: ByteString -> [Benchmark]
 tests19 input =
   [ bench " Day 19, part 1" $ nf (D19.part1 False) input
   , bench " Day 19, part 2" $ nf (D19.part2 False) input
   ]
 
-tests20 :: Text -> [Benchmark]
+tests20 :: ByteString -> [Benchmark]
 tests20 input =
   [ bench " Day 20, part 1" $ nf (D20.part1 False) input
   , bench " Day 20, part 2" $ nf (D20.part2 False) input
   ]
 
-tests21 :: Text -> [Benchmark]
+tests21 :: ByteString -> [Benchmark]
 tests21 input =
   [ bench " Day 21, part 1" $ nf (D21.part1 False) input
   , bench " Day 21, part 2" $ nf (D21.part2 False) input
   ]
 
-tests22 :: Text -> [Benchmark]
+tests22 :: ByteString -> [Benchmark]
 tests22 input =
   [ bench " Day 22, part 1" $ nf (D22.part1 False) input
   , bench " Day 22, part 2" $ nf (D22.part2 False) input
   ]
 
-tests23 :: Text -> [Benchmark]
+tests23 :: ByteString -> [Benchmark]
 tests23 input =
   [ bench " Day 23, part 1" $ nf (D23.part1 False) input
   , bench " Day 23, part 2" $ nf (D23.part2 False) input
   ]
 
-tests24 :: Text -> [Benchmark]
+tests24 :: ByteString -> [Benchmark]
 tests24 input =
   [ bench " Day 24, part 1" $ nf (D24.part1 False) input
   , bench " Day 24, part 2" $ nf (D24.part2 False) input
   ]
 
-tests25 :: Text -> [Benchmark]
+tests25 :: ByteString -> [Benchmark]
 tests25 input =
   [ bench " Day 25, part 1" $ nf (D25.part1 False) input
   , bench " Day 25, part 2" $ nf (D25.part2 False) input
@@ -236,31 +236,31 @@ tests25 input =
 
 main = do
   home <- getHomeDirectory
-  file1 <- TIO.readFile $ home ++ input1
-  file2 <- TIO.readFile $ home ++ input2
-  file3 <- TIO.readFile $ home ++ input3
-  file4 <- TIO.readFile $ home ++ input4
-  file5 <- TIO.readFile $ home ++ input5
-  file6 <- TIO.readFile $ home ++ input6
-  file7 <- TIO.readFile $ home ++ input7
-  file8 <- TIO.readFile $ home ++ input8
-  file9 <- TIO.readFile $ home ++ input9
-  file10 <- TIO.readFile $ home ++ input10
-  file11 <- TIO.readFile $ home ++ input11
-  file12 <- TIO.readFile $ home ++ input12
-  file13 <- TIO.readFile $ home ++ input13
-  file14 <- TIO.readFile $ home ++ input14
-  file15 <- TIO.readFile $ home ++ input15
-  file16 <- TIO.readFile $ home ++ input16
-  file17 <- TIO.readFile $ home ++ input17
-  file18 <- TIO.readFile $ home ++ input18
-  file19 <- TIO.readFile $ home ++ input19
-  file20 <- TIO.readFile $ home ++ input20
-  file21 <- TIO.readFile $ home ++ input21
-  file22 <- TIO.readFile $ home ++ input22
-  file23 <- TIO.readFile $ home ++ input23
-  file24 <- TIO.readFile $ home ++ input24
-  file25 <- TIO.readFile $ home ++ input25
+  file1 <- B.readFile $ home ++ input1
+  file2 <- B.readFile $ home ++ input2
+  file3 <- B.readFile $ home ++ input3
+  file4 <- B.readFile $ home ++ input4
+  file5 <- B.readFile $ home ++ input5
+  file6 <- B.readFile $ home ++ input6
+  file7 <- B.readFile $ home ++ input7
+  file8 <- B.readFile $ home ++ input8
+  file9 <- B.readFile $ home ++ input9
+  file10 <- B.readFile $ home ++ input10
+  file11 <- B.readFile $ home ++ input11
+  file12 <- B.readFile $ home ++ input12
+  file13 <- B.readFile $ home ++ input13
+  file14 <- B.readFile $ home ++ input14
+  file15 <- B.readFile $ home ++ input15
+  file16 <- B.readFile $ home ++ input16
+  file17 <- B.readFile $ home ++ input17
+  file18 <- B.readFile $ home ++ input18
+  file19 <- B.readFile $ home ++ input19
+  file20 <- B.readFile $ home ++ input20
+  file21 <- B.readFile $ home ++ input21
+  file22 <- B.readFile $ home ++ input22
+  file23 <- B.readFile $ home ++ input23
+  file24 <- B.readFile $ home ++ input24
+  file25 <- B.readFile $ home ++ input25
   defaultMain
     $ concat
         [ tests1 file1
