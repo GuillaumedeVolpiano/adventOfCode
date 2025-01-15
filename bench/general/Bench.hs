aba498af736074f19510057bd3df4136e9e0aa2f
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-15 13:34:19 +0100

    2018, day 18, part 2 off

--- bench/general/Bench.hs
+++ bench/general/Bench.hs
@@ -1,18 +1,33 @@
 module Main where
 
-import           Data.ByteString  (ByteString)
-import qualified Data.ByteString  as B (putStr, readFile)
+import           Data.ByteString            (ByteString)
+import qualified Data.ByteString            as B (putStr, readFile)
+import qualified Data.ByteString.Char8      as BC (lines, unpack)
+import           Data.ByteString.UTF8       (toString)
+import           Data.Massiv.Array          (Array, Comp (Seq), P, fromLists')
+import           Data.Massiv.Core.Index     (Ix2)
+import           Data.Text.Encoding         (decodeUtf8)
 import           Day12
-import           System.Directory (getHomeDirectory)
-import           Test.Tasty.Bench (Benchmark, bcompare, bench, bgroup,
-                                   defaultMain, nf, nfIO)
+import qualified Helpers.Parsers            as P (arrayFromString)
+import qualified Helpers.Parsers.ByteString as B (arrayFromByteString)
+import qualified Helpers.Parsers.Text       as T (arrayFromText)
+import           System.Directory           (getHomeDirectory)
+import           Test.Tasty.Bench           (Benchmark, bcompare, bench, bgroup,
+                                             defaultMain, nf, nfIO, whnf)
 
-inputPath = "/adventOfCode/input/2015/day12.txt"
+inputPath = "/adventOfCode/input/2015/day18.txt"
 
 tests :: ByteString -> [Benchmark]
 tests input =
-  [ bench "Part 1" $ nf (part1 True) input
-  , bench "Part 2" $ nf (part2 True) input
+  [ bench "String" $ whnf (P.arrayFromString . toString) input
+  , bench "Text" $ whnf (T.arrayFromText . decodeUtf8) input
+  , bench "ByteString" $ whnf B.arrayFromByteString input
+  , bench "Massiv"
+      $ nf
+          ((fromLists' Seq :: [String] -> Array P Ix2 Char)
+             . map BC.unpack
+             . BC.lines)
+          input
   ]
 
 main :: IO ()
--- lib/Helpers/Parsers/FlatParse.hs
+++ lib/Helpers/Parsers/FlatParse.hs
@@ -11,3 +11,4 @@ type Parser = F.Parser Void
 
 extract :: Result Void a -> a
 extract (OK result _) = result
+extract _             = error "parser failed"
