a63c64c75740fbb1abf4a306babb485bd0f04ec7
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-18 09:01:00 +0100

    2015, day 24

--- 2015/days/Day23.hs
+++ 2015/days/Day23.hs
@@ -4,11 +4,20 @@ module Day23
   ) where
 
 import           Data.ByteString           (ByteString)
+import           Data.List                 (unfoldr)
 import           Helpers.Parsers.FlatParse (extract)
 import qualified Helpers.Parsers.FlatParse as F (Parser)
 
+-- The program sets a to 4591 if a is 0, 113383 if a is 1, then calculate the
+-- collatz sequence of the number, using b to count the number of steps.
+collatz :: Int -> Maybe (Int, Int)
+collatz 1 = Nothing
+collatz k
+  | even k = Just (div k 2, div k 2)
+  | otherwise = Just (3 * k + 1, 3 * k + 1)
+
 part1 :: Bool -> ByteString -> String
-part1 _ _ = "Part 1"
+part1 _ _ = show . length . unfoldr collatz $ 4591
 
 part2 :: Bool -> ByteString -> String
-part2 _ _ = "Part 2"
+part2 _ _ = show . length . unfoldr collatz $ 113383
--- 2015/days/Day24.hs
+++ 2015/days/Day24.hs
@@ -4,11 +4,34 @@ module Day24
   ) where
 
 import           Data.ByteString           (ByteString)
-import           Helpers.Parsers.FlatParse (extract)
-import qualified Helpers.Parsers.FlatParse as F (Parser)
+import           Data.List                 (sortBy, subsequences)
+import           Data.Ord                  (comparing)
+import           FlatParse.Basic           (anyAsciiDecimalInt, runParser, skip,
+                                            some)
+import           Helpers.Parsers.FlatParse (Parser, extract)
+
+subsequencesOfLength :: Int -> [a] -> [[a]]
+subsequencesOfLength 0 _ = []
+subsequencesOfLength l xs@(x:xs')
+  | length xs == l = [xs]
+  | otherwise =
+    map (x :) (subsequencesOfLength (l - 1) xs') ++ subsequencesOfLength l xs'
+
+divideIn :: Int -> [Int] -> Int
+divideIn x packages = minimum . map product . head $ subsequences
+  where
+    partSize = flip div x . sum $ packages
+    subsequences =
+      dropWhile null
+        . map
+            (filter ((== partSize) . sum) . flip subsequencesOfLength packages)
+        $ [1 ..]
+
+parseInput :: Parser [Int]
+parseInput = some (anyAsciiDecimalInt <* skip 1)
 
 part1 :: Bool -> ByteString -> String
-part1 _ _ = "Part 1"
+part1 _ = show . divideIn 3 . extract . runParser parseInput
 
 part2 :: Bool -> ByteString -> String
-part2 _ _ = "Part 2"
+part2 _ = show . divideIn 4 . extract . runParser parseInput
