5013a5971aba27b7b3e782888996162a3a32748b
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-18 09:31:03 +0100

    2015, day 25

--- 2015/days/Day25.hs
+++ 2015/days/Day25.hs
@@ -1,14 +1,45 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day25
   ( part1
   , part2
   ) where
 
+import           Control.Monad             (void)
 import           Data.ByteString           (ByteString)
-import           Helpers.Parsers.FlatParse (extract)
-import qualified Helpers.Parsers.FlatParse as F (Parser)
+import           FlatParse.Basic           (anyAsciiDecimalInt, isDigit,
+                                            runParser, skipSatisfy, some)
+import           Helpers.Parsers.FlatParse (Parser, extract)
+
+modulus = 33554393
+
+operand = 252533
+
+startPos = (1, 1)
+
+startVal = 20151125
+
+consume :: Parser ()
+consume = void . some $ skipSatisfy (not . isDigit)
+
+parseInput :: Parser (Int, Int)
+parseInput =
+  consume
+    >> anyAsciiDecimalInt
+    >>= \row -> consume >> (row, ) <$> anyAsciiDecimalInt
+
+encode :: Int -> Int
+encode = (`mod` modulus) . (* operand)
+
+calculate :: (Int, Int) -> (Int, Int) -> Int -> Int
+calculate goal cur@(row, col) curVal
+  | cur == goal = curVal
+  | row == 1 = calculate goal (col + 1, 1) . encode $ curVal
+  | otherwise = calculate goal (row - 1, col + 1) . encode $ curVal
 
 part1 :: Bool -> ByteString -> String
-part1 _ _ = "Part 1"
+part1 _ =
+  show . (\x -> calculate x startPos startVal) . extract . runParser parseInput
 
 part2 :: Bool -> ByteString -> String
 part2 _ _ = "Part 2"
