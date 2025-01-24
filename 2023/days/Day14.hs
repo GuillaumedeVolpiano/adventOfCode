0b433cbd780ed5695f4c225def6e6cadfb3d18a1
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-24 17:07:42 +0100

    migrating 2023 to ByteString, optimised day 14

--- 2023/days/Day14.hs
+++ 2023/days/Day14.hs
@@ -5,101 +5,141 @@ module Day14
   , part2
   ) where
 
-import           Data.Bifunctor            (first, second)
-import           Data.Bits                 (shiftL, shiftR, (.&.))
+import           Control.Monad             (forM_, replicateM, when)
+import           Control.Monad.ST          (ST, runST)
+import           Data.Array.ST             (STUArray, freeze, getBounds,
+                                            readArray, writeArray)
+import           Data.Array.Unboxed        (UArray, array, bounds, (!))
+import           Data.Array.Unsafe         (unsafeFreeze, unsafeThaw)
 import           Data.ByteString           (ByteString)
-import           Data.IntSet               (IntSet, insert, notMember, union)
-import qualified Data.IntSet               as S (filter, foldr, map)
-import           Data.List                 (foldl')
+import           Data.Ix                   (inRange)
 import           Data.Maybe                (fromJust)
-import           Data.Sequence             as Sq (Seq ((:<|)), drop, iterateN,
-                                                  length, take, takeWhileL,
-                                                  (!?))
-import           Data.Vector.Unboxed       (Vector, generate)
-import qualified Data.Vector.Unboxed       as V (fromList, (!))
-import           FlatParse.Stateful        (eof, get, modify, put, runParser,
-                                            switch, (<|>))
+import           Data.Sequence             ((!?))
+import qualified Data.Sequence             as Sq (fromList)
+import           Data.STRef                (STRef, modifySTRef, newSTRef,
+                                            readSTRef)
+import           FlatParse.Stateful        (ask, char, eof, get, local, modify,
+                                            put, runParser, satisfy, (<|>))
 import           Helpers.Parsers.FlatParse (ParserS, extractS)
 import           Helpers.Search            (findPattern)
+import           Linear.V2                 (V2 (..))
 
-type Rocks = IntSet
+import           Debug.Trace
 
-type Fixed = Vector Bool
+type Position = V2 Int
 
-north = -128
+north = V2 0 1
 
-south = 128
+south = V2 0 (-1)
 
-east = 1
+east = V2 1 0
 
-west = -1
+west = V2 (-1) 0
 
 numCycles = 1000000000
 
-parseInput :: ParserS () (Rocks, IntSet)
-parseInput =
-  $(switch
-      [|case _ of
-          "."  -> modify (+ 1) >> parseInput
-          "O"  -> get >>= \x -> put (x + 1) >> first (insert x) <$> parseInput
-          "#"  -> get >>= \x -> put (x + 1) >> second (insert x) <$> parseInput
-          "\n" -> modify ((+ 128) . (.&. 16256)) >> parseInput|])
-    <|> (eof >> pure (mempty, mempty))
+parseInput :: ParserS Int [(Position, Char)]
+parseInput = (put 1 >> parseLine) <|> (eof >> pure [])
 
-makeVector :: IntSet -> Fixed
-makeVector set = generate (2 ^ 14) (`notMember` set)
+parseLine :: ParserS Int [(Position, Char)]
+parseLine = parseChar <|> ($(char '\n') >> local (+ (-1)) parseInput)
 
-move :: Fixed -> Int -> Rocks -> Rocks
-move fixed dir rocks = allMoved
+parseChar :: ParserS Int [(Position, Char)]
+parseChar =
+  ask >>= \y ->
+    get >>= \x ->
+      put (x + 1)
+        >> satisfy (`elem` ".#O")
+        >>= \c -> ((V2 x y, c) :) <$> parseLine
+
+move :: Position -> STUArray s Position Char -> ST s Int
+move dir dish = do
+  (_, V2 mx my) <- getBounds dish
+  let order
+        | dir == north = [V2 x y | x <- [1 .. mx], y <- [my,my - 1 .. 1]]
+        | dir == west = [V2 x y | y <- [1 .. my], x <- [1 .. mx]]
+        | dir == south = [V2 x y | x <- [1 .. mx], y <- [1 .. my]]
+        | dir == east = [V2 x y | y <- [1 .. my], x <- [mx,mx - 1 .. 1]]
+  score <- newSTRef 0
+  forM_ order $ \pos ->
+    readArray dish pos >>= \cur ->
+      when (cur == 'O') $ moveOne dir dish score pos
+  readSTRef score
+
+moveOne ::
+     Position -> STUArray s Position Char -> STRef s Int -> Position -> ST s ()
+moveOne dir dish score pos@(V2 _ y) =
+  getBounds dish >>= \bounds ->
+    if inRange bounds (pos + dir)
+      then testNext dir dish score pos
+      else modifySTRef score (+ y)
+
+testNext ::
+     Position -> STUArray s Position Char -> STRef s Int -> Position -> ST s ()
+testNext dir dish score pos@(V2 _ y) =
+  readArray dish (pos + dir) >>= \next ->
+    if next == '.'
+      then writeArray dish pos '.'
+             >> writeArray dish (pos + dir) 'O'
+             >> moveOne dir dish score (pos + dir)
+      else modifySTRef score (+ y)
+
+moveNorth :: UArray Position Char -> ST s Int
+moveNorth dish = unsafeThaw dish >>= \d -> move north d
+
+cycleRocks :: STUArray s Position Char -> ST s Int
+cycleRocks dish =
+  move north dish >> move west dish >> move south dish >> move east dish
+
+testCycles :: UArray Position Char -> String
+testCycles dish =
+  prettyPrint array1 ++ "\n" ++ prettyPrint array2 ++ "\n" ++ prettyPrint array3
   where
-    my = shiftL 99 7
-    mx = 99
-    order
-      | dir == north = [my,my - 128 .. my .&. 127]
-      | dir == south = [0,128 .. my]
-      | dir == west = [mx,mx - 1 .. 0]
-      | dir == east = [0 .. mx]
-    coord
-      | dir `elem` [north, south] = flip shiftR 7
-      | otherwise = (.&. 127)
-    allMoved = foldr displaceByRow mempty order
-    displaceByRow x moved = (S.map (fullMove moved) . atX $ x) `union` moved
-    atX x = S.filter (\p -> coord p == coord x) rocks
-    canMove moved p =
-      py <= 99
-        && py >= 0
-        && px <= 99
-        && px >= 0
-        && mp `notMember` moved
-        && fixed V.! mp
-      where
-        mp = p + dir
-        py = shiftR mp 7
-        px = mp .&. 127
-    fullMove moved x
-      | canMove moved x = fullMove moved (x + dir)
-      | otherwise = x
+    (array1, array2, array3) =
+      runST $ do
+        d <- unsafeThaw dish
+        cycleRocks d
+        d1 <- freeze d
+        cycleRocks d
+        d2 <- freeze d
+        cycleRocks d
+        d3 <- freeze d
+        pure (d1, d2, d3)
 
-score :: Rocks -> Int
-score = S.foldr (\p -> (offset - shiftR p 7 +)) 0
+firstCycles' :: UArray Position Char -> ST s [Int]
+firstCycles' dish = do
+  d <- unsafeThaw dish
+  replicateM 250 $ cycleRocks d
+
+prettyPrint :: UArray Position Char -> String
+prettyPrint dish =
+  unlines [[dish ! V2 x y | x <- [1 .. mx]] | y <- [my,my - 1 .. 1]]
   where
-    offset = 100
-
-cycleRocks :: Fixed -> Rocks -> Rocks
-cycleRocks fixed rocks = foldr (move fixed) rocks [east, south, west, north]
+    (_, V2 mx my) = bounds dish
 
 part1 :: Bool -> ByteString -> String
-part1 _ input = show . score . move fixed north $ rocks
+part1 test input =
+  show
+    $ runST
+    $ moveNorth
+        . array (V2 1 1, V2 max max)
+        . extractS
+        . runParser parseInput max 1
+    $ input
   where
-    (rocks, fixed) =
-      second makeVector . extractS . runParser parseInput () 0 $ input
+    max
+      | test = 10
+      | otherwise = 100
 
 part2 :: Bool -> ByteString -> String
-part2 _ input = show pos
+part2 test input = show pos
   where
-    (rocks, fixed) =
-      second makeVector . extractS . runParser parseInput () 0 $ input
-    firstCycles = fmap score . iterateN 250 (cycleRocks fixed) $ rocks
+    dish =
+      array (V2 1 1, V2 max max) . extractS . runParser parseInput max 1 $ input
+    max
+      | test = 10
+      | otherwise = 100
+    firstCycles = Sq.fromList $ runST $ firstCycles' dish
     pat = findPattern 100 1 (==) firstCycles
     remainder = mod (numCycles - 100) pat
-    pos = fromJust $ firstCycles !? (100 + remainder)
+    pos = fromJust $ firstCycles !? (99 + remainder)
