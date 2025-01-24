cdbf537af9a95ac27beba77b5da9402016e60424
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-24 18:13:10 +0100

    migrating 2023 to ByteString, optimised day 14 with Massiv Array

--- 2023/days/Day14.hs
+++ 2023/days/Day14.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module Day14
   ( part1
   , part2
@@ -7,12 +5,15 @@ module Day14
 
 import           Control.Monad             (forM_, replicateM, when)
 import           Control.Monad.ST          (ST, runST)
-import           Data.Array.ST             (STUArray, freeze, getBounds,
-                                            readArray, writeArray)
-import           Data.Array.Unboxed        (UArray, array, bounds, (!))
-import           Data.Array.Unsafe         (unsafeFreeze, unsafeThaw)
 import           Data.ByteString           (ByteString)
+import           Data.ByteString.Char8     (unpack)
+import qualified Data.ByteString.Char8     as B (lines)
 import           Data.Ix                   (inRange)
+import           Data.Massiv.Array         (Array, Comp (Seq), Ix2 ((:.)), P,
+                                            Sz (Sz, Sz2), fromLists')
+import           Data.Massiv.Array.Mutable (MArray, sizeOfMArray, write)
+import qualified Data.Massiv.Array.Mutable as A (read)
+import           Data.Massiv.Array.Unsafe  (unsafeFreeze, unsafeThaw)
 import           Data.Maybe                (fromJust)
 import           Data.Sequence             ((!?))
 import qualified Data.Sequence             as Sq (fromList)
@@ -22,120 +23,72 @@ import           FlatParse.Stateful        (ask, char, eof, get, local, modify,
                                             put, runParser, satisfy, (<|>))
 import           Helpers.Parsers.FlatParse (ParserS, extractS)
 import           Helpers.Search            (findPattern)
-import           Linear.V2                 (V2 (..))
 
-import           Debug.Trace
+type Position = Ix2
 
-type Position = V2 Int
+east = 0 :. 1
 
-north = V2 0 1
+west = 0 :. (-1)
 
-south = V2 0 (-1)
+north = 1 :. 0
 
-east = V2 1 0
-
-west = V2 (-1) 0
+south = (-1) :. 0
 
 numCycles = 1000000000
 
-parseInput :: ParserS Int [(Position, Char)]
-parseInput = (put 1 >> parseLine) <|> (eof >> pure [])
-
-parseLine :: ParserS Int [(Position, Char)]
-parseLine = parseChar <|> ($(char '\n') >> local (+ (-1)) parseInput)
-
-parseChar :: ParserS Int [(Position, Char)]
-parseChar =
-  ask >>= \y ->
-    get >>= \x ->
-      put (x + 1)
-        >> satisfy (`elem` ".#O")
-        >>= \c -> ((V2 x y, c) :) <$> parseLine
-
-move :: Position -> STUArray s Position Char -> ST s Int
+move :: Position -> MArray s P Position Char -> ST s Int
 move dir dish = do
-  (_, V2 mx my) <- getBounds dish
+  let Sz (mx :. my) = sizeOfMArray dish
   let order
-        | dir == north = [V2 x y | x <- [1 .. mx], y <- [my,my - 1 .. 1]]
-        | dir == west = [V2 x y | y <- [1 .. my], x <- [1 .. mx]]
-        | dir == south = [V2 x y | x <- [1 .. mx], y <- [1 .. my]]
-        | dir == east = [V2 x y | y <- [1 .. my], x <- [mx,mx - 1 .. 1]]
+        | dir == north =
+          [y :. x | x <- [0 .. mx - 1], y <- [my - 1,my - 2 .. 0]]
+        | dir == west = [y :. x | y <- [0 .. my - 1], x <- [0 .. mx - 1]]
+        | dir == south = [y :. x | x <- [0 .. mx - 1], y <- [0 .. my - 1]]
+        | dir == east = [y :. x | y <- [0 .. my - 1], x <- [mx - 1,mx - 2 .. 0]]
   score <- newSTRef 0
   forM_ order $ \pos ->
-    readArray dish pos >>= \cur ->
-      when (cur == 'O') $ moveOne dir dish score pos
+    A.read dish pos >>= \cur ->
+      when (cur == Just 'O') $ moveOne dir dish score pos
   readSTRef score
 
 moveOne ::
-     Position -> STUArray s Position Char -> STRef s Int -> Position -> ST s ()
-moveOne dir dish score pos@(V2 _ y) =
-  getBounds dish >>= \bounds ->
-    if inRange bounds (pos + dir)
-      then testNext dir dish score pos
-      else modifySTRef score (+ y)
+     Position -> MArray s P Position Char -> STRef s Int -> Position -> ST s ()
+moveOne dir dish score pos@(y :. _) =
+  testNext dir dish pos >>= \test ->
+    if test
+      then write dish pos '.' >> moveOne dir dish score (pos + dir)
+      else write dish pos 'O' >> modifySTRef score (+ (y + 1))
 
-testNext ::
-     Position -> STUArray s Position Char -> STRef s Int -> Position -> ST s ()
-testNext dir dish score pos@(V2 _ y) =
-  readArray dish (pos + dir) >>= \next ->
-    if next == '.'
-      then writeArray dish pos '.'
-             >> writeArray dish (pos + dir) 'O'
-             >> moveOne dir dish score (pos + dir)
-      else modifySTRef score (+ y)
+testNext :: Position -> MArray s P Position Char -> Position -> ST s Bool
+testNext dir dish pos = (== Just '.') <$> A.read dish (pos + dir)
 
-moveNorth :: UArray Position Char -> ST s Int
+moveNorth :: Array P Position Char -> ST s Int
 moveNorth dish = unsafeThaw dish >>= \d -> move north d
 
-cycleRocks :: STUArray s Position Char -> ST s Int
+cycleRocks :: MArray s P Position Char -> ST s Int
 cycleRocks dish =
   move north dish >> move west dish >> move south dish >> move east dish
 
-testCycles :: UArray Position Char -> String
-testCycles dish =
-  prettyPrint array1 ++ "\n" ++ prettyPrint array2 ++ "\n" ++ prettyPrint array3
-  where
-    (array1, array2, array3) =
-      runST $ do
-        d <- unsafeThaw dish
-        cycleRocks d
-        d1 <- freeze d
-        cycleRocks d
-        d2 <- freeze d
-        cycleRocks d
-        d3 <- freeze d
-        pure (d1, d2, d3)
-
-firstCycles' :: UArray Position Char -> ST s [Int]
+firstCycles' :: Array P Position Char -> ST s [Int]
 firstCycles' dish = do
   d <- unsafeThaw dish
   replicateM 250 $ cycleRocks d
-
-prettyPrint :: UArray Position Char -> String
-prettyPrint dish =
-  unlines [[dish ! V2 x y | x <- [1 .. mx]] | y <- [my,my - 1 .. 1]]
-  where
-    (_, V2 mx my) = bounds dish
 
 part1 :: Bool -> ByteString -> String
 part1 test input =
   show
     $ runST
-    $ moveNorth
-        . array (V2 1 1, V2 max max)
-        . extractS
-        . runParser parseInput max 1
+    $ moveNorth . fromLists' Seq . reverse . map unpack . B.lines
     $ input
-  where
-    max
-      | test = 10
-      | otherwise = 100
 
 part2 :: Bool -> ByteString -> String
 part2 test input = show pos
   where
     dish =
-      array (V2 1 1, V2 max max) . extractS . runParser parseInput max 1 $ input
+      fromLists' Seq . reverse . map unpack . B.lines $ input :: Array
+        P
+        Position
+        Char
     max
       | test = 10
       | otherwise = 100
