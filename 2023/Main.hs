ffdfea32ccf2327461265cbe166ca0e0b022b003
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-25 19:37:40 +0100

    migrating 2023 to ByteString, up to day 16, part 2 to optimise

--- 2023/Main.hs
+++ 2023/Main.hs
@@ -67,7 +67,7 @@ solver =
     , (13, (Day13.part1, Day13.part2))
     , (14, (Day14.part1, Day14.part2))
     , (15, (Day15.part1, Day15.part2))
-    -- , (16, (Day16.part1, Day16.part2))
+    , (16, (Day16.part1, Day16.part2))
     -- , (17, (Day17.part1, Day17.part2))
     -- , (18, (Day18.part1, Day18.part2))
     -- , (19, (Day19.part1, Day19.part2))
--- 2023/days/Day16.hs
+++ 2023/days/Day16.hs
@@ -7,17 +7,21 @@ import           Control.Parallel.Strategies (parMap, rpar)
 import           Data.Array.IArray           as I (Array, array, (!))
 import           Data.Array.Unboxed          as U (Ix, UArray, bounds, inRange,
                                                    range, (!))
+import           Data.ByteString             (ByteString)
 import           Data.List                   as L (map)
 import           Data.Map                    as M (fromList, (!))
 import           Data.Set                    as St (Set, empty, map, singleton,
                                                     size)
-import           Helpers.Parsers             (arrayFromString)
+import           Data.Word                   (Word8)
+import           Data.Word8                  (_backslash, _bar, _hyphen,
+                                              _period, _slash)
+import           Helpers.Parsers.ByteString  (arrayFromByteString)
 import           Helpers.Search              (dfs)
 import           Linear.V2                   (V2 (..))
 
 type Pos = (V2 Int)
 
-type Cave = UArray Pos Char
+type Cave = UArray Pos Word8
 
 data Beam =
   Beam Pos Pos
@@ -37,22 +41,22 @@ startPos = Beam (V2 0 0) east
 
 reflections =
   fromList
-    [ ((north, '/'), east)
-    , ((north, '\\'), west)
-    , ((south, '/'), west)
-    , ((south, '\\'), east)
-    , ((west, '/'), south)
-    , ((west, '\\'), north)
-    , ((east, '/'), north)
-    , ((east, '\\'), south)
+    [ ((north, _slash), east)
+    , ((north, _backslash), west)
+    , ((south, _slash), west)
+    , ((south, _backslash), east)
+    , ((west, _slash), south)
+    , ((west, _backslash), north)
+    , ((east, _slash), north)
+    , ((east, _backslash), south)
     ]
 
 edges :: Cave -> [Beam]
 edges cave =
-  [Beam (V2 x 0) south | x <- [0 .. mx]] ++
-  [Beam (V2 x my) north | x <- [0 .. mx]] ++
-  [Beam (V2 0 y) east | y <- [0 .. my]] ++
-  [Beam (V2 mx y) west | y <- [0 .. my]]
+  [Beam (V2 x 0) south | x <- [0 .. mx]]
+    ++ [Beam (V2 x my) north | x <- [0 .. mx]]
+    ++ [Beam (V2 0 y) east | y <- [0 .. my]]
+    ++ [Beam (V2 mx y) west | y <- [0 .. my]]
   where
     (_, V2 mx my) = bounds cave
 
@@ -61,12 +65,12 @@ neighbours cave beam@(Beam pos dir) =
   filter (\(Beam t _) -> inRange (bounds cave) t) newPos
   where
     newPos
-      | cave U.! pos == '.' ||
-          cave U.! pos == '−' && elem dir [east, west] ||
-          cave U.! pos == '|' && elem dir [north, south] =
+      | cave U.! pos == _period
+          || cave U.! pos == _hyphen && elem dir [east, west]
+          || cave U.! pos == _bar && elem dir [north, south] =
         [Beam (pos + dir) dir]
-      | cave U.! pos == '-' = split [east, west]
-      | cave U.! pos == '|' = split [north, south]
+      | cave U.! pos == _hyphen = split [east, west]
+      | cave U.! pos == _bar = split [north, south]
       | otherwise =
         [ Beam
             (pos + reflections M.! (dir, cave U.! pos))
@@ -77,21 +81,21 @@ neighbours cave beam@(Beam pos dir) =
 toPos :: Beam -> Pos
 toPos (Beam p _) = p
 
-part1 :: Bool -> String -> String
+part1 :: Bool -> ByteString -> String
 part1 _ input =
   show . size . St.map toPos . dfs [startPos] (neighbours cave) $ empty
   where
-    cave = arrayFromString input
+    cave = arrayFromByteString input
 
-part2 :: Bool -> String -> String
+part2 :: Bool -> ByteString -> String
 part2 _ input =
-  show .
-  maximum .
-  parMap rpar (\x -> size . St.map toPos . dfs [x] (allBeams I.!) $ empty) .
-  edges $
-  cave
+  show
+    . maximum
+    . parMap rpar (\x -> size . St.map toPos . dfs [x] (allBeams I.!) $ empty)
+    . edges
+    $ cave
   where
-    cave = arrayFromString input
+    cave = arrayFromByteString input
     (start, end@(V2 mx my)) = bounds cave
     allBeams =
       array
