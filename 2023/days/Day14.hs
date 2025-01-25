40e5f2bc583efcb1f41e4c75fb0beccf628df443
GuillaumedeVolpiano <xavier.dectot@gmail.com>
2025-01-25 13:52:27 +0100

    migrating 2023 to ByteString, optimised day 14 with Massiv Array

--- 2023/days/Day14.hs
+++ 2023/days/Day14.hs
@@ -8,9 +8,8 @@ import           Control.Monad.ST          (ST, runST)
 import           Data.ByteString           (ByteString)
 import           Data.ByteString.Char8     (unpack)
 import qualified Data.ByteString.Char8     as B (lines)
-import           Data.Ix                   (inRange)
 import           Data.Massiv.Array         (Array, Comp (Seq), Ix2 ((:.)), P,
-                                            Sz (Sz, Sz2), fromLists')
+                                            Sz (Sz, Sz2), fromLists', loopA_)
 import           Data.Massiv.Array.Mutable (MArray, sizeOfMArray, write)
 import qualified Data.Massiv.Array.Mutable as A (read)
 import           Data.Massiv.Array.Unsafe  (unsafeFreeze, unsafeThaw)
@@ -39,16 +38,24 @@ numCycles = 1000000000
 move :: Position -> MArray s P Position Char -> ST s Int
 move dir dish = do
   let Sz (mx :. my) = sizeOfMArray dish
-  let order
-        | dir == north =
-          [y :. x | x <- [0 .. mx - 1], y <- [my - 1,my - 2 .. 0]]
-        | dir == west = [y :. x | y <- [0 .. my - 1], x <- [0 .. mx - 1]]
-        | dir == south = [y :. x | x <- [0 .. mx - 1], y <- [0 .. my - 1]]
-        | dir == east = [y :. x | y <- [0 .. my - 1], x <- [mx - 1,mx - 2 .. 0]]
   score <- newSTRef 0
-  forM_ order $ \pos ->
-    A.read dish pos >>= \cur ->
-      when (cur == Just 'O') $ moveOne dir dish score pos
+  let iterator1
+        | dir == north = loopA_ (my - 1) (>= 0) (+ (-1))
+        | dir == west = loopA_ 0 (< mx) (+ 1)
+        | dir == south = loopA_ 0 (< my) (+ 1)
+        | dir == east = loopA_ (mx - 1) (>= 0) (+ (-1))
+      iterator2 coord f
+        | dir == north || dir == south =
+          loopA_ 0 (< mx) (+ 1) (\x -> f (coord :. x))
+        | dir == east || dir == west =
+          loopA_ 0 (< my) (+ 1) (\y -> f (y :. coord))
+  iterator1
+    (\coord ->
+       iterator2
+         coord
+         (\pos ->
+            A.read dish pos >>= \cur ->
+              when (cur == Just 'O') $ moveOne dir dish score pos))
   readSTRef score
 
 moveOne ::
@@ -75,23 +82,20 @@ firstCycles' dish = do
   replicateM 250 $ cycleRocks d
 
 part1 :: Bool -> ByteString -> String
-part1 test input =
+part1 _ input =
   show
     $ runST
     $ moveNorth . fromLists' Seq . reverse . map unpack . B.lines
     $ input
 
 part2 :: Bool -> ByteString -> String
-part2 test input = show pos
+part2 _ input = show pos
   where
     dish =
       fromLists' Seq . reverse . map unpack . B.lines $ input :: Array
         P
         Position
         Char
-    max
-      | test = 10
-      | otherwise = 100
     firstCycles = Sq.fromList $ runST $ firstCycles' dish
     pat = findPattern 100 1 (==) firstCycles
     remainder = mod (numCycles - 100) pat
