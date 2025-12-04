fc822dcbd7dd932df835006f6e4914053691f801
GuillaumedeVolpiano <xavier@wheredoibegin.fr>
2025-12-04 23:11:16 +0100

    Day4, comments

--- 2025/days/Day4.hs
+++ 2025/days/Day4.hs
@@ -1,3 +1,13 @@
+
+
+-- |
+-- Module      : Day4
+-- Description : Grid accessibility analysis using mutable vectors and IntSet frontier propagation.
+--
+-- This module provides two functions ('part1' and 'part2') for solving the
+-- Advent of Code Day 4 problem, measuring accessibility in a grid structure.
+-- The grid is represented as a flat mutable vector, and frontier pruning is
+-- used to efficiently compute reachable or removable cells.
 module Day4
   ( part1
   , part2
@@ -7,32 +17,46 @@ module Day4
   , purge
   ) where
 
-import           Streamly.Data.Stream (Stream)
-import           Data.Word (Word8)
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IS (insert, size)
-import qualified Streamly.Data.Fold as F (foldlM')
-import Streamly.Data.Fold (Fold)
-import qualified Streamly.Data.Stream as S (fold)
-import Data.Bits ((.|.), shiftL)
-import Data.Vector.Unboxed.Mutable (IOVector)
-import Data.Sequence (Seq ((:|>)), ViewL (EmptyL, (:<)))
-import qualified Data.Vector.Unboxed.Mutable as MV (unsafeWrite, new, unsafeRead)
-import qualified Data.Sequence as Seq (viewl)
-import Data.Bifunctor (first)
-import Control.DeepSeq (NFData, rnf)
-import Control.Monad (foldM)
+import           Control.DeepSeq             (NFData, rnf)
+import           Control.Monad               (foldM)
+import           Data.Bifunctor              (first)
+import           Data.Bits                   (shiftL, (.|.))
+import           Data.IntSet                 (IntSet)
+import qualified Data.IntSet                 as IS (insert, size)
+import           Data.Sequence               (Seq ((:|>)), ViewL (EmptyL, (:<)))
+import qualified Data.Sequence               as Seq (viewl)
+import           Data.Vector.Unboxed.Mutable (IOVector)
+import qualified Data.Vector.Unboxed.Mutable as MV (new, unsafeRead,
+                                                    unsafeWrite)
+import           Data.Word                   (Word8)
+import qualified Streamly.Data.Fold          as F (foldlM')
+import           Streamly.Data.Fold          (Fold)
+import qualified Streamly.Data.Stream        as S (fold)
+import           Streamly.Data.Stream        (Stream)
 
+-- | State during processing:
+--
+-- * A mutable occupancy grid
+-- * A frontier queue of positions to check
+-- * A set of confirmed \"freed\" positions
 data ForkRolls = FR !(IOVector Bool) !(Seq Int) !IntSet
 
+-- | For benchmarks
 instance NFData ForkRolls where
   rnf (FR _ sq st) = rnf sq `seq` rnf st
 
+-- | Iteratively prune the frontier:
+--
+-- * If a cell has fewer than 4 occupied neighbors, it becomes \"free\"
+-- * The freed cell is marked False and added to the free set
+-- * Newly affected neighbors are appended to the frontier
+--
+-- Continues until no more cells can be removed.
 purge :: ForkRolls -> IO IntSet
-purge (FR rs rseq fs) = case Seq.viewl rseq of 
+purge (FR rs rseq fs) = case Seq.viewl rseq of
                           EmptyL -> pure fs
                           (next :< rest) -> do
-                            stillOccupied <- MV.unsafeRead rs next :: IO Bool
+                            stillOccupied <- MV.unsafeRead rs next :: IO Bool
                             if stillOccupied then do
                                              let checkFree :: Int -> [Int] -> IO (Maybe (Seq Int, IntSet))
                                                  checkFree 4 _ = pure Nothing
@@ -43,7 +67,7 @@ purge (FR rs rseq fs) = case Seq.viewl rseq of
                                                   | next + x < 0 = checkFree c xs
                                                   | otherwise = do
                                                       occ <- MV.unsafeRead rs (next + x)
-                                                      if occ then fmap (first (:|> (next +  x))) 
+                                                      if occ then fmap (first (:|> (next +  x)))
                                                                     <$> checkFree (c + 1) xs
                                                              else checkFree c xs
                                              res <- checkFree 0 [-255, -256, -257, -1, 1, 255, 256, 257]
@@ -52,6 +76,10 @@ purge (FR rs rseq fs) = case Seq.viewl rseq of
                                                Just (rseq', fs') -> purge (FR rs rseq' fs')
                                              else purge (FR rs rest fs)
 
+-- | Compute the set of accessible cells without modifying the grid in place.
+--
+-- This performs the same neighbor counting as 'purge', but does not propagate
+-- frontier changes.
 removeFree :: ForkRolls -> IO IntSet
 removeFree (FR rs rseq freeSet) = do
   let isNotFree s p = insertFree p 0 s [-255, -256, -257, -1, 1, 255, 256, 257]
@@ -65,12 +93,19 @@ removeFree (FR rs rseq freeSet) = do
                    else insertFree p c s xs
         | otherwise = insertFree p c s xs
   foldM isNotFree freeSet rseq
-    
+
+-- | Count the number of accessible cells given a strategy
+-- such as 'purge' or 'removeFree'.
 countAccessible :: (ForkRolls -> IO IntSet) -> ForkRolls -> IO Int
 countAccessible f = fmap IS.size . f
 
+-- | Internal stream reader converting ByteString map input into a populated 'ForkRolls'.
+--
+-- `'@'` (64) becomes an occupied cell.
+-- `'.'` (46) becomes empty.
+-- Newlines adjust indexing by a full row width.
 mapReader :: IO (Int, ForkRolls) -> Fold IO Word8 ForkRolls
-mapReader = fmap snd . F.foldlM' folder 
+mapReader = fmap snd . F.foldlM' folder
   where
     folder :: (Int, ForkRolls) -> Word8 -> IO (Int, ForkRolls)
     folder (pos, FR rs rseq freeSet) w = case w of
@@ -81,13 +116,20 @@ mapReader = fmap snd . F.foldlM' folder
                             10 -> pure ((pos .|. 255) + 1, FR rs rseq freeSet)
                             _ -> error $ "unexpectd bit" ++ show w
 
+-- | Construct the mutable grid and frontier queue from stream input.
 mkMap :: Stream IO Word8 -> IO ForkRolls
 mkMap s = do
-  mv <- MV.new (1 `shiftL` 16) 
+  mv <- MV.new (1 `shiftL` 16)
   S.fold (mapReader $ pure (0, FR mv mempty mempty)) s
 
+-- | Solution for Part 1 (static accessibility).
+--
+-- Ignores frontier pruning and evaluates all candidates.
 part1 :: Bool -> Stream IO Word8 -> IO ()
 part1 _ s = mkMap s >>= countAccessible removeFree >>= print
 
+-- | Solution for Part 2 (dynamic frontier pruning).
+--
+-- Much faster thanks to cascading updates.
 part2 :: Bool -> Stream IO Word8 -> IO ()
 part2 _ s = mkMap s >>= countAccessible purge >>= print
