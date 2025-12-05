99952b953105b4d71931fb27f974fdf0261b6b4c
GuillaumedeVolpiano <xavier@wheredoibegin.fr>
2025-12-05 15:11:34 +0100

    2025, Day 5, optimising, first part

--- 2025/days/Day5.hs
+++ 2025/days/Day5.hs
@@ -6,22 +6,30 @@ module Day5
   , countAllFresh
   ) where
 
-import           Data.IntMap.Strict   (IntMap)
-import qualified Data.IntMap.Strict   as IM (delete, foldrWithKey, insert, keys,
-                                             lookupLE)
-import           Data.Maybe           (fromJust, isNothing)
-import           Data.Word            (Word8)
-import           Data.Word8           (_hyphen, _lf)
-import qualified Streamly.Data.Fold   as F (foldl')
-import qualified Streamly.Data.Stream as S (fold)
-import           Streamly.Data.Stream (Stream)
+import           Data.IntMap.Strict           (IntMap)
+import qualified Data.IntMap.Strict           as IM (assocs, delete,
+                                                     foldrWithKey, insert, keys,
+                                                     lookupLE)
+import           Data.Maybe                   (fromJust, isNothing)
+import           Data.Vector.Unboxed          (Vector)
+import qualified Data.Vector.Unboxed          as V (fromList, length,
+                                                    unsafeIndex)
+import           Data.Word                    (Word8)
+import           Data.Word8                   (_hyphen, _lf)
+import qualified Streamly.Data.Fold           as F (foldl')
+import qualified Streamly.Data.Stream         as S (fold, foldBreak)
+import           Streamly.Data.Stream         (Stream)
+import qualified Streamly.Internal.Data.Fold  as F (foldt')
+import           Streamly.Internal.Data.Scanl (Step (Done, Partial))
+import Data.Bits (shiftR)
 
 -- | Folding state machine: pper
 -- limit, current lower value (or just an value), current higher value, if in
 -- the first stage of the fold was the last character we saw a line feed, if in
 -- the first stage of the fold was the last character we saw an hyphen or a
 -- second linefeed, count of valid values
-data FoodRange = FR !(IntMap Int) {-# UNPACK #-} !Int {-# UNPACK #-} !Int !Bool !Bool {-# UNPACK #-} !Int
+data FoodRange = FR !(IntMap Int) {-# UNPACK #-} !Int {-# UNPACK #-} !Int !Bool !Bool
+data RangeCount = RC !(Vector (Int, Int)) {-# UNPACK #-} !Int {-# UNPACK #-} !Int  {-# UNPACK #-} !Int
 
 isDigit :: Word8 -> Bool
 isDigit w = w - 48 <= 9
@@ -31,65 +39,78 @@ digit :: Word8 -> Int
 digit w = fromIntegral (w - 48)
 {-# INLINE digit #-}
 
+mkRangeCount :: IntMap Int -> RangeCount
+mkRangeCount !lohis = RC v (V.length v) 0 0
+  where
+    v = V.fromList . IM.assocs $ lohis
+{-# SCC mkRangeCount #-}
+
 insertFR :: FoodRange -> FoodRange
-insertFR (FR !lohis !l !h _ _ c)
-  | isNothing h' = FR (IM.insert l h lohis) 0 0 True False c
+insertFR (FR !lohis !l !h _ _)
+  | isNothing h' = FR (IM.insert l h lohis) 0 0 True False
   | isNothing l' = FR (IM.insert l (max h . snd . fromJust $ h') . foldr IM.delete lohis
       . filter (\p -> p > l && p <= h)
-      . IM.keys $ lohis) 0 0 True False c
-  | l' == h' = if (snd .fromJust $ l') < l then FR (IM.insert l h lohis) 0 0 True False c
-                                        else FR (IM.insert (fst . fromJust $ l') (max h . snd . fromJust $ l') lohis) 0 0 True False c
-  | otherwise = FR lohis'' 0 0 True False c
+      . IM.keys $ lohis) 0 0 True False
+  | l' == h' = if (snd .fromJust $ l') < l then FR (IM.insert l h lohis) 0 0 True False
+                                        else FR (IM.insert (fst . fromJust $ l') (max h . snd . fromJust $ l') lohis) 0 0 True False
+  | otherwise = FR lohis'' 0 0 True False
   where
     l' = IM.lookupLE l lohis
     h' = IM.lookupLE h lohis
     lohis' = foldr IM.delete lohis . filter (\p -> p > l && p <= h) . IM.keys $ lohis
     lohis'' = if (snd .fromJust $ l') < l then IM.insert l (max h . snd . fromJust $ h') lohis'
                                           else IM.insert (fst . fromJust $ l') (max h . snd . fromJust $ h') lohis'
+{-# SCC insertFR #-}
 
-checkVal :: FoodRange -> FoodRange
-checkVal (FR !lohis !l h s t !c)
-  | isNothing l' = FR lohis 0 h s t c
-  | otherwise = if snd (fromJust l') >= l then FR lohis 0 h s t (c + 1)
-                                          else FR lohis 0 h s t c
+searchIngredient :: RangeCount -> RangeCount
+searchIngredient (RC v l n c) 
+  | l < fst (V.unsafeIndex v 0) || l > snd (V.unsafeIndex v (l - 1)) = RC v l 0 c
+  | otherwise = binarySearch 0 (l - 1)
   where
-    l' = IM.lookupLE l lohis
-{-# INLINE checkVal #-}
+    binarySearch !lo !hi
+      | lo > hi = RC v l 0 c
+      | n < a = binarySearch lo (mid - 1)
+      | n > b = binarySearch (mid + 1) hi
+      | otherwise = RC v l 0 (c + 1)
+      where
+        mid = (lo + hi) `shiftR` 1
+        (a, b) = V.unsafeIndex v mid
+{-# SCC searchIngredient #-}
 
-readDB :: FoodRange -> Word8 -> FoodRange
-readDB fr@(FR !lohis !l !h !switch !toggle c) w
-  | switch && toggle && isDigit w = FR lohis (10 * l + digit w) h True True c
-  | switch && toggle && w == _lf = checkVal fr
-  | isDigit w = if toggle then FR lohis l (10*h + digit w) False toggle c
-                          else FR lohis (10*l + digit w) h False toggle c
-  | w == _hyphen = FR lohis l h switch True c
-  | w == _lf = if switch then FR lohis l h True True c
-                         else insertFR fr
+shortReadDB :: FoodRange -> Word8 -> Step FoodRange (IntMap Int)
+shortReadDB fr@(FR !lohis !l !h !switch !toggle) w
+  | switch && toggle = Done lohis
+  | isDigit w = if toggle then Partial $ FR lohis l (10*h + digit w) False toggle
+                          else Partial $ FR lohis (10*l + digit w) h False toggle
+  | w == _hyphen = Partial $ FR lohis l h switch True
+  | w == _lf = if switch then Partial $ FR lohis l h True True
+                         else Partial $ insertFR fr
   | otherwise = undefined
 
-shortReadDB :: FoodRange -> Word8 -> FoodRange
-shortReadDB fr@(FR !lohis !l !h !switch !toggle c) w
-  | switch && toggle = fr
-  | isDigit w = if toggle then FR lohis l (10*h + digit w) False toggle c
-                          else FR lohis (10*l + digit w) h False toggle c
-  | w == _hyphen = FR lohis l h switch True c
-  | w == _lf = if switch then FR lohis l h True True c
-                         else insertFR fr
+readIngredients :: RangeCount -> Word8 -> RangeCount
+readIngredients rc@(RC v l n c) w
+  | isDigit w = RC v l (10 * n + digit w) c
+  | w == _lf = searchIngredient rc
   | otherwise = undefined
 
-getCount :: FoodRange -> Int
-getCount (FR _ _ _ _ _ !c) = c
-{-# INLINE getCount #-}
-
-freshCount :: FoodRange -> Int
-freshCount (FR !lohis _ _ _ _ _) = IM.foldrWithKey (\k v c -> c + v - k + 1) 0 lohis
+freshCount :: IntMap Int -> Int
+freshCount !lohis = IM.foldrWithKey (\k v c -> c + v - k + 1) 0 lohis
 {-# INLINE freshCount #-}
 
+getCount :: RangeCount -> Int
+getCount (RC _ _ _ c) = c
+{-# INLINE getCount #-}
+
+extractLoHis :: FoodRange -> IntMap Int
+extractLoHis (FR !lohis _ _ _ _) = lohis
+{-# INLINE extractLoHis #-}
+
 countFresh :: Stream IO Word8 -> IO Int
-countFresh s = getCount <$> S.fold (F.foldl' readDB (FR mempty 0 0 False False 0)) s
+countFresh s = S.foldBreak (F.foldt' shortReadDB (Partial $ FR mempty 0 0 False False) extractLoHis) s
+  >>= \(res, s') -> getCount <$> S.fold (F.foldl' readIngredients (mkRangeCount res)) s'
 
 countAllFresh :: Stream IO Word8 -> IO Int
-countAllFresh s = freshCount <$> S.fold (F.foldl' shortReadDB (FR mempty 0 0 False False 0)) s
+countAllFresh s = freshCount <$> S.fold (F.foldt' shortReadDB (Partial $ FR mempty 0 0 False False) extractLoHis) s
 
 part1 :: Bool -> Stream IO Word8 -> IO ()
 part1 _ s = countFresh s >>= print
--- bench/general/Bench.hs
+++ bench/general/Bench.hs
@@ -10,14 +10,24 @@ import           Test.Tasty.Bench     (Benchmark, bench, defaultMain, env,
                                        whnfIO)
 import Data.Function ((&))
 import qualified Streamly.Data.Fold as F (drain)
+import Streamly.Internal.Data.Fold (Step(Partial, Done))
+import Data.Word8 (Word8, _lf)
+import qualified Streamly.Internal.Data.Fold as F (foldt')
 
 inputPath :: String
 inputPath = "/github/adventOfCode/input/2025/day5.txt"
+
+shortDrain :: Bool -> Word8 -> Step Bool ()
+shortDrain b w
+  | w == _lf && b = Done ()
+  | w == _lf = Partial True
+  | otherwise = Partial False
 
 tests :: IO ByteString -> [Benchmark]
 tests input =
   [ env (BS.unpack <$> input) $ \bs -> bench "Overhead" $ whnfIO $ S.fromList bs & S.fold F.drain
   , env (BS.unpack <$> input) $ \bs -> bench "Part 1" $ whnfIO $ S.fromList bs & Day5.countFresh
+  , env (BS.unpack <$> input) $ \bs -> bench "Part 2 overhead" $ whnfIO $ S.fromList bs & S.fold (F.foldt' shortDrain (Partial True) (const ()))
   , env (BS.unpack <$> input) $ \bs -> bench "Part 2" $ whnfIO $ S.fromList bs & Day5.countAllFresh ]
 
 main :: IO ()
