9d8986efb1a5b7e97045060a7552518e51627886
GuillaumedeVolpiano <xavier@wheredoibegin.fr>
2025-12-07 15:49:01 +0100

    Day6, finally optimised

--- 2025/days/Day6.hs
+++ 2025/days/Day6.hs
@@ -1,98 +1,215 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Day6
   ( part1
   , part2
-  , getWrongWorksheet
-  , getTotal
-  , getCorrectWorksheet
-  , buildWrongWorksheet
   , getCorrectTotal
+  , foldSheet
+  , getTotal
   ) where
 
 import           Control.DeepSeq          (NFData, rnf)
-import           Data.Bifunctor           (second)
-import           Data.Either              (fromRight)
-import qualified Data.List                as L (transpose)
 import           Data.Word                (Word8)
 import           Helpers.General.Streamly (digit, isDigit)
-import qualified Streamly.Data.Fold       as F (drain, foldl', toList)
-import           Streamly.Data.Fold       (Fold)
-import qualified Streamly.Data.Parser     as P (eof, many, manyTill, satisfy,
-                                                some)
-import           Streamly.Data.Parser     (Parser)
-import qualified Streamly.Data.Stream     as S (parse)
+import qualified Streamly.Data.Fold       as F (foldl')
+import qualified Streamly.Data.Stream     as S (fold)
 import           Streamly.Data.Stream     (Stream)
+import Data.Sequence (Seq((:|>), (:<|)), ViewL((:<), EmptyL))
+import qualified Data.Sequence as Sq (viewl, splitAt, drop)
 
-data Op = Add Int | Mult Int deriving (Show, Eq)
+-- | Operators applied to columns.
+--
+--   Each operator also tracks how many numbers are under it in that column.
+--   * 'Add n' – column to be summed, with n numbers.
+--   * 'Mult n' – column to be multiplied, with n numbers.
+--   * 'Null' – placeholder for initial/filler state.
+data Op = Add Int | Mult Int | Null deriving (Show, Eq)
+
+-- | Correct worksheet state: left numbers, current in-progress number, right
+-- numbers.
+data Correct = C !(Seq Int) {-# UNPACK #-} !Int !(Seq Int) deriving Show
+
+-- | Wrong worksheet state for a column: accumulates sum/product per operator.
+data Oped = O {-# UNPACK #-} !Int {-# UNPACK #-} !Int deriving Show
+
+-- | Wrong worksheet state: left columns, current in-progress value, right columns.
+data Wrong = W !(Seq Oped) {-# UNPACK #-} !Int !(Seq Oped) deriving Show
+
+
+-- | Holds the internal parser/folder state while scanning the input stream.
+--
+-- Fields:
+--   * Bool        – whether the previous character was a space
+--   * Correct     – state of the correct worksheet for columns
+--   * Wrong       – state of the wrong worksheet for columns
+--   * Op          – current operator
+data FoldState = FS Bool !Correct !Wrong !Op deriving Show
+
+-- | Represents the final worksheet parsed from input.
+--
+-- Fields:
+--   * Int – total of wrong worksheet values
+--   * Int – total of correct worksheet values
+data WorkSheet = WS {-# UNPACK #-} !Int {-# UNPACK #-} !Int 
+
+instance NFData Oped where
+  rnf (O a p) = rnf a `seq` rnf p
+
+instance NFData WorkSheet where
+  rnf (WS w c) = rnf w `seq` rnf c
 
 instance NFData Op where
   rnf (Add v)  = rnf v
   rnf (Mult v) = rnf v
+  rnf Null = rnf (0 :: Int)
 
-toInt :: Monad m => Fold m Word8 Int
-toInt = F.foldl' (\a b -> digit b + 10 * a) 0
+-- | Byte constants for parsing.
+pattern LF :: Word8
+pattern LF = 10
 
-correctParser :: Monad m => Parser Word8 m ([Op], [[Int]])
-correctParser = P.many parseIntLineRight F.toList >>= \ints -> parseOps >>= \ops -> pure (ops, ints)
+pattern Space :: Word8
+pattern Space = 32
 
-parseIntLineRight :: Monad m => Parser Word8 m [Int]
-parseIntLineRight = P.manyTill ((\w -> if w == 32 then -1 else digit w) <$> P.satisfy (\w -> w == 32 || isDigit w)) (P.satisfy (== 10)) F.toList
+pattern Times :: Word8
+pattern Times = 42
 
-wrongParser :: Monad m => Parser Word8 m ([Op], [[Int]])
-wrongParser = P.many parseIntLineWrong F.toList >>= \ints -> parseOps >>= \ops -> pure (ops, ints)
+pattern Plus :: Word8
+pattern Plus = 43
 
-parseIntLineWrong :: Monad m => Parser Word8 m [Int]
-parseIntLineWrong = P.manyTill parseIntWrong (P.many (P.satisfy (== 32)) F.drain >> P.satisfy (==10)) F.toList
-
-parseIntWrong :: Monad m => Parser Word8 m Int
-parseIntWrong = P.many (P.satisfy (== 32)) F.drain >> P.some (P.satisfy isDigit) toInt
-
-parseOps :: Monad m => Parser Word8 m [Op]
-parseOps = do
-  ops <- P.some parseOp F.toList
-  P.eof
-  pure ops
-
-parseOp :: Monad m => Parser Word8  m Op
-parseOp = do
-  v <- P.satisfy (\w -> w == 42 || w == 43)
-  l <- length <$> P.many (P.satisfy (\w -> w == 32 || w == 10)) F.toList
-  pure $ if v == 42 then Mult l else Add l
-
-getWrongWorksheet :: Stream IO Word8 -> IO ([Op], [[Int]])
-getWrongWorksheet s = fromRight undefined <$> S.parse wrongParser s
-
-getCorrectWorksheet :: Stream IO Word8 -> IO ([Op], [[Int]])
-getCorrectWorksheet s = fromRight undefined <$> S.parse correctParser s
-
-buildWrongWorksheet :: [[Int]] -> [[Int]]
-buildWrongWorksheet = L.transpose
-
-getTotal :: [Op] -> [[Int]] -> Int
-getTotal os = sum  . zipWith calc os
-  where
-    calc (Add _)  = sum
-    calc (Mult _) = product
-
-getCorrectTotal :: [Op] -> [[Int]] -> Int
-getCorrectTotal [] is
-  | all null is = 0
+-- | Fold function applied over the stream of input bytes.
+--   Updates the FoldState according to spaces, newlines, digits, and operators.
+folder :: FoldState -> Word8 -> FoldState
+folder fs@(FS seenSpace correct wrong op) w
+  | w == Space && op == Null = if seenSpace then slideSpace fs
+                              else slideNoSpace fs
+  | w == Space = FS False correct wrong (increaseOp op)
+  | w ==  LF && op == Null = slideEnd fs
+  | w == LF = FS False (correctCalc correct $ increaseOp op) (calc wrong op) (increaseOp op)
+  | w == Times = mult fs
+  | w == Plus = addition fs
+  | isDigit w = addFS (digit w) fs
   | otherwise = undefined
-getCorrectTotal (o:os) is = r + getCorrectTotal os is'
+
+-- | Update the wrong worksheet with the current operator.
+calc :: Wrong -> Op -> Wrong
+calc (W leftWrong acc ((O add _) :<| next)) (Add _) = W leftWrong (acc + add) next
+calc (W leftWrong acc ((O _ prod) :<| next)) (Mult _) = W leftWrong (acc + prod) next
+calc _ _ = undefined
+
+-- | Update the correct worksheet with the current operator.
+correctCalc :: Correct -> Op -> Correct
+correctCalc (C leftCorrect acc rightCorrect) op = C leftCorrect acc' next'
   where
-    l = case o of
-          Add v  -> v
+    l = case op of
+          Add v -> v
           Mult v -> v
-    sis = map (splitAt l) is
-    is' = map (drop 1 .snd) sis
-    i = buildVals . map fst $ sis
-    r = case o of
-          Add _  -> sum i
-          Mult _ -> product i
-    buildVals = map (foldl' (\c v -> if v == -1 then c else v + 10*c) 0) . L.transpose
+          Null -> undefined
+    (cur, next) = Sq.splitAt l rightCorrect
+    next' = Sq.drop 1 next
+    acc' = case op of
+             Add _ -> acc + sum cur
+             Mult _ -> acc + product cur
+             Null -> undefined
+
+-- | Set multiplication operator in FoldState.
+mult :: FoldState -> FoldState
+mult (FS seenSpace correct@(C leftCorrect _ rightCorrect) wrong@(W leftWrong _ rightWrong) op)
+  | op == Null = FS seenSpace (C leftCorrect 0 rightCorrect) (W leftWrong 0 rightWrong) (Mult 0) 
+  | otherwise = FS seenSpace (correctCalc correct op) (calc wrong op) (Mult 0)
+
+-- | Set addition operator in FoldState.
+addition :: FoldState -> FoldState
+addition (FS seenSpace correct@(C leftCorrect _ rightCorrect) wrong@(W leftWrong _ rightWrong) op)
+  | op == Null = FS seenSpace (C leftCorrect 0 rightCorrect) (W leftWrong 0 rightWrong) (Add 0)
+  | otherwise = FS seenSpace (correctCalc correct op) (calc wrong op) (Add 0)
+
+
+-- | Handle end-of-line: shift columns and reset current counters.
+slideEnd :: FoldState -> FoldState
+slideEnd (FS seenSpace correct wrong@(W leftWrong _ _) ops)
+  | seenSpace = FS False (C mempty (-1) lc) (W mempty 0 leftWrong) ops
+  | otherwise = FS False (C mempty (-1) lc) (W mempty 0 lw) ops
+  where
+    (C lc _ _) = slideCorrect correct
+    (W lw _ _) = slideWrong wrong
+
+-- | Add a digit to the current column in the state.
+addFS :: Int -> FoldState -> FoldState
+addFS dig (FS _ correct (W leftWrong wrong rightWrong) ops) =
+  FS False correct' (W leftWrong (10*wrong + dig) rightWrong) ops
+    where
+      correct' = addCorrect dig correct
+{-# INLINE addFS #-}
+
+-- | Update the correct worksheet state when a digit is added.
+addCorrect :: Int -> Correct -> Correct
+addCorrect !dig correct = C lc (10*c + dig) rc
+  where
+    (C lc c rc) = slideCorrect correct
+{-# INLINE addCorrect #-}
+
+-- | Handle a space character when parsing correct numbers.
+slideSpace :: FoldState -> FoldState
+slideSpace (FS _ correct wrong ops) =
+  FS True correct' wrong ops
+  where
+    correct' = slideCorrect correct
+{-# INLINE slideSpace #-}
+
+-- | Shift the current correct value into leftCorrect and advance the
+-- rightCorrect cursor.
+slideCorrect :: Correct -> Correct
+slideCorrect (C !leftCorrect !correct !rightCorrect)
+  | correct < 0 = case Sq.viewl rightCorrect of
+                    EmptyL -> C leftCorrect 0 rightCorrect
+                    (c' :< rest) -> C leftCorrect c' rest
+  | otherwise = case Sq.viewl rightCorrect of
+                  EmptyL -> C (leftCorrect :|> correct) 0 rightCorrect
+                  (c' :< rest) -> C (leftCorrect :|> correct) c' rest
+{-# INLINE slideCorrect #-}
+
+
+-- | Shift the current wrong value into leftWrong and advance the rightWrong
+-- cursor.
+slideWrong :: Wrong -> Wrong
+slideWrong (W !leftWrong !wrong !rightWrong)
+  | wrong == 0 = W leftWrong 0 rightWrong
+  | otherwise = case Sq.viewl rightWrong of
+                  EmptyL -> W (leftWrong :|> O wrong wrong) 0 rightWrong
+                  ((O add prod) :< rest) -> W (leftWrong :|> O (wrong + add) (wrong * prod)) 0 rest
+{-# INLINE slideWrong #-}
+
+-- | Increase the number of numbers counted for the last operator.
+increaseOp :: Op -> Op
+increaseOp (Add v) = Add (v + 1)
+increaseOp (Mult v) = Mult (v + 1)
+increaseOp _ = undefined
+{-# INLINE increaseOp #-}
+
+-- | Handle a space when no previous space was seen.
+slideNoSpace :: FoldState -> FoldState
+slideNoSpace (FS _ correct wrong ops) = FS True correct' wrong' ops
+  where
+    correct' = slideCorrect correct
+    wrong' = slideWrong wrong
+{-# INLINE slideNoSpace #-}
+
+-- | Fold a stream of Word8 into a WorkSheet.
+foldSheet :: Stream IO Word8 -> IO WorkSheet
+foldSheet = fmap (\(FS _ (C _ correct _) (W _ wrong _) _ ) -> WS wrong correct) .
+  S.fold (F.foldl' folder (FS False (C mempty (-1) mempty) (W mempty 0 mempty) Null))
+
+-- | Compute the total for the wrong worksheet values
+getTotal :: WorkSheet -> Int
+getTotal (WS w _) = w
+
+-- | Compute the total for using correct worksheet values.
+getCorrectTotal :: WorkSheet -> Int
+getCorrectTotal (WS _ c) = c
 
 part1 :: Bool -> Stream IO Word8 -> IO ()
-part1 _ s = getWrongWorksheet s >>= print . uncurry getTotal . second buildWrongWorksheet
+part1 _ s = foldSheet s >>= print . getTotal
 
 part2 :: Bool -> Stream IO Word8 -> IO ()
-part2 _ s = getCorrectWorksheet s >>= print . uncurry getCorrectTotal
+part2 _ s = foldSheet s >>= print . getCorrectTotal
--- bench/general/Bench.hs
+++ bench/general/Bench.hs
@@ -3,18 +3,18 @@ module Main where
 
 import           Data.ByteString             (ByteString)
 import qualified Data.ByteString             as BS (readFile, unpack)
+import           Data.Function               ((&))
 import           Data.Word8                  (Word8, _lf)
-import qualified Day7
-import qualified Streamly.Data.Stream        as S (fromList, fold)
-import qualified Streamly.Data.Fold as F (drain)
+import qualified Day6
+import qualified Streamly.Data.Fold          as F (drain)
+import qualified Streamly.Data.Stream        as S (fold, fromList)
 import           Streamly.Internal.Data.Fold (Step (Done, Partial))
 import           System.Directory            (getHomeDirectory)
-import           Test.Tasty.Bench            (Benchmark, bench,
-                                              defaultMain, env, whnfIO)
-import Data.Function ((&))
+import           Test.Tasty.Bench            (Benchmark, bench, defaultMain,
+                                              env, whnf, whnfIO)
 
 inputPath :: String
-inputPath = "/github/adventOfCode/input/2025/day7.txt"
+inputPath = "/github/adventOfCode/input/2025/day6.txt"
 
 shortDrain :: Bool -> Word8 -> Step Bool ()
 shortDrain b w
@@ -22,14 +22,17 @@ shortDrain b w
   | w == _lf = Partial True
   | otherwise = Partial False
 
-tests :: IO ByteString -> [Benchmark]
-tests input =
-  [ env (BS.unpack <$> input) $ \bs -> bench "Overhead" $ whnfIO $ S.fromList bs & S.fold F.drain
-  , env (BS.unpack <$> input) $ \bs -> bench "Part 1" $ whnfIO $ S.fromList bs & Day7.countSplits
-  , env (BS.unpack <$> input) $ \bs -> bench "Part 2" $ whnfIO $ S.fromList bs & Day7.countPaths ]
+tests :: ByteString -> IO ByteString -> [Benchmark]
+tests input ioInput =
+  [ env (Day6.foldSheet . S.fromList . BS.unpack $ input) $ \ws -> bench "Part 1" $ whnf Day6.getTotal ws
+  , env (Day6.foldSheet . S.fromList . BS.unpack $ input) $ \ws -> bench "Part 2" $ whnf Day6.getCorrectTotal ws
+  , env (BS.unpack <$> ioInput) $ \bs -> bench "Overhead" $ whnfIO $ S.fromList bs & S.fold F.drain
+  , env (BS.unpack <$> ioInput) $ \bs -> bench "Part 1 with parsing" $ whnfIO $ Day6.getTotal <$> (S.fromList bs & Day6.foldSheet) 
+  , env (BS.unpack <$> ioInput) $ \bs -> bench "Part 2 with parsing" $ whnfIO $ Day6.getCorrectTotal <$> (S.fromList bs & Day6.foldSheet)]
 
 main :: IO ()
 main = do
   home <- getHomeDirectory
-  let file = BS.readFile $ home ++ inputPath
-  defaultMain . tests $ file
+  let file = BS.readFile $ home ++ inputPath
+  file' <- file
+  defaultMain . tests file' $ file
