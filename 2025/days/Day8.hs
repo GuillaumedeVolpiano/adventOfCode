34a98990c7b83d712ab5b961cfce644313b68456
GuillaumedeVolpiano <xavier@wheredoibegin.fr>
2025-12-08 19:14:04 +0100

    Day 8, optimised part 2

--- 2025/days/Day8.hs
+++ 2025/days/Day8.hs
@@ -11,9 +11,12 @@ module Day8
   , DisjointSets
   ) where
 
+import           Control.DeepSeq          (NFData, rnf)
+import           Data.Bifunctor           (first)
 import           Data.Hashable            (Hashable, hashWithSalt)
-import           Data.HashMap.Strict      (HashMap)
-import qualified Data.HashMap.Strict      as HM (foldrWithKey, insert, (!), filterWithKey, elems, size)
+import           Data.IntMap.Strict       (IntMap)
+import qualified Data.IntMap.Strict       as IM (elems, filterWithKey, insert,
+                                                 size, (!))
 import qualified Data.List                as L (sortBy, uncons)
 import           Data.Ord                 (Down (Down), comparing)
 import           Data.Word                (Word8)
@@ -25,16 +28,14 @@ import qualified Streamly.Data.Parser     as P (eof, manyTill, satisfy)
 import           Streamly.Data.Parser     (Parser)
 import qualified Streamly.Data.Stream     as S (parse)
 import           Streamly.Data.Stream     (Stream)
-import Data.Bifunctor (first)
-import Control.DeepSeq (NFData, rnf)
 
 data Coords = C {-# UNPACK #-} !Int {-# UNPACK #-} !Int {-# UNPACK #-} !Int deriving (Eq, Ord, Show)
 
-type Queue = [(Int, Coords, Coords)]
+type Queue = [(Int, Int, Int, Int, Int)]
 
 data DisjointSets = DS  {
-                          parent :: !(HashMap Coords Coords)
-                        , size :: !(HashMap Coords Int)
+                          parent :: !(IntMap Int)
+                        , size   :: !(IntMap Int)
                         }
 
 instance NFData Coords where
@@ -52,32 +53,32 @@ instance Semigroup DisjointSets where
 instance Monoid DisjointSets where
   mempty = DS mempty mempty
 
-mkSet :: Coords -> DisjointSets -> DisjointSets
-mkSet c (DS p s) = DS (HM.insert c c p) (HM.insert c 1 s)
+mkSet :: Int -> DisjointSets -> DisjointSets
+mkSet c (DS p s) = DS (IM.insert c c p) (IM.insert c 1 s)
 {-# INLINE mkSet #-}
 
-find :: Coords -> DisjointSets -> (Coords, DisjointSets)
+find :: Int -> DisjointSets -> (Int, DisjointSets)
 find c ds
   | pc == c = (c, ds)
   | otherwise = (c', ds')
   where
-    pc = parent ds HM.! c
+    pc = parent ds IM.! c
     (c', DS p s) = find pc ds
-    ds' = DS (HM.insert c c' p) s 
+    ds' = DS (IM.insert c c' p) s
 
-union :: Coords -> Coords -> DisjointSets -> (DisjointSets, Int)
+union :: Int -> Int -> DisjointSets -> (DisjointSets, Int)
 union c c' ds
   | x == y = (ds'', xs)
-  | xs < ys = (DS (HM.insert x y p) (HM.insert y (xs + ys) s), xs + ys)
-  | otherwise = (DS (HM.insert y x p) (HM.insert x (xs + ys) s), xs + ys)
+  | xs < ys = (DS (IM.insert x y p) (IM.insert y (xs + ys) s), xs + ys)
+  | otherwise = (DS (IM.insert y x p) (IM.insert x (xs + ys) s), xs + ys)
   where
     (x, ds') = find c ds
     (y, ds''@(DS p s)) = find c' ds'
-    xs = size ds'' HM.! x
-    ys = size ds'' HM.! y
+    xs = size ds'' IM.! x
+    ys = size ds'' IM.! y
 
-isRoot :: DisjointSets -> Coords -> Bool
-isRoot (DS p _) c = p HM.! c == c
+isRoot :: DisjointSets -> Int -> Bool
+isRoot (DS p _) c = p IM.! c == c
 
 distance :: Coords -> Coords -> Int
 distance (C h w d) (C h' w' d') = ((h - h')^(2 :: Int)) + ((w - w')^(2 :: Int)) + ((d - d')^(2 :: Int))
@@ -87,11 +88,11 @@ toInt :: Fold IO Word8 Int
 toInt = F.foldl' (\a b -> digit b + 10 * a) 0
 
 toQueue :: Fold IO Coords (Queue, DisjointSets)
-toQueue = first (L.sortBy (\(a, _, _) (b, _, _) -> compare a b)) <$> F.foldl' folder ([], mempty)
+toQueue = first (L.sortBy (\(a, _, _, _, _) (b, _, _, _, _) -> compare a b)) . (\(a, b, _, _) -> (a, b)) <$> F.foldl' folder ([], mempty, [], 0)
   where
-    folder (queue, circuits) coords = (HM.foldrWithKey (addQueue coords) queue $ parent circuits
-      , mkSet coords circuits)
-    addQueue coords coords' _ = ((distance coords coords', coords, coords') :)
+    folder (queue, circuits, cs, p) coords = (foldr (addQueue coords p) queue cs
+      , mkSet p circuits, (coords, p):cs, p + 1)
+    addQueue coords@(C x _ _) p (coords'@(C x' _ _), p') = ((distance coords coords', p, p', x, x') :)
 
 parseLine :: Parser Word8 IO Coords
 parseLine = do
@@ -108,15 +109,15 @@ getCircuit 0 _ circuit = circuit
 getCircuit c queue circuit = getCircuit (c - 1) queue' circuit'
   where
     mPairQueue = L.uncons queue
-    ((_, coords, coords'), queue') = case mPairQueue of
+    ((_, coords, coords', _, _), queue') = case mPairQueue of
                                        Nothing -> error $ "empty queue, still needed " ++ show c ++ " connections."
                                        Just (v, q) -> (v, q)
     (circuit', _) = union coords coords' circuit
-  
+
 prodThree :: DisjointSets -> Int
-prodThree ds@(DS _ s) = product . take 3 . L.sortBy (comparing Down) . HM.elems $ s'
+prodThree ds@(DS _ s) = product . take 3 . L.sortBy (comparing Down) . IM.elems $ s'
   where
-    s' = HM.filterWithKey (\k _ -> isRoot ds k) s
+    s' = IM.filterWithKey (\k _ -> isRoot ds k) s
 
 
 connectAll :: Int -> Queue -> DisjointSets -> Int
@@ -125,7 +126,7 @@ connectAll sz queue ds
   | otherwise = connectAll sz queue' ds'
   where
     mPairQueue = L.uncons queue
-    ((_, coords@(C x _ _), coords'@(C x' _ _)), queue') = case mPairQueue of
+    ((_, coords, coords', x, x'), queue') = case mPairQueue of
                                                           Nothing -> undefined
                                                           Just (k, q) -> (k, q)
     (ds', ns) = union coords coords' ds
@@ -133,7 +134,7 @@ connectAll sz queue ds
 findLastCircuits :: Stream IO Word8 -> IO Int
 findLastCircuits s = do
   (queue, circuit) <- either (error "parser failed") id <$> S.parse parseInput s
-  let sz = HM.size (parent circuit)
+  let sz = IM.size (parent circuit)
   pure $ connectAll sz queue circuit
 
 findCircuits :: Bool -> Stream IO Word8 -> IO Int
